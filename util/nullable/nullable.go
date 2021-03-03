e62b990082020f9c7c42383bb0f65f4f50957ccc
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-03-03 17:35:49 +0100

    Add missing docstrings to the nullable pkg

--- util/nullable/nullable.go
+++ util/nullable/nullable.go
@@ -5,10 +5,13 @@ import (
 	"encoding/json"
 )
 
+// NullBool relays the sql package NullBool and adds the json
+// marshal and unmarshal methods.
 type NullBool struct {
 	sql.NullBool
 }
 
+// MarshalJSON is the interface method to dump this type to json.
 func (nb NullBool) MarshalJSON() ([]byte, error) {
 	if nb.Valid {
 		return json.Marshal(nb.Bool)
@@ -16,6 +19,7 @@ func (nb NullBool) MarshalJSON() ([]byte, error) {
 	return json.Marshal(nil)
 }
 
+// UnmarshalJSON is the interface method to load a json into this type.
 func (nb *NullBool) UnmarshalJSON(data []byte) error {
 	var b *bool
 	if err := json.Unmarshal(data, &b); err != nil {
