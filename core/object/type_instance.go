7b004cd4416615c53175459321f71612a09cbec8
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-03-18 14:41:04 +0100

    Add flags rendering in print status

--- core/object/type_instance.go
+++ core/object/type_instance.go
@@ -8,12 +8,13 @@ import (
 type (
 	// InstanceMonitor describes the in-daemon states of an instance
 	InstanceMonitor struct {
-		GlobalExpect        string  `json:"global_expect"`
-		LocalExpect         string  `json:"local_expect"`
-		Status              string  `json:"status"`
-		StatusUpdated       float64 `json:"status_updated"`
-		GlobalExpectUpdated float64 `json:"global_expect_updated"`
-		Placement           string  `json:"placement"`
+		GlobalExpect        string         `json:"global_expect"`
+		LocalExpect         string         `json:"local_expect"`
+		Status              string         `json:"status"`
+		StatusUpdated       float64        `json:"status_updated"`
+		GlobalExpectUpdated float64        `json:"global_expect_updated"`
+		Placement           string         `json:"placement"`
+		Restart             map[string]int `json:"restart,omitempty"`
 	}
 
 	// InstanceConfig describes a configuration file content checksum,
@@ -31,20 +32,20 @@ type (
 		Nodename    string                    `json:"-"`
 		Path        Path                      `json:"-"`
 		App         string                    `json:"app,omitempty"`
-		Avail       status.T               `json:"avail,omitempty"`
+		Avail       status.T                  `json:"avail,omitempty"`
 		DRP         bool                      `json:"drp,omitempty"`
-		Overall     status.T               `json:"overall,omitempty"`
+		Overall     status.T                  `json:"overall,omitempty"`
 		Csum        string                    `json:"csum,omitempty"`
 		Env         string                    `json:"env,omitempty"`
 		Frozen      float64                   `json:"frozen,omitempty"`
 		Kind        Kind                      `json:"kind"`
 		Monitor     InstanceMonitor           `json:"monitor"`
-		Optional    status.T               `json:"optional,omitempty"`
+		Optional    status.T                  `json:"optional,omitempty"`
 		Orchestrate string                    `json:"orchestrate,omitempty"` // TODO enum
 		Topology    string                    `json:"topology,omitempty"`    // TODO enum
 		Placement   string                    `json:"placement,omitempty"`   // TODO enum
 		Priority    int                       `json:"priority,omitempty"`
-		Provisioned provisioned.T          `json:"provisioned,omitempty"`
+		Provisioned provisioned.T             `json:"provisioned,omitempty"`
 		Preserved   bool                      `json:"preserved,omitempty"`
 		Updated     float64                   `json:"updated"`
 		FlexTarget  int                       `json:"flex_target,omitempty"`
@@ -52,8 +53,14 @@ type (
 		FlexMax     int                       `json:"flex_max,omitempty"`
 		Subsets     map[string]SubsetStatus   `json:"subsets,omitempty"`
 		Resources   map[string]ResourceStatus `json:"resources,omitempty"`
-		Running     []string                  `json:"running,omitempty"`
+		Running     ResourceRunningSet        `json:"running,omitempty"`
 	}
+
+	// ResourceRunningSet is the list of resource currently running (sync and task).
+	ResourceRunningSet []string
+
+	// TagSet is the list of unique tag names found in the resource definition.
+	TagSet []string
 
 	// SubsetStatus describes a resource subset properties.
 	SubsetStatus struct {
@@ -63,15 +70,33 @@ type (
 	// ResourceStatus describes the status of a resource of an instance of an object.
 	ResourceStatus struct {
 		Label       string                  `json:"label"`
-		Log         []string                `json:"log"`
-		Status      status.T             `json:"status"`
+		Log         []string                `json:"log,omitempty"`
+		Status      status.T                `json:"status"`
 		Type        string                  `json:"type"`
 		Provisioned ResourceStatusProvision `json:"provisioned"`
+		Monitor     bool                    `json:"monitor,omitempty"`
+		Disable     bool                    `json:"disable,omitempty"`
+		Optional    bool                    `json:"optional,omitempty"`
+		Encap       bool                    `json:"encap,omitempty"`
+		Standby     bool                    `json:"standby,omitempty"`
+		Subset      string                  `json:"subset,omitempty"`
+		Info        map[string]string       `json:"info,omitempty"`
+		Restart     int                     `json:"restart,omitempty"`
+		Tags        TagSet                  `json:"tags,omitempty"`
 	}
 
 	// ResourceStatusProvision define if and when the resource became provisioned.
 	ResourceStatusProvision struct {
-		Mtime float64          `json:"mtime"`
+		Mtime float64       `json:"mtime"`
 		State provisioned.T `json:"state"`
 	}
 )
+
+func (t ResourceRunningSet) Has(rid string) bool {
+	for _, r := range t {
+		if r == rid {
+			return true
+		}
+	}
+	return false
+}
--- core/object/type_instance_render.go
+++ core/object/type_instance_render.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 
 	"opensvc.com/opensvc/config"
+	"opensvc.com/opensvc/core/provisioned"
 	"opensvc.com/opensvc/util/render/palette"
 	"opensvc.com/opensvc/util/render/tree"
+)
+
+const (
+	FlagEmpty = "."
 )
 
 // Render returns a human friendly string representation of the type instance.
@@ -34,8 +39,91 @@ func (t InstanceStatus) LoadTreeNode(head *tree.Node) {
 	for rid, r := range t.Resources {
 		n := head.AddNode()
 		n.AddColumn().AddText(rid)
-		n.AddColumn().AddText("") // flags
+		n.AddColumn().AddText(t.resourceFlagsString(rid, r)) // flags
 		n.AddColumn().AddText(r.Status.ColorString())
 		n.AddColumn().AddText(r.Label)
 	}
+}
+
+//
+// resourceFlagsString formats resource flags as a vector of characters.
+//
+//   R  Running
+//   M  Monitored
+//   D  Disabled
+//   O  Optional
+//   E  Encap
+//   P  Provisioned
+//   S  Standby
+//
+func (t InstanceStatus) resourceFlagsString(rid string, r ResourceStatus) string {
+	flags := ""
+
+	// Running task or sync
+	if t.Running.Has(rid) {
+		flags += "R"
+	} else {
+		flags += FlagEmpty
+	}
+
+	// Monitored
+	if r.Monitor {
+		flags += "M"
+	} else {
+		flags += FlagEmpty
+	}
+
+	// Disabled
+	if r.Disable {
+		flags += "D"
+	} else {
+		flags += FlagEmpty
+	}
+
+	// Optional
+	if r.Optional {
+		flags += "O"
+	} else {
+		flags += FlagEmpty
+	}
+
+	// Encapsulated
+	if r.Encap {
+		flags += "E"
+	} else {
+		flags += FlagEmpty
+	}
+
+	// Provisioned
+	switch r.Provisioned.State {
+	case provisioned.True:
+		flags += "."
+	case provisioned.False:
+		flags += "P"
+	default:
+		flags += "/"
+	}
+
+	// Standby
+	if r.Standby {
+		flags += "S"
+	} else {
+		flags += FlagEmpty
+	}
+
+	// Restart and retries
+	retries := 0
+	retries, _ = t.Monitor.Restart[rid]
+	remaining_restart := r.Restart - retries
+	switch {
+	case r.Restart <= 0:
+		flags += "."
+	case remaining_restart < 0:
+		flags += "0"
+	case remaining_restart < 10:
+		flags += fmt.Sprintf("%d", remaining_restart)
+	default:
+		flags += "+"
+	}
+	return flags
 }
