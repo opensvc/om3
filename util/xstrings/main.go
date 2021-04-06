478aa4d71a831c31bf7831fbfaa5fd68e09d18b0
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-04-06 15:43:18 +0200

    Add Capitalize() to util/xstring
    And add more tests.

--- util/xstrings/main.go
+++ util/xstrings/main.go
@@ -5,6 +5,9 @@ import "strings"
 // TrimLast returns the string truncated of its last n runes.
 func TrimLast(s string, n int) string {
 	r := []rune(s)
+	if len(s) < n {
+		return ""
+	}
 	return string(r[:len(r)-n])
 }
 
@@ -29,4 +32,19 @@ func SwapRuneCase(r rune) rune {
 //
 func SwapCase(s string) string {
 	return strings.Map(SwapRuneCase, s)
+}
+
+//
+// Capitalize return a copy of the input string with the first rune
+// uppercased.
+//
+func Capitalize(s string) string {
+	switch len(s) {
+	case 0:
+		return s
+	case 1:
+		return strings.ToTitle(s)
+	default:
+		return strings.ToTitle(s[0:1]) + s[1:]
+	}
 }
--- util/xstrings/main_test.go
+++ util/xstrings/main_test.go
@@ -6,17 +6,50 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSwapCase(t *testing.T) {
-	t.Run("TrimLast(Foo, 1) => Fo", func(t *testing.T) {
+func TestTrimLast(t *testing.T) {
+	t.Run("TrimLast(F, 2)=>", func(t *testing.T) {
+		assert.Equal(t, TrimLast("F", 2), "")
+	})
+	t.Run("TrimLast(F, 1)=>", func(t *testing.T) {
+		assert.Equal(t, TrimLast("F", 1), "")
+	})
+	t.Run("TrimLast(Foo, 1)=>Fo", func(t *testing.T) {
 		assert.Equal(t, TrimLast("Foo", 1), "Fo")
 	})
-	t.Run("TrimLast(Foo, 2) => Fo", func(t *testing.T) {
+	t.Run("TrimLast(Foo, 2)=>Fo", func(t *testing.T) {
 		assert.Equal(t, TrimLast("Foo", 2), "F")
 	})
-	t.Run("SwapRuneCase(F) => f", func(t *testing.T) {
+}
+
+func TestSwapCase(t *testing.T) {
+	t.Run("SwapRuneCase(F)=>f", func(t *testing.T) {
 		assert.Equal(t, SwapRuneCase('F'), 'f')
 	})
-	t.Run("SwapCase(Foo) => fOO", func(t *testing.T) {
+	t.Run("SwapCase()=>", func(t *testing.T) {
+		assert.Equal(t, SwapCase(""), "")
+	})
+	t.Run("SwapCase(F)=>f", func(t *testing.T) {
+		assert.Equal(t, SwapCase("F"), "f")
+	})
+	t.Run("SwapCase(Foo)=>fOO", func(t *testing.T) {
 		assert.Equal(t, SwapCase("Foo"), "fOO")
+	})
+}
+
+func TestCapitalize(t *testing.T) {
+	t.Run("Capitalize()=>", func(t *testing.T) {
+		assert.Equal(t, Capitalize(""), "")
+	})
+	t.Run("Capitalize(f)=>F", func(t *testing.T) {
+		assert.Equal(t, Capitalize("f"), "F")
+	})
+	t.Run("Capitalize(F)=>F", func(t *testing.T) {
+		assert.Equal(t, Capitalize("F"), "F")
+	})
+	t.Run("Capitalize(foo foo)=>Foo foo", func(t *testing.T) {
+		assert.Equal(t, Capitalize("foo foo"), "Foo foo")
+	})
+	t.Run("Capitalize(FOO foo)=>FOO foo", func(t *testing.T) {
+		assert.Equal(t, Capitalize("FOO foo"), "FOO foo")
 	})
 }
