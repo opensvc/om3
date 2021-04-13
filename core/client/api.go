3ed1a035d3b827a491f3cb810d7dae78d44ad015
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-04-13 10:51:04 +0200

    Normalize core/client/api methods protoype
    No need to expose the client address.

--- core/client/api.go
+++ core/client/api.go
@@ -2,46 +2,46 @@ package client
 
 import "opensvc.com/opensvc/core/client/api"
 
-func (t *T) NewGetDaemonStats() *api.GetDaemonStats {
+func (t T) NewGetDaemonStats() *api.GetDaemonStats {
 	return api.NewGetDaemonStats(t)
 }
 
-func (t *T) NewGetDaemonStatus() *api.GetDaemonStatus {
+func (t T) NewGetDaemonStatus() *api.GetDaemonStatus {
 	return api.NewGetDaemonStatus(t)
 }
 
-func (t *T) NewGetEvents() *api.GetEvents {
+func (t T) NewGetEvents() *api.GetEvents {
 	return api.NewGetEvents(t)
 }
 
-func (t *T) NewGetObjectConfig() *api.GetObjectConfig {
+func (t T) NewGetObjectConfig() *api.GetObjectConfig {
 	return api.NewGetObjectConfig(t)
 }
 
-func (t *T) NewGetObjectSelector() *api.GetObjectSelector {
+func (t T) NewGetObjectSelector() *api.GetObjectSelector {
 	return api.NewGetObjectSelector(t)
 }
 
-func (t *T) NewGetObjectStatus() *api.GetObjectStatus {
+func (t T) NewGetObjectStatus() *api.GetObjectStatus {
 	return api.NewGetObjectStatus(t)
 }
 
-func (t *T) NewPostNodeAction() *api.PostNodeAction {
+func (t T) NewPostNodeAction() *api.PostNodeAction {
 	return api.NewPostNodeAction(t)
 }
 
-func (t *T) NewPostNodeMonitor() *api.PostNodeMonitor {
+func (t T) NewPostNodeMonitor() *api.PostNodeMonitor {
 	return api.NewPostNodeMonitor(t)
 }
 
-func (t *T) NewPostObjectAction() *api.PostObjectAction {
+func (t T) NewPostObjectAction() *api.PostObjectAction {
 	return api.NewPostObjectAction(t)
 }
 
-func (t *T) NewPostObjectCreate() *api.PostObjectCreate {
+func (t T) NewPostObjectCreate() *api.PostObjectCreate {
 	return api.NewPostObjectCreate(t)
 }
 
-func (t *T) NewPostObjectMonitor() *api.PostObjectMonitor {
+func (t T) NewPostObjectMonitor() *api.PostObjectMonitor {
 	return api.NewPostObjectMonitor(t)
 }
