407d0ae3c293f1874acd50bad9346cbdc6a4b1db
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-04-13 10:23:50 +0200

    Fix the daemon_status api test

--- core/client/api/get_daemon_status_test.go
+++ core/client/api/get_daemon_status_test.go
@@ -5,17 +5,18 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"opensvc.com/opensvc/core/client"
+	"opensvc.com/opensvc/core/client/api"
 )
 
 func TestNewGetDaemonStatusHasCustomValues(t *testing.T) {
 	c, _ := client.New(client.WithURL("https://localhost:1215"))
-	a := NewGetDaemonStatus(c).SetSelector("foo").SetNamespace("ns1")
+	a := api.NewGetDaemonStatus(c).SetSelector("foo").SetNamespace("ns1")
 	assert.Equal(t, a.Selector(), "foo")
 	assert.Equal(t, a.Namespace(), "ns1")
 }
 
 func TestNewGetDaemonStatusHasDefaultNullNamespace(t *testing.T) {
 	c, _ := client.New(client.WithURL("https://localhost:1215"))
-	a := NewGetDaemonStatus(c)
+	a := api.NewGetDaemonStatus(c)
 	assert.Equal(t, a.Namespace(), "")
 }
