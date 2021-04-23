b423793e1a70645740fc58bcdcfcee2e0f6d7718
Cyril Galibern <cyril.galibern@opensvc.com>
2021-04-23 17:51:48 +0200

    Update util/fache to use github.com/opensvc/fcache and github.com/opensvc/fcntllock

--- util/fcache/main.go
+++ util/fcache/main.go
@@ -2,31 +2,41 @@
 package fcache
 
 import (
+	"errors"
 	"github.com/opensvc/fcache"
+	"github.com/opensvc/fcntllock"
+	"github.com/opensvc/flock"
 	"opensvc.com/opensvc/config"
-	"opensvc.com/opensvc/util/flock"
 	"opensvc.com/opensvc/util/xsession"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
 var (
-	sessionId       = xsession.Id()
-	cacheDir        = filepath.Join(config.Node.Paths.Var, "cache", sessionId)
 	maxLockDuration = 30 * time.Second
-	lockerP         = flock.New
 )
 
 // Output manage output session function cache
 func Output(o fcache.Outputter, sig string) (out []byte, err error) {
-	return fcache.Output(o, sig, cacheDir, maxLockDuration, outputLockP)
+	return fcache.Output(o, sig, cacheDir(), maxLockDuration, outputLockP)
 }
 
 // Purge session function cache
 func PurgeCache() error {
+	cacheDir := cacheDir()
+	if !strings.Contains(cacheDir, "/cache/") {
+		return errors.New("unexpected cachedir " + cacheDir)
+	}
 	return fcache.Purge(cacheDir)
 }
 
 func outputLockP(name string) fcache.Locker {
-	return lockerP(sessionId + "-out-" + name)
+	sessionId := xsession.Id()
+	path := filepath.Join(config.Node.Paths.Lock, sessionId+"-out-"+name)
+	return flock.New(path, sessionId, fcntllock.New)
+}
+
+func cacheDir() string {
+	return filepath.Join(config.Node.Paths.Cache, xsession.Id())
 }
