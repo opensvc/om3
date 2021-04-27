6253b2d88db28ab3e435c189b868a1c2190b5e9a
Cyril Galibern <cyril.galibern@opensvc.com>
2021-04-27 11:57:30 +0200

    Implement human renderer for node capabilities commands

--- core/object/node_capabilities.go
+++ core/object/node_capabilities.go
@@ -4,11 +4,25 @@ import (
 	"opensvc.com/opensvc/util/capabilities"
 )
 
+type (
+	// NodeCapabilities contain the current node capabilities
+	NodeCapabilities []string
+)
+
+// Render is a human rendered for node capabilities
+func (t NodeCapabilities) Render() string {
+	s := ""
+	for _, c := range t {
+		s = s + c + "\n"
+	}
+	return s
+}
+
 // NodeScanCapabilities scan node capabilities and return new capabilities
 func (t Node) NodeScanCapabilities() (interface{}, error) {
 	err := capabilities.Scan()
 	if err != nil {
 		return nil, err
 	}
-	return capabilities.Data(), nil
+	return NodeCapabilities(capabilities.Data()), nil
 }
