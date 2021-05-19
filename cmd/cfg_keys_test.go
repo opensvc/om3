3ba4a66052a142e3e7a3ee05fa91fc1d63e05b3a
Cyril Galibern <cyril.galibern@opensvc.com>
2021-05-19 10:47:18 +0200

    [Test] Add checks on 'om cfg -s path keys'
    === RUN   TestCfgKeys/json
        --- PASS: TestCfgKeys/json (0.01s)
            cfg_keys_test.go:56: run 'om cfg -s test/cfg/cfg1 keys --format json'
    === RUN   TestCfgKeys/--match
        --- PASS: TestCfgKeys/--match (0.01s)
            cfg_keys_test.go:56: run 'om cfg -s test/cfg/cfg1 keys --match **/foo*'
    === RUN   TestCfgKeys/keys
        --- PASS: TestCfgKeys/keys (0.01s)
            cfg_keys_test.go:56: run 'om cfg -s test/cfg/cfg1 keys'

---
+++ cmd/cfg_keys_test.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"encoding/json"
+	"github.com/opensvc/testhelper"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"opensvc.com/opensvc/config"
+	"opensvc.com/opensvc/test_conf_helper"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+type (
+	jsonOutput struct {
+		Nodename string   `json:"nodename"`
+		Path     string   `json:"path"`
+		Data     []string `json:"data"`
+	}
+)
+
+func TestCfgKeys(t *testing.T) {
+	td, cleanup := testhelper.Tempdir(t)
+	defer cleanup()
+
+	config.Load(map[string]string{"osvc_root_path": td})
+	defer config.Load(map[string]string{})
+
+	test_conf_helper.InstallSvcFile(t, "cfg1.conf", filepath.Join(td, "etc", "namespaces", "test", "cfg", "cfg1.conf"))
+
+	cases := map[string]struct {
+		extraArgs       []string
+		expectedResults string
+	}{
+		"--match": {[]string{"--match", "**/foo*"}, "foo/foo1\nfoo/foo2\n"},
+		"keys":    {[]string{}, "foo/bar\nfoo/foo1\nfoo/foo2\nbar/bar1\n"},
+		"json":    {[]string{"--format", "json"}, "foo/bar\nfoo/foo1\nfoo/foo2\nbar/bar1"},
+	}
+
+	getCmd := func(name string) []string {
+		args := []string{"cfg", "-s", "test/cfg/cfg1", "keys"}
+		args = append(args, cases[name].extraArgs...)
+		return args
+	}
+
+	if name, ok := os.LookupEnv("TC_NAME"); ok == true {
+		rootCmd.SetArgs(getCmd(name))
+		_ = rootCmd.Execute()
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			t.Logf("run 'om %v'", strings.Join(getCmd(name), " "))
+			cmd := exec.Command(os.Args[0], "-test.run=TestCfgKeys")
+			cmd.Env = append(os.Environ(), "TC_NAME="+name)
+			out, err := cmd.Output()
+			require.Nil(t, err)
+			if strings.Contains(name, "json") {
+				var response []jsonOutput
+				err := json.Unmarshal(out, &response)
+				require.Nil(t, err)
+				assert.Equalf(t, strings.Split(tc.expectedResults, "\n"), response[0].Data, string(out))
+			} else {
+				assert.Equal(t, tc.expectedResults, string(out))
+			}
+		})
+	}
+}
