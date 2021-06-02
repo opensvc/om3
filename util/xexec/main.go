9294901c483250cad14f2a06565fa0c81e3bb829
Cyril Galibern <cyril.galibern@opensvc.com>
2021-06-02 12:45:38 +0200

    [util/xexec] Implement CommandFromLimits

--- util/xexec/main.go
+++ util/xexec/main.go
@@ -2,6 +2,7 @@ package xexec
 
 import (
 	"github.com/anmitsu/go-shlex"
+	"opensvc.com/opensvc/util/limits"
 	"os/exec"
 	"strings"
 )
@@ -32,4 +33,14 @@ func CommandFromString(s string) (*exec.Cmd, error) {
 		return nil, nil
 	}
 	return exec.Command(sSplit[0], sSplit[1:]...), nil
+}
+
+// CommandFromLimits wrapper to exec.Command from a string command 's'
+// and 'l' limits.T
+func CommandFromLimits(l limits.T, s string) (*exec.Cmd, error) {
+	limitCommands := shLimitCommands(l)
+	if len(limitCommands) > 0 {
+		s = strings.Join(limitCommands, " && ") + " && " + s
+	}
+	return CommandFromString(s)
 }
---
+++ util/xexec/sh_test.go
@@ -0,0 +1,64 @@
+package xexec
+
+import (
+	"github.com/stretchr/testify/assert"
+	"opensvc.com/opensvc/util/limits"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func TestT_shLimitCommands(t *testing.T) {
+	var LimitNProcCommand string
+	if runtime.GOOS == "darwin" {
+		LimitNProcCommand = "ulimit -u 8"
+	} else {
+		LimitNProcCommand = "ulimit -p 8"
+	}
+	cases := map[string]struct {
+		limit    limits.T
+		commands []string
+	}{
+		"null": {
+			limits.T{},
+			[]string{},
+		},
+		"limit_nofile_64": {
+			limits.T{LimitNoFile: 64},
+			[]string{"ulimit -n 64"},
+		},
+		"all_limits": {
+			limits.T{
+				LimitAs:      1 * 1000,
+				LimitCpu:     2 * time.Hour,
+				LimitCore:    3 * 512,
+				LimitData:    4 * 1000,
+				LimitFSize:   5 * 512,
+				LimitMemLock: 6 * 1000,
+				LimitNoFile:  7,
+				LimitNProc:   8,
+				LimitRss:     9 * 1000,
+				LimitStack:   10 * 1000,
+				LimitVMem:    11 * 1000,
+			},
+			[]string{
+				"ulimit -n 7",
+				"ulimit -s 10",
+				"ulimit -l 6",
+				"ulimit -v 11",
+				"ulimit -t 7200",
+				"ulimit -c 3",
+				"ulimit -d 4",
+				"ulimit -f 5",
+				"ulimit -m 9",
+				LimitNProcCommand,
+			},
+		},
+	}
+	for name := range cases {
+		t.Run(name, func(t *testing.T) {
+			commands := shLimitCommands(cases[name].limit)
+			assert.ElementsMatch(t, cases[name].commands, commands)
+		})
+	}
+}
