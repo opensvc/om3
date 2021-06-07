d43679ce86e80a601f229d6bf98981f9c7f7afef
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-06-07 15:12:10 +0200

    Add User and Group converters

---
+++ util/converters/user.go
@@ -0,0 +1,48 @@
+package converters
+
+import (
+	"os/user"
+	"strconv"
+)
+
+type (
+	TUser  string
+	TGroup string
+)
+
+var (
+	User  TUser
+	Group TGroup
+)
+
+//
+func (t TUser) Convert(s string) (interface{}, error) {
+	return t.convert(s)
+}
+
+func (t TUser) convert(s string) (*user.User, error) {
+	if _, err := strconv.Atoi(s); err == nil {
+		return user.LookupId(s)
+	}
+	return user.Lookup(s)
+}
+
+func (t TUser) String() string {
+	return "user"
+}
+
+//
+func (t TGroup) Convert(s string) (interface{}, error) {
+	return t.convert(s)
+}
+
+func (t TGroup) convert(s string) (*user.Group, error) {
+	if _, err := strconv.Atoi(s); err == nil {
+		return user.LookupGroupId(s)
+	}
+	return user.LookupGroup(s)
+}
+
+func (t TGroup) String() string {
+	return "group"
+}
