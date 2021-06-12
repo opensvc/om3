002c17ac473680503aec80c565b4f8236cc45bac
Cyril Galibern <cyril.galibern@opensvc.com>
2021-06-12 09:19:07 +0200

    Disbale 'start_timeout' kw on app.type simple

--- drivers/resapp/base_keywords.go
+++ drivers/resapp/base_keywords.go
@@ -19,18 +19,6 @@ var (
 			Example: "180",
 		},
 		{
-			Option:    "start_timeout",
-			Attr:      "StartTimeout",
-			Converter: converters.Duration,
-			Scopable:  true,
-			Text: "Wait for <duration> before declaring the app launcher start action a failure." +
-				"  Takes precedence over :kw:`timeout`. If neither :kw:`timeout` nor :kw:`start_timeout` is set," +
-				" the agent waits indefinitely for the app launcher to return." +
-				" A timeout can be coupled with :kw:`optional=true to not abort a service instance start when an app" +
-				" launcher did not return.",
-			Example: "180",
-		},
-		{
 			Option:    "stop_timeout",
 			Attr:      "StopTimeout",
 			Converter: converters.Duration,
