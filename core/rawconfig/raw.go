905050d2c613ed0e15940c50e188688a046fe62b
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-07-02 16:48:30 +0200

    Add more renderers for the "print config --eval"

--- core/rawconfig/raw.go
+++ core/rawconfig/raw.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
+	"github.com/golang-collections/collections/set"
 	"github.com/iancoleman/orderedmap"
 )
 
@@ -75,11 +77,42 @@ func renderComment(k string, v interface{}) string {
 
 func renderKey(k string, v interface{}) string {
 	k = RegexpScope.ReplaceAllString(k, Node.Colorize.Error("$1"))
-	vs, ok := v.(string)
-	if ok {
-		vs = RegexpReference.ReplaceAllString(vs, Node.Colorize.Optimal("$1"))
+	var vs string
+	type stringer interface {
+		String() string
+	}
+	switch o := v.(type) {
+	case []interface{}:
+		l := make([]string, 0)
+		for _, e := range o {
+			if s, ok := e.(string); ok {
+				l = append(l, s)
+			}
+		}
+		vs = strings.Join(l, " ")
+	case *set.Set:
+		l := make([]string, 0)
+		o.Do(func(e interface{}) {
+			if s, ok := e.(string); ok {
+				l = append(l, s)
+			}
+		})
+		vs = strings.Join(l, " ")
+	case []string:
+		vs = strings.Join(o, " ")
+	case float64:
+		vs = fmt.Sprintf("%f", o)
+	case int, uint, int8, uint8, int64, uint64:
+		vs = fmt.Sprintf("%d", o)
+	case bool:
+		vs = strconv.FormatBool(o)
+	case string:
+		vs = RegexpReference.ReplaceAllString(o, Node.Colorize.Optimal("$1"))
 		vs = strings.ReplaceAll(vs, "\n", "\n\t")
-	} else {
+	case stringer:
+		vs = o.String()
+	default:
+		//fmt.Println(o, reflect.TypeOf(o))
 		vs = ""
 	}
 	return fmt.Sprintf("%s = %s\n", Node.Colorize.Secondary(k), vs)
