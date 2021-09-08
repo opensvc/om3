7cb3253fd51dca0f935a4bf13d6a12facb381527
Christophe Varoqui <christophe.varoqui@opensvc.com>
2021-09-08 10:51:41 +0200

    Add a Equal() function to util/stringslice

--- util/stringslice/main.go
+++ util/stringslice/main.go
@@ -1,5 +1,6 @@
 package stringslice
 
+// Has returns true if any string in l is s
 func Has(s string, l []string) bool {
 	for _, e := range l {
 		if e == s {
@@ -7,4 +8,17 @@ func Has(s string, l []string) bool {
 		}
 	}
 	return false
+}
+
+// Equal returns a boolean reporting whether a == b
+func Equal(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
