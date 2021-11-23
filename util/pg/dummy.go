30298d5290331806ca6220f001d5b1865b249457
Cyril Galibern <cyril.galibern@opensvc.com>
2021-11-23 17:29:03 +0100

    [util/pg] Enable Solaris build

---
+++ util/pg/dummy.go
@@ -0,0 +1,12 @@
+// +build !linux
+
+package pg
+
+// ApplyProc creates the cgroup, set caps, and add the specified process
+func (c Config) ApplyProc(pid int) error {
+	return nil
+}
+
+func (c Config) Delete() (bool, error) {
+	return false, nil
+}
