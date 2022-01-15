715d4e5bdf9672e5095a6ffda734de7618f14a59
Cyril Galibern <cyril.galibern@opensvc.com>
2022-01-15 15:24:02 +0100

    [test] Add more checks to daemon/daemoncli

--- daemon/daemoncli/main_test.go
+++ daemon/daemoncli/main_test.go
@@ -16,3 +16,36 @@ func TestDaemonStartThenStop(t *testing.T) {
 	require.Nil(t, Stop())
 	require.False(t, Running())
 }
+
+func TestDaemonStartRestartThenStop(t *testing.T) {
+	require.False(t, Running())
+	go func() {
+		require.Nil(t, Start())
+	}()
+	require.Nil(t, WaitRunning())
+	require.True(t, Running())
+	go func() {
+		require.Nil(t, ReStart())
+	}()
+	require.Nil(t, WaitRunning())
+	require.True(t, Running())
+	require.Nil(t, Stop())
+	require.False(t, Running())
+}
+
+func TestDaemonReStartThenStop(t *testing.T) {
+	require.False(t, Running())
+	go func() {
+		require.Nil(t, ReStart())
+	}()
+	require.Nil(t, WaitRunning())
+	require.True(t, Running())
+	require.Nil(t, Stop())
+	require.False(t, Running())
+}
+
+func TestStop(t *testing.T) {
+	require.False(t, Running())
+	require.Nil(t, Stop())
+	require.False(t, Running())
+}
