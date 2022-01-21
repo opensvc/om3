a700c84da7a05be41671635fd02e5d0705591de6
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-01-21 13:39:21 +0100

    Reorg core/network/setup.go to prepare for more setup actions

--- core/network/setup.go
+++ core/network/setup.go
@@ -21,31 +21,47 @@ var (
 )
 
 func Setup(n *object.Node) error {
-	dir, err := n.CNIConfig()
+	dir, err := mkCNIConfigDir(n)
 	if err != nil {
 		return err
 	}
-	if !file.Exists(dir) {
-		if err := os.MkdirAll(dir, 0600); err != nil {
-			return err
-		}
-	}
 	for _, nw := range Networks(n) {
-		if !nw.IsValid() {
-			n.Log().Info().
-				Str("name", nw.Name()).
-				Str("network", nw.Network()).
-				Msgf("skip setup of invalid network")
-			continue
-		}
-		if err := Create(n, nw, dir); err != nil {
+		if err := setupNetwork(n, nw, dir); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func Create(n *object.Node, nw Networker, dir string) error {
+func setupNetwork(n *object.Node, nw Networker, dir string) error {
+	if !nw.IsValid() {
+		n.Log().Info().
+			Str("name", nw.Name()).
+			Str("network", nw.Network()).
+			Msgf("skip setup of invalid network")
+		return nil
+	}
+	if err := setupNetworkCNI(n, nw, dir); err != nil {
+		return err
+	}
+	return nil
+}
+
+func mkCNIConfigDir(n *object.Node) (string, error) {
+	dir, err := n.CNIConfig()
+	if err != nil {
+		return dir, err
+	}
+	if file.Exists(dir) {
+		return dir, nil
+	}
+	if err := os.MkdirAll(dir, 0600); err != nil {
+		return dir, err
+	}
+	return dir, nil
+}
+
+func setupNetworkCNI(n *object.Node, nw Networker, dir string) error {
 	p := filepath.Join(dir, nw.Name()+".conf")
 	if file.Exists(p) {
 		n.Log().Info().Msgf("preserve %s", p)
