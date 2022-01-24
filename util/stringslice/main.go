38934c600dcde196337c7e500a0dfc56d8cc9c15
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-01-24 16:41:05 +0100

    Add the "Index(string) int"  func to stringslice

--- util/stringslice/main.go
+++ util/stringslice/main.go
@@ -10,6 +10,17 @@ func Has(s string, l []string) bool {
 	return false
 }
 
+// Index returns the index of the s element in l.
+// If s is not present in l, return -1.
+func Index(s string, l []string) int {
+	for i, e := range l {
+		if e == s {
+			return i
+		}
+	}
+	return -1
+}
+
 // Equal returns a boolean reporting whether a == b
 func Equal(a, b []string) bool {
 	if len(a) != len(b) {
