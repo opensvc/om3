337456f306ec17ce8c52501eba16d6ce3514ef7d
Cyril Galibern <cyril.galibern@opensvc.com>
2022-02-04 17:25:47 +0100

    [daemon/listener] Enable sub daemon https listener on 1225

--- daemon/listener/main.go
+++ daemon/listener/main.go
@@ -1,12 +1,14 @@
 package listener
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/rs/zerolog"
 
 	"opensvc.com/opensvc/daemon/enable"
-	"opensvc.com/opensvc/daemon/listener/lsnrraw"
+	"opensvc.com/opensvc/daemon/listener/lsnrhttp"
+	"opensvc.com/opensvc/daemon/listener/mux/httpmux"
 	"opensvc.com/opensvc/daemon/routinehelper"
 	"opensvc.com/opensvc/daemon/subdaemon"
 	"opensvc.com/opensvc/util/funcopt"
@@ -21,6 +23,7 @@ type (
 		loopEnabled  *enable.T
 		routineTrace routineTracer
 		rootDaemon   subdaemon.RootManager
+		httpHandler  http.Handler
 		routinehelper.TT
 	}
 	action struct {
@@ -45,6 +48,17 @@ var (
 				return lsnrraw.New(
 					lsnrraw.WithRoutineTracer(&t.TT),
 					lsnrraw.WithRootDaemon(t.rootDaemon),
+				)
+			},
+		},
+		"listenerHttp": {
+			new: func(t *T) subdaemon.Manager {
+				return lsnrhttp.New(
+					lsnrhttp.WithRoutineTracer(&t.TT),
+					lsnrhttp.WithHandler(t.httpHandler),
+					lsnrhttp.WithAddr(":1225"),
+					lsnrhttp.WithCertFile("/tmp/certificate_chain"),
+					lsnrhttp.WithKeyFile("/tmp/private_key"),
 				)
 			},
 		},
@@ -78,6 +92,7 @@ func (t *T) MainStart() error {
 		defer t.Trace(t.Name() + "-loop")()
 		t.loop(started)
 	}()
+	t.httpHandler = httpmux.New(t.log, t.rootDaemon)
 	for subName, sub := range mandatorySubs {
 		sub.subActions = sub.new(t)
 		if err := sub.subActions.Init(); err != nil {
