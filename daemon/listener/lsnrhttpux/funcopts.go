07dfed338d5b58c2fcc5a042702c96596f138e08
Cyril Galibern <cyril.galibern@opensvc.com>
2022-02-10 18:01:39 +0100

    [daemon/listerner/lsnrhttpux] Implement listener http on ux socket

---
+++ daemon/listener/lsnrhttpux/funcopts.go
@@ -0,0 +1,48 @@
+package lsnrhttpux
+
+import (
+	"net/http"
+
+	"opensvc.com/opensvc/daemon/routinehelper"
+	"opensvc.com/opensvc/util/funcopt"
+)
+
+func WithRoutineTracer(o routinehelper.Tracer) funcopt.O {
+	return funcopt.F(func(i interface{}) error {
+		t := i.(*T)
+		t.SetTracer(o)
+		return nil
+	})
+}
+
+func WithHandler(o http.Handler) funcopt.O {
+	return funcopt.F(func(i interface{}) error {
+		t := i.(*T)
+		t.handler = o
+		return nil
+	})
+}
+
+func WithAddr(o string) funcopt.O {
+	return funcopt.F(func(i interface{}) error {
+		t := i.(*T)
+		t.addr = o
+		return nil
+	})
+}
+
+func WithCertFile(o string) funcopt.O {
+	return funcopt.F(func(i interface{}) error {
+		t := i.(*T)
+		t.certFile = o
+		return nil
+	})
+}
+
+func WithKeyFile(o string) funcopt.O {
+	return funcopt.F(func(i interface{}) error {
+		t := i.(*T)
+		t.keyFile = o
+		return nil
+	})
+}
