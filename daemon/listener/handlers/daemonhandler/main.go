c398ee8868e5d5c3dd63502ec07f616ca536cc15
Cyril Galibern <cyril.galibern@opensvc.com>
2022-02-10 18:17:25 +0100

    daemonhandlers add Events for demo

--- daemon/listener/handlers/daemonhandler/main.go
+++ daemon/listener/handlers/daemonhandler/main.go
@@ -4,9 +4,13 @@
 package daemonhandler
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
+	"opensvc.com/opensvc/core/event"
 	"opensvc.com/opensvc/daemon/listener/mux/muxctx"
+	"opensvc.com/opensvc/util/timestamp"
 )
 
 func Running(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +45,58 @@ func Stop(w http.ResponseWriter, r *http.Request) {
 		logger.Info().Msg(msg)
 		_, _ = write(w, r, funcName, []byte(msg))
 	}
+}
+
+func Events(w http.ResponseWriter, r *http.Request) {
+	funcName := "daemonhandler.Events"
+	logger := muxctx.Logger(r.Context()).With().Str("func", funcName).Logger()
+	logger.Debug().Msg("starting")
+	var httpBody bool
+	if r.Header.Get("accept") == "text/event-stream" {
+		httpBody = true
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-control", "no-store")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Transfer-Encoding", "chunked")
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	for i := 0; i < 3; i++ {
+		rawMsg := json.RawMessage("\"demo msg xxx\"")
+		ev := event.Event{
+			Kind:      "demo",
+			ID:        uint64(i),
+			Timestamp: timestamp.Now(),
+			Data:      &rawMsg,
+		}
+		b, err := json.Marshal(ev)
+		if err != nil {
+			logger.Error().Err(err).Interface("event", ev).Msg("Marshal")
+			return
+		}
+		logger.Debug().Msgf("send fragment: %#v", ev)
+
+		var endMsg, msg []byte
+		if httpBody {
+			endMsg = []byte("\n\n")
+			msg = append([]byte("data: "), b...)
+		} else {
+			endMsg = []byte("\n\n\x00")
+			msg = append([]byte(""), b...)
+		}
+
+		msg = append(msg, endMsg...)
+		if _, err := write(w, r, funcName, msg); err != nil {
+			logger.Error().Err(err).Msg("write failure")
+			return
+		}
+		if f, ok := w.(http.Flusher); ok {
+			f.Flush()
+		}
+		time.Sleep(1000 * time.Millisecond)
+	}
+	logger.Debug().Msg("done")
 }
 
 func write(w http.ResponseWriter, r *http.Request, funcName string, b []byte) (int, error) {
