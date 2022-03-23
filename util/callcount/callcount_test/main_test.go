008c184997b2f303a18d54f0c45924521b297949
Cyril Galibern <cyril.galibern@opensvc.com>
2022-03-23 22:29:07 +0100

    Implement pkg util/callcount to count some daemon calls
    	Package callcount provides call count watcher
    
    	Example:
    		import "callcount"
    		mapping := map[int]string{
    			1: "operation 1",
    			2: "operation 2",
    			3: "operation 3",
    		}
    		c, cancel := callcount.Start(context.Background(), mapping)
    		defer cancel() stop the counter
    		c <- 1 // register func with id 1 as been called
    		c <- 2 // register func with id 2 as been called
    		c <- 1 // register func with id 1 as been called
    		c <- 8 // register func with id 8 has been called (undefined in mapping)
    
    		Get(c) // return Counts{1: 2, 2:1, 8:1}
    		GetStats(c) // return Stats{"operation 1": 1, "operation 2":1, "unknown":1}
    		Reset(c) // resets current counters

---
+++ util/callcount/callcount_test/main_test.go
@@ -0,0 +1,48 @@
+package callcount_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"opensvc.com/opensvc/util/callcount"
+)
+
+func TestCounter(t *testing.T) {
+	mapping := map[int]string{
+		1: "operation 1",
+		2: "operation 2",
+		3: "operation 3",
+	}
+	c, cancel := callcount.Start(context.Background(), mapping)
+	defer cancel()
+	for _, i := range []int{1, 1, 2, 8, 9, 5, 9} {
+		c <- i
+	}
+	expectedCounts := callcount.Counts{
+		1: 2,
+		2: 1,
+		8: 1,
+		5: 1,
+		9: 2,
+	}
+	expectedStats := callcount.Stats{
+		"operation 1": 2,
+		"operation 2": 1,
+		"unknown":     4,
+	}
+
+	zeroCounts := callcount.Counts{}
+	require.Equal(t, expectedCounts, callcount.Get(c))
+	require.Equal(t, expectedStats, callcount.GetStats(c))
+	callcount.Reset(c)
+	require.Equal(t, zeroCounts, callcount.Get(c))
+	require.Equal(t, zeroCounts, callcount.Get(c))
+
+	c <- 9
+	require.Equal(t, callcount.Counts{9: 1}, callcount.Get(c))
+
+	callcount.Reset(c)
+	require.Equal(t, zeroCounts, callcount.Get(c))
+}
