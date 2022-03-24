b3aff160ff75077560869cf9e00a4d2552cfb5e3
Cyril Galibern <cyril.galibern@opensvc.com>
2022-03-24 13:39:02 +0100

    [core/hbtype] Implement New to create a *Msg from []byte and nodename
    This will is required to allow handle of b2.1 hb full msg

--- core/hbtype/main.go
+++ core/hbtype/main.go
@@ -2,6 +2,8 @@
 package hbtype
 
 import (
+	"encoding/json"
+
 	"opensvc.com/opensvc/core/cluster"
 	"opensvc.com/opensvc/util/jsondelta"
 	"opensvc.com/opensvc/util/timestamp"
@@ -34,3 +36,66 @@ type (
 		Id() string
 	}
 )
+
+// New create new Msg from b and nodename
+func New(b []byte, nodename string) (*Msg, error) {
+	// TODO use full in Msg once 2.1 not anymore needed
+	//msg := &Msg{}
+	//if err := json.Unmarshal(b, msg); err != nil {
+	//	return nil, err
+	//}
+	msgI := make(map[string]interface{})
+	var msg *Msg
+	if err := json.Unmarshal(b, &msgI); err != nil {
+		return nil, err
+	}
+	if _, ok := msgI["kind"]; ok {
+		msg = &Msg{
+			Nodename: nodename,
+		}
+		for k, v := range msgI {
+			tmp, err := json.Marshal(v)
+			if err != nil {
+				return nil, err
+			}
+			switch k {
+			case "kind":
+				if err := json.Unmarshal(tmp, &msg.Kind); err != nil {
+					return nil, err
+				}
+			case "compat":
+				if err := json.Unmarshal(tmp, &msg.Compat); err != nil {
+					return nil, err
+				}
+			case "gen":
+				if err := json.Unmarshal(tmp, &msg.Gen); err != nil {
+					return nil, err
+				}
+			case "updated":
+				if err := json.Unmarshal(tmp, &msg.Updated); err != nil {
+					return nil, err
+				}
+			case "ping":
+				if err := json.Unmarshal(tmp, &msg.Ping); err != nil {
+					return nil, err
+				}
+			case "deltas":
+				if err := json.Unmarshal(tmp, &msg.Deltas); err != nil {
+					return nil, err
+				}
+			}
+		}
+	} else {
+		full := &cluster.NodeStatus{}
+		if err := json.Unmarshal(b, &full); err != nil {
+			return nil, err
+		}
+		msg = &Msg{
+			Kind:     "full",
+			Gen:      full.Gen,
+			Full:     *full,
+			Nodename: nodename,
+		}
+	}
+	return msg, nil
+}
