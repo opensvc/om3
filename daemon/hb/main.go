b84d05aed8448ef55f85a2b9b706c15856bdbe6d
Cyril Galibern <cyril.galibern@opensvc.com>
2022-03-25 14:48:14 +0100

    [demo] Send patch to remotes

--- daemon/hb/main.go
+++ daemon/hb/main.go
@@ -4,21 +4,22 @@ import (
 	"context"
 	"encoding/json"
 	"runtime"
+	"strconv"
 	"time"
 
 	"github.com/rs/zerolog"
 
-	"opensvc.com/opensvc/core/cluster"
 	"opensvc.com/opensvc/core/clusterhb"
 	"opensvc.com/opensvc/core/hbtype"
 	"opensvc.com/opensvc/daemon/daemonctx"
 	"opensvc.com/opensvc/daemon/daemondatactx"
 	"opensvc.com/opensvc/daemon/hb/hbctrl"
-	"opensvc.com/opensvc/util/hostname"
-
 	"opensvc.com/opensvc/daemon/routinehelper"
 	"opensvc.com/opensvc/daemon/subdaemon"
 	"opensvc.com/opensvc/util/funcopt"
+	"opensvc.com/opensvc/util/hostname"
+	"opensvc.com/opensvc/util/jsondelta"
+	"opensvc.com/opensvc/util/timestamp"
 )
 
 type (
@@ -55,30 +56,6 @@ func New(opts ...funcopt.O) *T {
 	t.txs = make(map[string]hbtype.Transmitter)
 	t.rxs = make(map[string]hbtype.Receiver)
 	return t
-}
-
-// pingMsg function is for demo
-func pingMsg(gen map[string]uint64) ([]byte, error) {
-	msg := hbtype.Msg{
-		Kind:     "ping",
-		Nodename: hostname.Hostname(),
-		Gen:      gen,
-	}
-	return json.Marshal(msg)
-}
-
-// fullMsg function is for demo
-func fullMsg(nodeStatus *cluster.NodeStatus) ([]byte, error) {
-	// TODO for 3
-	//msg := hbtype.Msg{
-	//	Kind:     "full",
-	//	Nodename: hostname.Hostname(),
-	//	Full:     *nodeStatus,
-	//	Gen:      nodeStatus.Gen,
-	//}
-	//return json.Marshal(msg)
-	// For b2.1
-	return json.Marshal(*nodeStatus)
 }
 
 func (t *T) MainStart() error {
@@ -132,33 +109,14 @@ func (t *T) start(ctx context.Context, data *hbctrl.T, msgC chan *hbtype.Msg) er
 			}
 		}
 	}()
-	go func() {
-		// for demo loop on sending ping messages
-		dataBus := daemondatactx.DaemonData(t.Ctx)
-		for {
-			gen := dataBus.GetLocalNodeStatus().Gen
-			d, err := pingMsg(gen)
-			if err != nil {
-				return
-			}
-			dataC <- d
-			time.Sleep(time.Second)
-		}
-	}()
-	go func() {
-		// for demo loop on sending full messages
-		dataBus := daemondatactx.DaemonData(t.Ctx)
-		for {
-			nodeStatus := dataBus.GetLocalNodeStatus()
-			d, err := fullMsg(nodeStatus)
-			if err != nil {
-				t.log.Debug().Err(err).Msg("create fullMsg")
-				return
-			}
-			dataC <- d
-			time.Sleep(5 * time.Second)
-		}
-	}()
+
+	// For demo
+	go t.sendPing(dataC, 1, 5*time.Second)
+	time.Sleep(2 * time.Second)
+	go t.sendFull(dataC, 1, 5*time.Second)
+	time.Sleep(2 * time.Second)
+	go t.sendPatch(dataC, 100000000, 1*time.Second)
+
 	go func() {
 		// for demo handle received messages
 		count := 0.0
@@ -178,7 +136,10 @@ func (t *T) start(ctx context.Context, data *hbctrl.T, msgC chan *hbtype.Msg) er
 				case "full":
 					dataBus.ApplyFull(msg.Nodename, &msg.Full)
 				case "patch":
-					dataBus.ApplyPatch(msg.Nodename, msg)
+					err := dataBus.ApplyPatch(msg.Nodename, msg)
+					if err != nil {
+						t.log.Error().Err(err).Msgf("ApplyPatch %s from %s gens: %v", msg.Kind, msg.Nodename, msg.Gen)
+					}
 				}
 				count++
 			}
@@ -203,4 +164,91 @@ func (t *T) MainStop() error {
 	}
 	t.log.Info().Msg("mgr stopped")
 	return nil
+}
+
+func (t *T) sendPing(dataC chan<- []byte, count int, interval time.Duration) {
+	// for demo loop on sending ping messages
+	dataBus := daemondatactx.DaemonData(t.Ctx)
+	for i := 0; i < count; i++ {
+		nodeStatus := dataBus.GetLocalNodeStatus()
+		msg := hbtype.Msg{
+			Kind:     "ping",
+			Nodename: hostname.Hostname(),
+			Gen:      nodeStatus.Gen,
+		}
+		d, err := json.Marshal(msg)
+		if err != nil {
+			return
+		}
+		t.log.Error().Msgf("send ping message %v", nodeStatus.Gen)
+		dataC <- d
+		time.Sleep(interval)
+	}
+}
+
+func (t *T) sendFull(dataC chan<- []byte, count int, interval time.Duration) {
+	// for demo loop on sending full messages
+	dataBus := daemondatactx.DaemonData(t.Ctx)
+	for i := 0; i < count; i++ {
+		nodeStatus := dataBus.GetLocalNodeStatus()
+		// TODO for 3
+		//msg := hbtype.Msg{
+		//	Kind:     "full",
+		//	Nodename: hostname.Hostname(),
+		//	Full:     *nodeStatus,
+		//	Gen:      nodeStatus.Gen,
+		//}
+		//return json.Marshal(msg)
+		// For b2.1
+		d, err := json.Marshal(*nodeStatus)
+		if err != nil {
+			t.log.Debug().Err(err).Msg("create fullMsg")
+			return
+		}
+		t.log.Error().Msgf("send fullMsg %v", nodeStatus.Gen)
+		dataC <- d
+		time.Sleep(interval)
+	}
+}
+
+func (t *T) sendPatch(dataC chan<- []byte, count int, interval time.Duration) {
+	// for demo loop on sending patch messages
+	dataBus := daemondatactx.DaemonData(t.Ctx)
+	localhost := hostname.Hostname()
+	for i := 0; i < count; i++ {
+		ops := make([]jsondelta.Operation, 0)
+		dataBus.CommitPending()
+		localNodeStatus := dataBus.GetLocalNodeStatus()
+		newGen := localNodeStatus.Gen[localhost]
+		newGen++
+		localNodeStatus.Gen[localhost] = newGen
+		ops = append(ops, jsondelta.Operation{
+			OpPath:  []interface{}{"gen", localhost},
+			OpValue: jsondelta.NewOptValue(newGen),
+			OpKind:  "replace",
+		})
+		ops = append(ops, jsondelta.Operation{
+			OpPath:  []interface{}{"updated"},
+			OpValue: jsondelta.NewOptValue(timestamp.Now()),
+			OpKind:  "replace",
+		})
+		patch := hbtype.Msg{
+			Kind: "patch",
+			Gen:  localNodeStatus.Gen,
+			Deltas: map[string]jsondelta.Patch{
+				strconv.FormatUint(newGen, 10): ops,
+			},
+			Nodename: localhost,
+		}
+		err := dataBus.ApplyPatch(localhost, &patch)
+		if err != nil {
+			t.log.Error().Err(err).Msgf("ApplyPatch node gen %d", newGen)
+		}
+		dataBus.CommitPending()
+		if b, err := json.Marshal(patch); err == nil {
+			t.log.Debug().Msgf("Send new patch %d: %s", newGen, b)
+			dataC <- b
+		}
+		time.Sleep(interval)
+	}
 }
