48d5b5f187c8de4fe46aea74cf11582ff9397e09
Cyril Galibern <cyril.galibern@opensvc.com>
2022-04-05 19:01:06 +0200

    [daemon/daemondata] PushOps append ops to T.pendingOps

--- daemon/daemondata/ids.go
+++ daemon/daemondata/ids.go
@@ -2,26 +2,28 @@ package daemondata
 
 const (
 	idUndef              = iota
-	idStats              = iota
-	idGetHbMessage       = iota
-	idGetLocalNodeStatus = iota
-	idGetStatus          = iota
 	idApplyFull          = iota
 	idApplyPatch         = iota
 	idApplyPing          = iota
 	idCommitPending      = iota
+	idGetHbMessage       = iota
+	idGetLocalNodeStatus = iota
+	idGetStatus          = iota
+	idPushOps            = iota
+	idStats              = iota
 )
 
 var (
 	idToName = map[int]string{
 		idUndef:              "undef",
-		idStats:              "stats",
-		idGetHbMessage:       "get-hb-message",
-		idGetLocalNodeStatus: "get-local-node-status",
-		idGetStatus:          "get-status",
 		idApplyFull:          "apply-full",
 		idApplyPatch:         "apply-patch",
 		idApplyPing:          "apply-ping",
 		idCommitPending:      "commit-pending",
+		idGetHbMessage:       "get-hb-message",
+		idGetLocalNodeStatus: "get-local-node-status",
+		idGetStatus:          "get-status",
+		idPushOps:            "push-ops",
+		idStats:              "stats",
 	}
 )
