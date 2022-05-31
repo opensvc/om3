f443596540086fd3ea411e97b0589a4c997bd3cd
Cyril Galibern <cyril.galibern@opensvc.com>
2022-05-31 22:08:01 +0200

    [daemon/daemonps] Add helpers functions for daemon objects
    o PubCfgDelete
    o PubCfgUpdate
    o SubCfg
    
    o PubInstStatusDelete
    o PubInstStatusUpdated
    o SubInstStatus
    
    o PubSmonDelete
    o PubSmonUpdated
    o SubSmon
    
    o PubSetSmonUpdated
    o SubSetSmon
    
    o PubSvcAggDelete
    o PubSvcAggUpdate
    o SubSvcAgg

---
+++ daemon/daemonps/events.go
@@ -0,0 +1,68 @@
+// Package daemonps define pub-sub namespaces for daemon
+package daemonps
+
+import (
+	"encoding/json"
+
+	"github.com/google/uuid"
+
+	"opensvc.com/opensvc/core/event"
+	ps "opensvc.com/opensvc/util/pubsub"
+	"opensvc.com/opensvc/util/timestamp"
+)
+
+// PubEvent publish a new event.Event on namespace NsEvent
+func PubEvent(cmdC chan<- interface{}, e event.Event) {
+	ps.Pub(cmdC, ps.Publication{Ns: NsEvent, Op: ps.OpCreate, Value: e})
+}
+
+// SubEvent subscribes on namespace NsEvent
+func SubEvent(cmdC chan<- interface{}, name string, fn func(event.Event)) uuid.UUID {
+	f := func(i interface{}) {
+		fn(i.(event.Event))
+	}
+	publication := ps.Publication{
+		Ns: NsEvent,
+		Op: ps.OpCreate,
+		Id: "subscribe-event",
+		Value: event.Event{
+			Kind:      "event-subscribe",
+			ID:        0,
+			Timestamp: timestamp.Now(),
+			Data:      jsonMsg("subscribe name: " + name),
+		},
+	}
+
+	go ps.Pub(cmdC, publication)
+
+	subscription := ps.Subscription{
+		Ns:   NsEvent,
+		Op:   ps.OpCreate,
+		Name: name,
+	}
+	return ps.Sub(cmdC, subscription, f)
+}
+
+// UnSubEvent unsubscribes a subscription on namespace NsEvent
+func UnSubEvent(cmdC chan<- interface{}, id uuid.UUID) {
+	name := ps.Unsub(cmdC, id)
+	if name != "" {
+		publication := ps.Publication{
+			Ns: NsEvent,
+			Op: ps.OpCreate,
+			Id: "unsubscribe-event",
+			Value: event.Event{
+				Kind:      "event-unsubscribe",
+				ID:        0,
+				Timestamp: timestamp.Now(),
+				Data:      jsonMsg("unsubscribe name: " + name),
+			},
+		}
+		go ps.Pub(cmdC, publication)
+	}
+}
+
+func jsonMsg(msg string) *json.RawMessage {
+	d := json.RawMessage("\"" + msg + "\"")
+	return &d
+}
--- daemon/daemonps/main.go
+++ daemon/daemonps/main.go
@@ -1,13 +1,9 @@
-// Package daemonps define pub-sub namespaces for daemon
 package daemonps
 
 import (
-	"encoding/json"
-
 	"github.com/google/uuid"
 
 	"opensvc.com/opensvc/core/event"
-	"opensvc.com/opensvc/daemon/busids"
 	ps "opensvc.com/opensvc/util/pubsub"
 	"opensvc.com/opensvc/util/timestamp"
 )
@@ -23,58 +19,41 @@ const (
 	NsStatus
 )
 
-// PubEvent publish a new event.Event on namespace NsEvent
-func PubEvent(cmdC chan<- interface{}, e event.Event) {
-	ps.Pub(cmdC, ps.Publication{Ns: NsEvent, Op: ps.OpCreate, Value: e})
-}
-
-// SubEvent subscribes on namespace NsEvent
-func SubEvent(cmdC chan<- interface{}, name string, fn func(event.Event)) uuid.UUID {
-	f := func(i interface{}) {
-		fn(i.(event.Event))
-	}
+func Pub(cmdC chan<- interface{}, ns, op uint, id string, i interface{}) {
 	publication := ps.Publication{
-		Ns: busids.NsEvent,
-		Op: ps.OpCreate,
-		Id: "subscribe-event",
-		Value: event.Event{
-			Kind:      "event-subscribe",
-			ID:        0,
-			Timestamp: timestamp.Now(),
-			Data:      jsonMsg("subscribe name: " + name),
-		},
+		Ns:    ns,
+		Op:    op,
+		Id:    id,
+		Value: i,
 	}
-
-	go ps.Pub(cmdC, publication)
-
-	subscription := ps.Subscription{
-		Ns:   busids.NsEvent,
-		Op:   ps.OpCreate,
-		Name: name,
-	}
-	return ps.Sub(cmdC, subscription, f)
+	ps.Pub(cmdC, publication)
 }
 
-// UnSubEvent unsubscribes a subscription on namespace NsEvent
-func UnSubEvent(cmdC chan<- interface{}, id uuid.UUID) {
+func Sub(cmdC chan<- interface{}, ns, op uint, name string, matching string, fn func(i interface{})) uuid.UUID {
+	subscription := ps.Subscription{
+		Ns:       ns,
+		Op:       op,
+		Matching: matching,
+		Name:     name,
+	}
+	go PubEvent(cmdC, event.Event{
+		Kind:      "event-subscribe",
+		ID:        0,
+		Timestamp: timestamp.Now(),
+		Data:      jsonMsg("subscribe name: " + name),
+	})
+
+	return ps.Sub(cmdC, subscription, fn)
+}
+
+func UnSub(cmdC chan<- interface{}, id uuid.UUID) {
 	name := ps.Unsub(cmdC, id)
 	if name != "" {
-		publication := ps.Publication{
-			Ns: busids.NsEvent,
-			Op: ps.OpCreate,
-			Id: "unsubscribe-event",
-			Value: event.Event{
-				Kind:      "event-unsubscribe",
-				ID:        0,
-				Timestamp: timestamp.Now(),
-				Data:      jsonMsg("unsubscribe name: " + name),
-			},
-		}
-		go ps.Pub(cmdC, publication)
+		go PubEvent(cmdC, event.Event{
+			Kind:      "event-unsubscribe",
+			ID:        0,
+			Timestamp: timestamp.Now(),
+			Data:      jsonMsg("unsubscribe name: " + name),
+		})
 	}
-}
-
-func jsonMsg(msg string) *json.RawMessage {
-	d := json.RawMessage("\"" + msg + "\"")
-	return &d
 }
