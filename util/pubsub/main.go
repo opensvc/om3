79d8821a48b9d30c0d3a2536253f0cded3d6665d
Cyril Galibern <cyril.galibern@opensvc.com>
2022-05-31 19:08:35 +0200

    [util/pubsub] Refactor to allow namespaces, operation and id subscription/publication

--- util/pubsub/main.go
+++ util/pubsub/main.go
@@ -1,3 +1,68 @@
+// Package pubsub implements simple pub-sub
+//
+// Example:
+//	  import (
+//    	"context"
+//    	"fmt"
+//
+//    	ps "opensvc.com/opensvc/util/pubsub"
+//    )
+//
+//    func main() {
+//    	const (
+//    		NsNum1 = ps.NsAll + 1 + iota
+//    		NsNum2
+//    	)
+//
+//    	// Start the pub-sub
+//    	pubSub1 := ps.T{}
+//    	c, err := pubSub1.Start(context.Background(), "pub-sub example")
+//    	if err != nil {
+//    		return
+//    	}
+//    	defer pubSub1.Stop()
+//
+//    	// Prepare a new subscription details
+//    	subOnCreate := ps.Subscription{
+//    		Ns:       NsNum1,
+//    		Op:       ps.OpCreate,
+//    		Matching: "idA",
+//    		Name:     "subscription example",
+//    	}
+//
+//    	// register the subscription
+//    	sub1Id := ps.Sub(c, subOnCreate, func(i interface{}) {
+//    		fmt.Printf("detected from subscription 1: value '%s' has been published with operation 'OpCreate' on id 'IdA' in name space 'NsNum1'\n", i)
+//    	})
+//    	defer ps.Unsub(c, sub1Id)
+//
+//    	// register another subscription that watch all namespaces/operations/ids
+//    	defer ps.Unsub(
+//    		c,
+//    		ps.Sub(c,
+//    			ps.Subscription{Name: "watch all"},
+//    			func(i interface{}) {
+//    				fmt.Printf("detected from subscription 2: value '%s' have been published\n", i)
+//    			}))
+//
+//    	// publish a create operation of "something" on namespace NsNum1
+//    	ps.Pub(c, ps.Publication{
+//    		Ns:    NsNum1,
+//    		Op:    ps.OpCreate,
+//    		Id:    "idA",
+//    		Value: "foo bar",
+//    	})
+//
+//    	// publish a Update operation of "a value" on namespace NsNum2
+//    	ps.Pub(c, ps.Publication{
+//    		Ns:    NsNum2,
+//    		Op:    ps.OpUpdate,
+//    		Id:    "idXXXX",
+//    		Value: "a value",
+//    	})
+//    }
+//
+
 package pubsub
 
 import (
@@ -9,7 +74,22 @@ import (
 	"opensvc.com/opensvc/daemon/daemonctx"
 )
 
+const (
+	// OpAll can be used on Subscription to subscribe on all operations
+	OpAll = iota
+	OpCreate
+	OpRead
+	OpUpdate
+	OpDelete
+)
+
+const (
+	// NsAll operation value can be used for all name spaces
+	NsAll = iota
+)
+
 type (
+	// T struct holds internal data for a pub-sub
 	T struct {
 		cmdC    chan interface{}
 		cancel  func()
@@ -17,15 +97,51 @@ type (
 		running bool
 	}
 
+	// Subscription struct holds a subscription details
+	Subscription struct {
+		// Ns is the namespace to subscribe on
+		Ns uint
+
+		// Op is operation to subscribe on
+		Op uint
+
+		// Matching is the publication id to subscribe on
+		// zero value means subscription on all Publications Id
+		Matching string
+
+		// Name is a description of the subscription
+		Name string
+	}
+
+	// Publication struct holds a new publication
+	Publication struct {
+		// Ns it the publication namespace
+		Ns uint
+		// Op is the publication operation
+		Op uint
+
+		// Id is the publication Id (used by Subscription)
+		Id string
+
+		// Value is the thing to publish
+		Value interface{}
+	}
+
 	cmdPub struct {
+		id   string
+		op   uint
+		ns   uint
 		data interface{}
 		resp chan<- bool
 	}
 
 	cmdSub struct {
-		fn   func(interface{})
-		name string
-		resp chan<- uuid.UUID
+		fn       func(interface{})
+		op       uint
+		ns       uint
+		matching string
+		name     string
+		resp     chan<- uuid.UUID
 	}
 
 	cmdUnsub struct {
@@ -35,30 +151,23 @@ type (
 )
 
 var (
-	ErrorAlreadyRunning = errors.New("pubsub already running")
+	ErrorAlreadyRunning = errors.New("pub sub already running")
 )
 
+// Stop function stops the pub-sub worker
 func (t *T) Stop() {
 	if t.cancel == nil {
 		return
 	}
 	t.cancel()
-	t.WaitStopped()
+	t.waitStopped()
 }
 
-func (t *T) WaitStopped() {
-	if t.stopped == nil {
-		return
-	}
-	select {
-	case <-t.stopped:
-	}
-}
-
-func (t *T) Run(parent context.Context, name string) (chan<- interface{}, error) {
+// Start function starts the pub-sub
+func (t *T) Start(parent context.Context, name string) (chan<- interface{}, error) {
 	log := daemonctx.Logger(parent).With().Str("name", name).Logger()
 	if t.running == true {
-		log.Error().Err(ErrorAlreadyRunning).Msg("Run")
+		log.Error().Err(ErrorAlreadyRunning).Msg("Start")
 		return nil, ErrorAlreadyRunning
 	}
 	running := make(chan bool)
@@ -66,6 +175,9 @@ func (t *T) Run(parent context.Context, name string) (chan<- interface{}, error)
 	go func() {
 		subs := make(map[uuid.UUID]func(interface{}))
 		subNames := make(map[uuid.UUID]string)
+		subNs := make(map[uuid.UUID]uint)
+		subOps := make(map[uuid.UUID]uint)
+		subMatching := make(map[uuid.UUID]string)
 		ctx, cancel := context.WithCancel(parent)
 		t.stopped = make(chan bool)
 		t.cancel = cancel
@@ -85,11 +197,21 @@ func (t *T) Run(parent context.Context, name string) (chan<- interface{}, error)
 			case cmd := <-cmdC:
 				switch c := cmd.(type) {
 				case cmdPub:
-					for _, fn := range subs {
+					for id, fn := range subs {
+						if subNs[id] != NsAll && subNs[id] != c.ns {
+							continue
+						}
+						if subOps[id] != OpAll && subOps[id] != c.op {
+							continue
+						}
+						if len(subMatching[id]) != 0 && subMatching[id] != c.id {
+							continue
+						}
 						running := make(chan bool)
+						goFunc := fn
 						go func() {
 							running <- true
-							fn(c.data)
+							goFunc(c.data)
 						}()
 						<-running
 					}
@@ -98,6 +220,9 @@ func (t *T) Run(parent context.Context, name string) (chan<- interface{}, error)
 					id := uuid.New()
 					subs[id] = c.fn
 					subNames[id] = c.name
+					subNs[id] = c.ns
+					subOps[id] = c.op
+					subMatching[id] = c.matching
 					c.resp <- id
 					log.Info().Msgf("subscribe %s", c.name)
 				case cmdUnsub:
@@ -107,6 +232,8 @@ func (t *T) Run(parent context.Context, name string) (chan<- interface{}, error)
 					}
 					delete(subs, c.subId)
 					delete(subNames, c.subId)
+					delete(subNs, c.subId)
+					delete(subOps, c.subId)
 					c.resp <- name
 					log.Info().Msgf("unsubscribe %s", name)
 				}
@@ -118,22 +245,35 @@ func (t *T) Run(parent context.Context, name string) (chan<- interface{}, error)
 	return cmdC, nil
 }
 
-func Pub(cmdC chan<- interface{}, i interface{}) {
+// Pub function publish a new p Publication
+func Pub(cmdC chan<- interface{}, p Publication) {
 	done := make(chan bool)
-	cmdC <- cmdPub{data: i, resp: done}
+	cmdC <- cmdPub{
+		id:   p.Id,
+		op:   p.Op,
+		ns:   p.Ns,
+		data: p.Value,
+		resp: done,
+	}
 	<-done
 }
 
-func Sub(cmdC chan<- interface{}, name string, fn func(interface{})) uuid.UUID {
+// Sub function submit a new Subscription on pub-sub
+// It returns the subscription uuid.UUID (can be used to un-subscribe)
+func Sub(cmdC chan<- interface{}, s Subscription, fn func(interface{})) uuid.UUID {
 	respC := make(chan uuid.UUID)
 	cmdC <- cmdSub{
-		fn:   fn,
-		name: name,
-		resp: respC,
+		fn:       fn,
+		op:       s.Op,
+		ns:       s.Ns,
+		matching: s.Matching,
+		name:     s.Name,
+		resp:     respC,
 	}
 	return <-respC
 }
 
+// Unsub function remove a subscription
 func Unsub(cmdC chan<- interface{}, id uuid.UUID) string {
 	respC := make(chan string)
 	cmdC <- cmdUnsub{
@@ -141,4 +281,13 @@ func Unsub(cmdC chan<- interface{}, id uuid.UUID) string {
 		resp:  respC,
 	}
 	return <-respC
+}
+
+func (t *T) waitStopped() {
+	if t.stopped == nil {
+		return
+	}
+	select {
+	case <-t.stopped:
+	}
 }
--- util/pubsub/main_test.go
+++ util/pubsub/main_test.go
@@ -2,14 +2,22 @@ package pubsub
 
 import (
 	"context"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	NsCfg = NsAll + 1 + iota
+	NsSvcAgg
+	NsStatus
+)
+
 func newRun(name string) (*T, chan<- interface{}) {
 	p := T{}
-	cmdC, err := p.Run(context.Background(), name)
+	cmdC, err := p.Start(context.Background(), name)
 	if err != nil {
 		return nil, nil
 	}
@@ -19,9 +27,9 @@ func TestRefuseRunTwice(t *testing.T) {
 	p := T{}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	_, err := p.Run(ctx, t.Name())
+	_, err := p.Start(ctx, t.Name())
 	require.Nil(t, err)
-	_, err = p.Run(ctx, t.Name())
+	_, err = p.Start(ctx, t.Name())
 	require.ErrorIs(t, err, ErrorAlreadyRunning)
 }
 
@@ -29,10 +37,10 @@ func TestRunStopRun(t *testing.T) {
 	p := T{}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	_, err := p.Run(ctx, t.Name())
+	_, err := p.Start(ctx, t.Name())
 	require.Nil(t, err)
 	p.Stop()
-	_, err = p.Run(ctx, t.Name())
+	_, err = p.Start(ctx, t.Name())
 	require.Nil(t, err)
 }
 
@@ -40,57 +48,140 @@ func TestRunCancelRun(t *testing.T) {
 	p := T{}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	_, err := p.Run(ctx, t.Name())
+	_, err := p.Start(ctx, t.Name())
 	require.Nil(t, err)
 	cancel()
-	p.WaitStopped()
-	_, err = p.Run(ctx, t.Name())
+	p.waitStopped()
+	_, err = p.Start(ctx, t.Name())
 	require.Nil(t, err)
 }
 
 func TestPub(t *testing.T) {
 	p, cmdC := newRun(t.Name())
 	defer p.Stop()
-	Pub(cmdC, "foo")
-	Pub(cmdC, "bar")
-	Pub(cmdC, "foobar")
+	Pub(cmdC, Publication{Value: "foo", Op: OpCreate})
+	Pub(cmdC, Publication{Value: "foo", Op: OpUpdate})
+	Pub(cmdC, Publication{Value: "foo", Op: OpRead})
+	Pub(cmdC, Publication{Value: "foo", Op: OpDelete})
+	Pub(cmdC, Publication{Value: "bar"})
+	Pub(cmdC, Publication{Value: "foobar"})
 }
 
 func TestSubUnSub(t *testing.T) {
 	p, cmdC := newRun(t.Name())
 	defer p.Stop()
-	sub := Sub(cmdC, t.Name(), func(_ interface{}) {})
+	sub := Sub(cmdC, Subscription{Name: t.Name()}, func(_ interface{}) {})
 	Unsub(cmdC, sub)
 }
 
 func TestSubThenPub(t *testing.T) {
 	p, cmdC := newRun(t.Name())
 	defer p.Stop()
-	published := []string{}
+	published := make([]string, 0)
 	toPublish := []string{"foo", "foo1", "foo2"}
-	Sub(cmdC, t.Name(), func(s interface{}) {
+	Sub(cmdC, Subscription{Name: t.Name()}, func(s interface{}) {
 		published = append(published, s.(string))
 	})
 	for _, s := range toPublish {
-		Pub(cmdC, s)
+		Pub(cmdC, Publication{Value: s})
 	}
 	require.Equal(t, published, toPublish)
 }
 
-func TestSubPubUnSubPub(t *testing.T) {
+func TestSubNsThenPub(t *testing.T) {
 	p, cmdC := newRun(t.Name())
 	defer p.Stop()
-	published := []string{}
+	expectedCfg := []uint32{1, 20, 30}
+	expectedCfgId1 := []uint32{20}
+	expectedSvcAgg := []string{"foo", "foo1", "foo2", "foo1", "foo2"}
+	expectedSvcAggDelete := []string{"foo1", "foo2"}
+
+	toPublishSvcAgg := []string{"foo", "foo1", "foo2"}
+	toPublishSvcAggDelete := []string{"foo1", "foo2"}
+	toPublishCfg := []uint32{1, 20, 30}
+
+	var (
+		publishedCfg          []uint32
+		publishedCfgId1       []uint32
+		publishedSvcAgg       []string
+		publishedSvcAggDelete []string
+	)
+
+	Sub(cmdC, Subscription{Ns: NsCfg, Matching: "1", Name: "onCfg for Id 1"}, func(s interface{}) {
+		t.Logf("-> NsCfg Id 1 sub receive: %v", s)
+		publishedCfgId1 = append(publishedCfgId1, s.(uint32))
+	})
+
+	Sub(cmdC, Subscription{Ns: NsCfg, Name: "onCfg"}, func(s interface{}) {
+		t.Logf("-> NsCfg sub receive: %v", s)
+		publishedCfg = append(publishedCfg, s.(uint32))
+	})
+	Sub(cmdC, Subscription{Ns: NsSvcAgg, Name: "onSvcAgg"}, func(s interface{}) {
+		t.Logf("-> NsSvcAgg sub receive: %v", s)
+		publishedSvcAgg = append(publishedSvcAgg, s.(string))
+	})
+
+	Sub(cmdC, Subscription{Ns: NsSvcAgg, Op: OpDelete, Name: "onSvcAggDelete"}, func(s interface{}) {
+		t.Logf("-> NsSvcAgg Op delete sub receive: %v", s)
+		publishedSvcAggDelete = append(publishedSvcAggDelete, s.(string))
+	})
+
+	t.Log("NsSvcAgg")
+	for i, s := range toPublishSvcAgg {
+		time.Sleep(1 * time.Nanosecond)
+		Pub(cmdC, Publication{
+			Id:    strconv.Itoa(i),
+			Value: s,
+			Ns:    NsSvcAgg,
+		})
+	}
+	t.Log("NsCfg")
+	for i, s := range toPublishCfg {
+		time.Sleep(1 * time.Nanosecond)
+		Pub(cmdC, Publication{
+			Id:    strconv.Itoa(i),
+			Value: s,
+			Ns:    NsCfg,
+		})
+	}
+	t.Log("nsCfgDelete")
+	for i, s := range toPublishSvcAggDelete {
+		time.Sleep(1 * time.Nanosecond)
+		Pub(cmdC, Publication{
+			Id:    strconv.Itoa(i),
+			Value: s,
+			Op:    OpDelete,
+			Ns:    NsSvcAgg,
+		})
+	}
+	time.Sleep(1 * time.Millisecond)
+
+	require.ElementsMatch(t, expectedCfg, publishedCfg, "cfg")
+	require.ElementsMatch(t, expectedCfgId1, publishedCfgId1, "cfg id1")
+	require.ElementsMatch(t, expectedSvcAgg, publishedSvcAgg, "svcAgg")
+	require.ElementsMatch(t, expectedSvcAggDelete, publishedSvcAggDelete, "svcAgg delete")
+}
+
+func TestSubPubUnSubPubWithoutFilter(t *testing.T) {
+	p, cmdC := newRun(t.Name())
+	defer p.Stop()
 	toPublish := []string{"foo", "foo1", "foo2"}
-	id := Sub(cmdC, t.Name(), func(s interface{}) {
+	expectedPublished := []string{"foo", "foo1", "foo2"}
+
+	var published []string
+	id := Sub(cmdC, Subscription{Name: t.Name()}, func(s interface{}) {
 		published = append(published, s.(string))
 	})
 	for _, s := range toPublish {
-		Pub(cmdC, s)
+		Pub(cmdC, Publication{Value: s})
 	}
 	Unsub(cmdC, id)
+	t.Logf("NsAll: %d", NsAll)
+	t.Logf("NsCfg: %d", NsCfg)
+	t.Logf("NsSvcAgg: %d", NsSvcAgg)
+	t.Logf("NsStatus: %d", NsStatus)
 	for _, s := range toPublish {
-		Pub(cmdC, s)
+		Pub(cmdC, Publication{Ns: NsSvcAgg, Value: s})
 	}
-	require.Equal(t, published, toPublish)
+	require.Equal(t, published, expectedPublished)
 }
