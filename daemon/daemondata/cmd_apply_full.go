5001963680a076f758011fb309beb13b817a0ea6
Cyril Galibern <cyril.galibern@opensvc.com>
2022-06-14 17:50:49 +0200

    publish patch event remote full dataset is applied

--- daemon/daemondata/cmd_apply_full.go
+++ daemon/daemondata/cmd_apply_full.go
@@ -1,6 +1,14 @@
 package daemondata
 
-import "opensvc.com/opensvc/core/cluster"
+import (
+	"encoding/json"
+
+	"opensvc.com/opensvc/core/cluster"
+	"opensvc.com/opensvc/core/event"
+	"opensvc.com/opensvc/daemon/daemonps"
+	"opensvc.com/opensvc/util/jsondelta"
+	"opensvc.com/opensvc/util/timestamp"
+)
 
 type opApplyRemoteFull struct {
 	nodename string
@@ -17,6 +25,28 @@ func (o opApplyRemoteFull) call(d *data) {
 	if gen, ok := d.pending.Monitor.Nodes[o.nodename].Gen[d.localNode]; ok {
 		d.mergedOnPeer[o.nodename] = gen
 	}
+
+	absolutePatch := jsondelta.Patch{
+		jsondelta.Operation{
+			OpPath:  jsondelta.OperationPath{"monitor", "nodes", o.nodename},
+			OpValue: jsondelta.NewOptValue(o.full),
+			OpKind:  "replace",
+		},
+	}
+
+	if eventB, err := json.Marshal(absolutePatch); err != nil {
+		d.log.Error().Err(err).Msgf("Marshal absolutePatch %s", o.nodename)
+	} else {
+		var eventData json.RawMessage = eventB
+		eventId++
+		daemonps.PubEvent(d.pubSub, event.Event{
+			Kind:      "patch",
+			ID:        eventId,
+			Timestamp: timestamp.Now(),
+			Data:      &eventData,
+		})
+	}
+
 	d.log.Debug().
 		Interface("remotesNeedFull", d.remotesNeedFull).
 		Interface("mergedOnPeer", d.mergedOnPeer).
