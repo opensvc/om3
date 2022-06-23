745a61d0cdef608969f13598dc2f68006707976a
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-06-23 17:10:42 +0200

    Add log events sorting
    Example:
    
    $ sudo ./opensvc 'test/svc/ip*' logs --local --sid 10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.616Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip4 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.616Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip5 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.616Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip2 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip1 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip6 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip7 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF checking 10.0.3.23 availability (5s) n=eggplant o=test/svc/ip4 rid=ip#0 sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip10 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip3 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip9 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    2022-06-23T14:08:57.622Z INF do argv=["./opensvc","test/svc/ip*","start","--local"] n=eggplant o=test/svc/ip8 origin=user sid=10f7526c-9432-4a9b-9c1f-46d9043d0a4f
    ...

--- core/commands/object_logs.go
+++ core/commands/object_logs.go
@@ -3,14 +3,17 @@ package commands
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/fatih/color"
 	"github.com/rs/zerolog"
 	"github.com/spf13/cobra"
 	"opensvc.com/opensvc/core/flag"
 	"opensvc.com/opensvc/core/object"
+	"opensvc.com/opensvc/core/path"
 	"opensvc.com/opensvc/util/render"
 )
 
@@ -41,25 +44,59 @@ func (t *CmdObjectLogs) cmd(kind string, selector *string) *cobra.Command {
 	}
 }
 
-func (t *CmdObjectLogs) render(fpath string) error {
+func (t *CmdObjectLogs) parseFile(fpath string) ([][]byte, error) {
 	f, err := os.Open(fpath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
-	w := zerolog.NewConsoleWriter()
-	w.TimeFormat = "2006-01-02T15:04:05.000Z07:00"
-	w.NoColor = color.NoColor
 	var patternSID []byte
 	if t.SID != "" {
 		patternSID = []byte(fmt.Sprintf("\"sid\":\"%s\"", t.SID))
 	}
+	events := make([][]byte, 0)
 	for scanner.Scan() {
 		b := scanner.Bytes()
 		if (patternSID != nil) && !bytes.Contains(b, patternSID) {
 			continue
 		}
+		events = append(events, b)
+	}
+	return events, nil
+}
+
+func (t *CmdObjectLogs) gather(paths []path.T) ([][]byte, error) {
+	type bareEvent struct {
+		TS float64 `json:"t"`
+	}
+	events := make([][]byte, 0)
+	for _, p := range paths {
+		fpath := object.LogFile(p)
+		if more, err := t.parseFile(fpath); err != nil {
+			return events, err
+		} else {
+			events = append(events, more...)
+		}
+	}
+	sort.Slice(events, func(i, j int) bool {
+		var e1, e2 bareEvent
+		if err := json.Unmarshal(events[i], &e1); err != nil {
+			return false
+		}
+		if err := json.Unmarshal(events[j], &e2); err != nil {
+			return false
+		}
+		return e1.TS < e2.TS
+	})
+	return events, nil
+}
+
+func (t *CmdObjectLogs) render(events [][]byte) error {
+	w := zerolog.NewConsoleWriter()
+	w.TimeFormat = "2006-01-02T15:04:05.000Z07:00"
+	w.NoColor = color.NoColor
+	for _, b := range events {
 		switch t.Global.Format {
 		case "json":
 			fmt.Printf("%s\n", string(b))
@@ -79,13 +116,11 @@ func (t *CmdObjectLogs) local(selStr string) error {
 	if err != nil {
 		return err
 	}
-	for _, p := range paths {
-		fpath := object.LogFile(p)
-		if err := t.render(fpath); err != nil {
-			return err
-		}
+	events, err := t.gather(paths)
+	if err != nil {
+		return err
 	}
-	return nil
+	return t.render(events)
 }
 
 func (t *CmdObjectLogs) run(selector *string, kind string) {
