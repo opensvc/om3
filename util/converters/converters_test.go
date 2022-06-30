f42bfbfb10b40f0e5241b06054a02bd769b6055c
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-06-30 16:20:23 +0200

    Add a test to highlight a duration converter regression (not fixed yet)
    cvaroqui@eggplant:~/dev/omg$ go test ./util/converters/...
    --- FAIL: TestDurationConvert (0.00s)
        --- FAIL: TestDurationConvert/valid_duration_expression_returns_expected_values (0.00s)
            --- FAIL: TestDurationConvert/valid_duration_expression_returns_expected_values/1y (0.00s)
                converters_test.go:31:
                    	Error Trace:	converters_test.go:31
                    	Error:      	Received unexpected error:
                    	            	time: unknown unit "y" in duration "1y"
                    	Test:       	TestDurationConvert/valid_duration_expression_returns_expected_values/1y
            --- FAIL: TestDurationConvert/valid_duration_expression_returns_expected_values/1d (0.00s)
                converters_test.go:31:
                    	Error Trace:	converters_test.go:31
                    	Error:      	Received unexpected error:
                    	            	time: unknown unit "d" in duration "1d"
                    	Test:       	TestDurationConvert/valid_duration_expression_returns_expected_values/1d
            --- FAIL: TestDurationConvert/valid_duration_expression_returns_expected_values/1w (0.00s)
                converters_test.go:31:
                    	Error Trace:	converters_test.go:31
                    	Error:      	Received unexpected error:
                    	            	time: unknown unit "w" in duration "1w"
                    	Test:       	TestDurationConvert/valid_duration_expression_returns_expected_values/1w
    FAIL
    FAIL	opensvc.com/opensvc/util/converters	0.002s
    FAIL

--- util/converters/converters_test.go
+++ util/converters/converters_test.go
@@ -2,39 +2,83 @@ package converters
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-var (
-	validStrings = map[string]int64{
-		"0":     int64(0),
-		"1":     int64(1),
-		"1000":  int64(1000),
-		"1KB":   int64(1000),
-		"1,3KB": int64(1300),
-		"1KiB":  int64(1024),
-		"2MiB":  int64(2 * 1024 * 1024),
-		"3GiB":  int64(3 * 1024 * 1024 * 1024),
-		"3gib":  int64(3 * 1024 * 1024 * 1024),
-		"4TiB":  int64(4 * 1024 * 1024 * 1024 * 1024),
-		"5PiB":  int64(5 * 1024 * 1024 * 1024 * 1024 * 1024),
-		"6EiB":  int64(6 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024),
-		"6eib":  int64(6 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024),
-		"8EB":   int64(8 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000),
-		"8.5EB": int64(8.5 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000),
-		"8.5eb": int64(8.5 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000),
-	}
-	invalidStrings = []string{
-		"-1",
-		"-1000",
-		"-1KB",
-		"8EiB",
-		"badValue",
-	}
-)
+func TestDurationConvert(t *testing.T) {
+	var (
+		validStrings = map[string]time.Duration{
+			"0":    0 * time.Second,
+			"1":    1 * time.Second,
+			"1s":   1 * time.Second,
+			"1m1s": 61 * time.Second,
+			"1d":   24 * time.Hour,
+			"1w":   7 * 24 * time.Hour,
+			"1y":   365 * 24 * time.Hour,
+		}
+		invalidStrings = []string{
+			"1p",
+		}
+	)
+
+	t.Run("valid duration expression returns expected values", func(t *testing.T) {
+		for s, expected := range validStrings {
+			t.Run(s, func(t *testing.T) {
+				result, err := Duration.Convert(s)
+				require.NoError(t, err)
+				assert.Equal(t, expected, *result.(*time.Duration))
+			})
+		}
+	})
+
+	t.Run("empty String returns nil", func(t *testing.T) {
+		result, err := Duration.Convert("")
+		assert.NoError(t, err)
+		assert.Nil(t, result)
+	})
+
+	t.Run("invalid duration expression returns (nil, error)", func(t *testing.T) {
+		for _, s := range invalidStrings {
+			t.Run(s, func(t *testing.T) {
+				_, err := Duration.Convert(s)
+				assert.Error(t, err)
+			})
+		}
+	})
+}
 
 func TestSizeConvert(t *testing.T) {
+	var (
+		validStrings = map[string]int64{
+			"0":     int64(0),
+			"1":     int64(1),
+			"1000":  int64(1000),
+			"1KB":   int64(1000),
+			"1,3KB": int64(1300),
+			"1KiB":  int64(1024),
+			"2MiB":  int64(2 * 1024 * 1024),
+			"3GiB":  int64(3 * 1024 * 1024 * 1024),
+			"3gib":  int64(3 * 1024 * 1024 * 1024),
+			"4TiB":  int64(4 * 1024 * 1024 * 1024 * 1024),
+			"5PiB":  int64(5 * 1024 * 1024 * 1024 * 1024 * 1024),
+			"6EiB":  int64(6 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024),
+			"6eib":  int64(6 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024),
+			"8EB":   int64(8 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000),
+			"8.5EB": int64(8.5 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000),
+			"8.5eb": int64(8.5 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000),
+		}
+		invalidStrings = []string{
+			"-1",
+			"-1000",
+			"-1KB",
+			"8EiB",
+			"badValue",
+		}
+	)
+
 	t.Run("Valid String return expected values", func(t *testing.T) {
 		for s, expected := range validStrings {
 			t.Run(s, func(t *testing.T) {
