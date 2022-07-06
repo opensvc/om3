9b5f031adb80622c02efef0f29df378cc9bb7ad4
Cyril Galibern <cyril.galibern@opensvc.com>
2022-07-06 15:57:05 +0200

    [timestamp] Test Unmarshal

--- util/timestamp/main_test.go
+++ util/timestamp/main_test.go
@@ -1,11 +1,21 @@
 package timestamp
 
 import (
+	"encoding/json"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTimestamp(t *testing.T) {
 	ts := Now()
 	s := ts.String()
 	t.Logf("%s", s)
+
+	require.True(t, NewZero().IsZero())
+	require.True(t, New(zero).IsZero())
+
+	var fromString = T{}
+	require.NoError(t, json.Unmarshal([]byte("0.0"), &fromString))
+	require.True(t, fromString.IsZero())
 }
