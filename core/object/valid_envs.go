a3e050b19dc9a5ffe31ee95d215a89315b3c6df5
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-07-13 17:50:12 +0200

    Add missing valid_envs.go file in core/object/

---
+++ core/object/valid_envs.go
@@ -0,0 +1,19 @@
+package object
+
+var validEnvs = []string{
+	"CERT",
+	"DEV",
+	"DRP",
+	"FOR",
+	"INT",
+	"PRA",
+	"PRD",
+	"PRJ",
+	"PPRD",
+	"QUAL",
+	"REC",
+	"STG",
+	"TMP",
+	"TST",
+	"UAT",
+}
