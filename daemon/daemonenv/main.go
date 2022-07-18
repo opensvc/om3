6a98c2a81703e2d833ef91e225bfb9ff2a984b86
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-07-18 16:47:56 +0200

    Use rawconfig.Paths instead of hardcoded paths in daemonenv

--- daemon/daemonenv/main.go
+++ daemon/daemonenv/main.go
@@ -1,20 +1,26 @@
 package daemonenv
 
+import (
+	"path/filepath"
+
+	"opensvc.com/opensvc/core/rawconfig"
+)
+
 var (
 	// TODO use values from config
 	RawPort  = "1214"
 	HttpPort = "1215"
 
-	PathUxRaw  = "/var/lib/opensvc/lsnr/lsnr.sock"
-	PathUxHttp = "/var/lib/opensvc/lsnr/h2.sock"
+	PathUxRaw  = filepath.Join(rawconfig.Paths.Var, "lsnr", "lsnr.sock")
+	PathUxHttp = filepath.Join(rawconfig.Paths.Var, "lsnr", "h2.sock")
 
 	UrlUxRaw    = "raw://" + PathUxRaw
 	UrlUxHttp   = "http://" + PathUxHttp
 	UrlInetHttp = "https://localhost:" + HttpPort
 	UrlInetRaw  = "raw://localhost:" + RawPort
 
-	CertFile = "/tmp/certificate_chain"
-	KeyFile  = "/tmp/private_key"
+	CertFile = filepath.Join(rawconfig.Paths.Certs, "certificate_chain")
+	KeyFile  = filepath.Join(rawconfig.Paths.Certs, "private_key")
 
 	HeaderNode        = "o-node"
 	HeaderMultiplexed = "o-multiplexed"
