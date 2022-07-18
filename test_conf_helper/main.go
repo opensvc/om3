21bb845c5986b58d3c5ba2f5a9c667dc33c61b4b
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-07-18 15:30:05 +0200

    Replace some Nil asserts by NoError
    So go test outputs are more informative than pointer address and type.

--- test_conf_helper/main.go
+++ test_conf_helper/main.go
@@ -1,18 +1,19 @@
 package test_conf_helper
 
 import (
-	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func InstallSvcFile(t *testing.T, sourceName, dstFile string) {
-	require.Nil(t, os.MkdirAll(filepath.Dir(dstFile), 0700))
+	require.NoError(t, os.MkdirAll(filepath.Dir(dstFile), 0700))
 	srcFile := filepath.Join("test-fixtures", sourceName)
 	data, err := ioutil.ReadFile(srcFile)
-	require.Nil(t, err)
+	require.NoError(t, err)
 	err = ioutil.WriteFile(dstFile, data, 0644)
-	require.Nil(t, err)
+	require.NoError(t, err)
 }
