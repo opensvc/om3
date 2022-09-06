1973253720067febf291a2672f284e6982897df9
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-09-06 08:23:13 +0200

    core.env enhancements
    Use vars to store env var names:
    
    	ActionOriginVar    = "OSVC_ACTION_ORIGIN"
    	NameVar      = "OSVC_NAME"
    	NamespaceVar = "OSVC_NAMESPACE"
    	KindVar      = "OSVC_KIND"
    	ContextVar   = "OSVC_CONTEXT"
    
    Add a helper func to add OSVC_ACTION_ORIGIN=daemon to the called command
    environment.

--- core/env/env.go
+++ core/env/env.go
@@ -2,37 +2,55 @@ package env
 
 import "os"
 
+var (
+	ActionOriginVar    = "OSVC_ACTION_ORIGIN"
+	ActionOriginUser   = "user"
+	ActionOriginDaemon = "daemon"
+
+	NameVar      = "OSVC_NAME"
+	NamespaceVar = "OSVC_NAMESPACE"
+	KindVar      = "OSVC_KIND"
+	ContextVar   = "OSVC_CONTEXT"
+)
+
 // HasDaemonOrigin returns true if the environment variable OSVC_ACTION_ORIGIN
 // is set to "daemon". The opensvc daemon sets this variable on every command
 // it executes.
 func HasDaemonOrigin() bool {
-	return os.Getenv("OSVC_ACTION_ORIGIN") == "daemon"
+	return os.Getenv(ActionOriginVar) == ActionOriginDaemon
 }
 
 // Origin returns the action origin using a env var that the daemon sets when
 // executing a CRM action. The only possible return values are "daemon" or "user".
 func Origin() string {
-	s := os.Getenv("OSVC_ACTION_ORIGIN")
+	s := os.Getenv(ActionOriginVar)
 	if s == "" {
-		s = "user"
+		s = ActionOriginUser
 	}
 	return s
+}
+
+// DaemonOriginSetenvArgs returns the args to pass to environment variable
+// setter functions to hint the called CRM command was launched from a daemon
+// policy.
+func DaemonOriginSetenvArgs() []string {
+	return []string{ActionOriginVar + "=" + ActionOriginDaemon}
 }
 
 // Namespace returns the namespace filter forced via the OSVC_NAMESPACE environment
 // variable.
 func Namespace() string {
-	return os.Getenv("OSVC_NAMESPACE")
+	return os.Getenv(NamespaceVar)
 }
 
 // Kind returns the object kind filter forced via the OSVC_NAMESPACE environment
 // variable.
 func Kind() string {
-	return os.Getenv("OSVC_KIND")
+	return os.Getenv(KindVar)
 }
 
 // Context returns the identifier of a remote cluster endpoint and credentials
 // configuration via the OSVC_CONTEXT variable.
 func Context() string {
-	return os.Getenv("OSVC_CONTEXT")
+	return os.Getenv(ContextVar)
 }
