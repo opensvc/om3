58533d5bec6578f7f172619dc4a28cbd82228ee1
Cyril Galibern <cyril.galibern@opensvc.com>
2022-09-20 11:41:55 +0200

    [daemon/daemondata] rename cmd_get_stats to stats.go

--- daemon/daemondata/stats.go
+++ daemon/daemondata/stats.go
@@ -10,16 +10,16 @@ type opStats struct {
 	stats chan<- callcount.Stats
 }
 
+func (t T) Stats() callcount.Stats {
+	stats := make(chan callcount.Stats)
+	t.cmdC <- opStats{stats: stats}
+	return <-stats
+}
+
 func (o opStats) call(ctx context.Context, d *data) {
 	d.counterCmd <- idStats
 	select {
 	case <-ctx.Done():
 	case o.stats <- callcount.GetStats(d.counterCmd):
 	}
-}
-
-func (t T) Stats() callcount.Stats {
-	stats := make(chan callcount.Stats)
-	t.cmdC <- opStats{stats: stats}
-	return <-stats
 }
