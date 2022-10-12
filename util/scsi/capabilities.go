a9942826e8dce70e225ccccfbffcb7d072c05af4
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-10-12 12:54:00 +0200

    Better mpathpersist capability scanner
    Add the "reservation_key file" detection in multipath.conf.

--- util/scsi/capabilities.go
+++ util/scsi/capabilities.go
@@ -1,7 +1,10 @@
 package scsi
 
 import (
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"regexp"
 
 	"opensvc.com/opensvc/util/capabilities"
 )
@@ -11,12 +14,18 @@ const (
 	SGPersistCapability    = "node.x.scsi.sg_persist"
 )
 
+var (
+	mpathReservationKeyFileRegexp = regexp.MustCompile(`(?m)^\s*reservation_key\s+("file"|file)\s*$`)
+)
+
 // CapabilitiesScanner is the capabilities scanner for scsi
 func CapabilitiesScanner() ([]string, error) {
 	l := make([]string, 0)
 	if _, err := exec.LookPath("mpathpersist"); err != nil {
 		// pass
-	} else {
+	} else if mpathReservationKeyConfigured, err := isMpathReservationKeyConfigured(); err != nil {
+		// pass
+	} else if mpathReservationKeyConfigured {
 		l = append(l, MpathPersistCapability)
 	}
 	if _, err := exec.LookPath("sg_persist"); err != nil {
@@ -25,6 +34,17 @@ func CapabilitiesScanner() ([]string, error) {
 		l = append(l, SGPersistCapability)
 	}
 	return l, nil
+}
+
+func isMpathReservationKeyConfigured() (bool, error) {
+	b, err := ioutil.ReadFile("/etc/multipath.conf")
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return mpathReservationKeyFileRegexp.Match(b), nil
 }
 
 // register node scanners
