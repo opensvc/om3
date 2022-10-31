4281326ecf51882f5fd7b4b1052850547575a447
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-10-31 09:35:14 +0100

    Merge entrypoints.DaemonStats into core/commands

--- core/commands/daemon_stats.go
+++ core/commands/daemon_stats.go
@@ -1,6 +1,13 @@
 package commands
 
-import "opensvc.com/opensvc/core/entrypoints"
+import (
+	"encoding/json"
+
+	"opensvc.com/opensvc/core/client"
+	"opensvc.com/opensvc/core/cluster"
+	"opensvc.com/opensvc/core/output"
+	"opensvc.com/opensvc/core/rawconfig"
+)
 
 type (
 	CmdDaemonStats struct {
@@ -9,9 +16,54 @@ type (
 )
 
 func (t *CmdDaemonStats) Run() error {
-	return entrypoints.DaemonStats{
-		Format: t.Format,
-		Color:  t.Color,
-		Server: t.Server,
-	}.Do()
+	var (
+		err  error
+		b    []byte
+		data cluster.Stats
+		c    *client.T
+	)
+	c, err = client.New(client.WithURL(t.Server))
+	if err != nil {
+		return err
+	}
+	handle := c.NewGetDaemonStats()
+	b, err = handle.Do()
+	if err != nil {
+		return err
+	}
+	data, err = parseDaemonStats(b)
+	if err != nil {
+		return err
+	}
+	renderer := output.Renderer{
+		Format:   t.Format,
+		Color:    t.Color,
+		Data:     data,
+		Colorize: rawconfig.Colorize,
+	}
+	renderer.Print()
+	return nil
+}
+
+func parseDaemonStats(b []byte) (cluster.Stats, error) {
+	type (
+		nodeData struct {
+			Status int                     `json:"status"`
+			Data   cluster.NodeStatsBundle `json:"data"`
+		}
+		responseType struct {
+			Status int                 `json:"status"`
+			Nodes  map[string]nodeData `json:"nodes"`
+		}
+	)
+	var t responseType
+	ds := make(cluster.Stats)
+	err := json.Unmarshal(b, &t)
+	if err != nil {
+		return ds, err
+	}
+	for k, v := range t.Nodes {
+		ds[k] = v.Data
+	}
+	return ds, nil
 }
