b83d9d490d7584a4ac39007ec23749195ac32d90
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-10-31 09:48:14 +0100

    Merge core/entrypoints/events into core/commands/node_events

--- core/commands/node_events.go
+++ core/commands/node_events.go
@@ -1,6 +1,11 @@
 package commands
 
-import "opensvc.com/opensvc/core/entrypoints"
+import (
+	"opensvc.com/opensvc/core/client"
+	"opensvc.com/opensvc/core/event"
+	"opensvc.com/opensvc/core/output"
+	"opensvc.com/opensvc/core/rawconfig"
+)
 
 type (
 	CmdNodeEvents struct {
@@ -9,10 +14,37 @@ type (
 )
 
 func (t *CmdNodeEvents) Run() error {
-	e := entrypoints.Events{
-		Format: t.Format,
-		Color:  t.Color,
-		Server: t.Server,
+	var (
+		err error
+		c   *client.T
+	)
+	c, err = client.New(client.WithURL(t.Server))
+	if err != nil {
+		return err
 	}
-	return e.Do()
+	streamer := c.NewGetEvents().SetRelatives(false)
+	events, err := streamer.Do()
+	if err != nil {
+		return err
+	}
+	for m := range events {
+		t.doOne(m)
+	}
+	return nil
+}
+
+func (t *CmdNodeEvents) doOne(e event.Event) {
+	human := func() string {
+		return event.Render(e)
+	}
+	if t.Format == output.JSON.String() {
+		t.Format = output.JSONLine.String()
+	}
+	output.Renderer{
+		Format:        t.Format,
+		Color:         t.Color,
+		Data:          e,
+		HumanRenderer: human,
+		Colorize:      rawconfig.Colorize,
+	}.Print()
 }
