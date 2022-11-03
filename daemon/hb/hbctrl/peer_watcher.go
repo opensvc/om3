9a2a998f3f45310439faf0c33d17cbfed4618936
Cyril Galibern <cyril.galibern@opensvc.com>
2022-11-04 00:08:13 +0100

    [daemon/hb/hbctrl] Fix peer watcher no event on fast up->down->up
    During kill -STOP -> sleep -> kill -RUN daemon:
    - hb_stale is detected, followed by hb_beating
      => a publication of dead node is done
    
    To avoid this we add a delay (200ms) before analyse beating value vs previous value.
    so beating false
       beating true
    is analysed has a noop

--- daemon/hb/hbctrl/peer_watcher.go
+++ daemon/hb/hbctrl/peer_watcher.go
@@ -8,29 +8,43 @@ import (
 	"opensvc.com/opensvc/daemon/daemonlogctx"
 )
 
+var (
+	// changeDelay is the delayed duration after a change to re-read current
+	// beating. It is useful to reduce beating event published on fast beating changes
+	// that may occur on ctrl-Z daemon.
+	// Example:
+	// t0: initial beating: true
+	// t1: evStale => beating false
+	// if t1 + changeDelay < t2: read current beating (false) => publish stale event
+	// t2: evBeating => beating true
+	// if t1 + changeDelay > t2: read current beating (true) => no event
+	changeDelay = 200 * time.Millisecond
+)
+
 // peerWatch starts a new peer watcher of nodename for hbId
 // when beating state change a hb_beating or hb_stale event is fired
 // Once beating, a hb_stale event is fired if no beating are received after timeout
 func (c *ctrl) peerWatch(ctx context.Context, beatingC chan bool, hbId, nodename string, timeout time.Duration) {
 	peer := cluster.HeartbeatPeerStatus{}
-	beatingCtx, cancel := context.WithCancel(ctx)
-	update := func(hbStatus cluster.HeartbeatPeerStatus) {
-		c.cmd <- CmdSetPeerStatus{
-			Nodename:   nodename,
-			HbId:       hbId,
-			PeerStatus: hbStatus,
-		}
-	}
-	event := func(name string) {
-		c.cmd <- CmdEvent{
-			Name:     name,
-			Nodename: nodename,
-			HbId:     hbId,
-		}
-	}
 	started := make(chan bool)
 	go func() {
-		defer cancel()
+		// changes tracks beating value changes
+		var changes bool
+
+		// lastBeating is the latest peer.Beating value changed
+		var lastBeating bool
+
+		// changeTicker the interval ticker to verify if peer.Beating != lastBeating
+		changeTicker := time.NewTicker(changeDelay)
+		defer changeTicker.Stop()
+		changeTicker.Stop()
+
+		// staleTicker is the ticker to watch beating==true not refreshed since timeout
+		// Reset when receive a beating true
+		// Stop when receive a beating false
+		staleTicker := time.NewTicker(timeout)
+		staleTicker.Stop()
+		defer staleTicker.Stop()
 		log := daemonlogctx.Logger(ctx).With().Str("Name", "peerWatch-"+hbId+"-"+nodename).Logger()
 		log.Info().Msg("watching")
 		started <- true
@@ -40,27 +54,50 @@ func (c *ctrl) peerWatch(ctx context.Context, beatingC chan bool, hbId, nodename
 				log.Info().Msg("done watching")
 				return
 			case <-c.ctx.Done():
-				log.Info().Msg("done watching (from crtl done)")
+				log.Info().Msg("done watching (from ctrl done)")
 				return
 			case beating := <-beatingC:
 				if beating {
 					if !peer.Beating {
 						peer.Beating = true
-						event("hb_beating")
+						changes = true
+						changeTicker.Reset(changeDelay)
 					}
-					cancel()
-					beatingCtx, cancel = context.WithTimeout(c.ctx, timeout)
+					staleTicker.Reset(timeout)
 					peer.Last = time.Now()
-					update(peer)
 				} else if peer.Beating {
-					event("hb_stale")
+					peer.Beating = false
+					changes = true
+					changeTicker.Reset(changeDelay)
+					staleTicker.Stop()
 				}
-			case <-beatingCtx.Done():
+			case <-changeTicker.C:
+				changeTicker.Stop()
+				if changes {
+					changes = false
+					if lastBeating != peer.Beating {
+						evName := evBeating
+						if !peer.Beating {
+							evName = evStale
+						}
+						c.cmd <- CmdEvent{
+							Name:     evName,
+							Nodename: nodename,
+							HbId:     hbId,
+						}
+						c.cmd <- CmdSetPeerStatus{
+							Nodename:   nodename,
+							HbId:       hbId,
+							PeerStatus: peer,
+						}
+						lastBeating = peer.Beating
+					}
+				}
+			case <-staleTicker.C:
 				if peer.Beating {
 					peer.Beating = false
-					event("hb_stale")
-					update(peer)
-					beatingCtx, cancel = context.WithCancel(c.ctx)
+					changes = true
+					changeTicker.Reset(changeDelay)
 				}
 			}
 		}
