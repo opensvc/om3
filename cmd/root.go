37ca75164785029ba6d6afd21d96b9fc3435b726
Christophe Varoqui <christophe.varoqui@opensvc.com>
2022-11-21 09:34:47 +0100

    Use a go;embed for the bash completion function fed to cobra commands

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -35,51 +36,20 @@ var (
 	debugFlag      bool
 	foregroundFlag bool
 	callerFlag     bool
+
+	//go:embed bash_completion.sh
+	bashCompletionFunction string
+
+	root = &cobra.Command{
+		Use:                    "opensvc",
+		Short:                  "Manage the opensvc cluster infrastructure and its deployed services.",
+		PersistentPreRunE:      persistentPreRunE,
+		SilenceUsage:           true,
+		SilenceErrors:          false,
+		ValidArgsFunction:      validArgs,
+		BashCompletionFunction: bashCompletionFunction,
+	}
 )
-
-var root = &cobra.Command{
-	Use:               "opensvc",
-	Short:             "Manage the opensvc cluster infrastructure and its deployed services.",
-	PersistentPreRunE: persistentPreRunE,
-	SilenceUsage:      true,
-	SilenceErrors:     false,
-	ValidArgsFunction: validArgs,
-	BashCompletionFunction: `__opensvc_handle_word()
-{
-    [ $cword -gt 1 ] && [ ! -z "${words[1]}" ] && ! __opensvc_contains_word ${words[1]} svc vol sec cfg usr ccfg nscfg all completion create daemon monitor help && {
-        words[1]="all"
-    }
-    ___opensvc_handle_word
-}
-
-___opensvc_handle_word()
-{ 
-    if [[ $c -ge $cword ]]; then
-        __opensvc_handle_reply
-        return
-    fi
-    __opensvc_debug "${FUNCNAME[0]}: c is $c words[c] is ${words[c]}"
-    if [[ "${words[c]}" == -* ]]; then
-        __opensvc_handle_flag
-    elif __opensvc_contains_word "${words[c]}" "${commands[@]}"; then
-        __opensvc_handle_command
-    elif [[ $c -eq 0 ]]; then
-        __opensvc_handle_command 
-    elif __opensvc_contains_word "${words[c]}" "${command_aliases[@]}"; then
-        # aliashash variable is an associative array which is only supported in bash > 3.
-        if [[ -z "${BASH_VERSION}" || "${BASH_VERSINFO[0]}" -gt 3 ]]; then
-            words[c]=${aliashash[${words[c]}]}
-            __opensvc_handle_command
-        else
-            __opensvc_handle_noun
-        fi
-    else
-        __opensvc_handle_noun
-    fi
-    __opensvc_handle_word
-}
-`,
-}
 
 func validArgs(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 	return listObjectPaths(), cobra.ShellCompDirectiveNoFileComp
