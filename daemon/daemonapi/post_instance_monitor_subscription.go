46bc0f6cd542252e36567c43498493a50d1146f3
Cyril Galibern <cyril.galibern@opensvc.com>
2022-12-06 12:53:00 +0100

    [daemon/daemonapi] fix missed return invalid duration value

--- daemon/daemonapi/post_instance_monitor_subscription.go
+++ daemon/daemonapi/post_instance_monitor_subscription.go
@@ -31,6 +31,7 @@ func (a *DaemonApi) PostInstanceMonitorSubscription(w http.ResponseWriter, r *ht
 		if v, err := converters.Duration.Convert(*params.Duration); err != nil {
 			log.Info().Err(err).Msgf("invalid duration: %s", *params.Duration)
 			sendError(w, http.StatusBadRequest, "invalid duration")
+			return
 		} else {
 			maxDuration = *v.(*time.Duration)
 		}
