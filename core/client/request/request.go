27ee359eb9f9d1fa07b8ea295cd93d2466af5b12
Cyril Galibern <cyril.galibern@opensvc.com>
2022-12-16 18:01:46 +0100

    [core/client/...] Allow multiple queryargs value for a key
    Now request.T defines Value as url.Values

--- core/client/request/request.go
+++ core/client/request/request.go
@@ -2,31 +2,31 @@ package request
 
 import (
 	"encoding/json"
+	"net/url"
 )
 
 type (
 	// T is a api request abstracting the protocol differences
 	T struct {
-		Method    string                 `json:"method,omitempty"`
-		Action    string                 `json:"action,omitempty"`
-		Node      string                 `json:"node,omitempty"`
-		Options   map[string]interface{} `json:"options,omitempty"`
-		QueryArgs map[string]string      `json:"query_args,omitempty"`
+		Method  string                 `json:"method,omitempty"`
+		Action  string                 `json:"action,omitempty"`
+		Node    string                 `json:"node,omitempty"`
+		Options map[string]interface{} `json:"options,omitempty"`
+		Values  url.Values             `json:"query_args,omitempty"`
 	}
 )
 
 // New allocates an unconfigured request and returns its
 // address.
 func New() *T {
-	r := &T{}
-	r.Options = make(map[string]interface{})
-	r.QueryArgs = make(map[string]string)
-	return r
+	return &T{
+		Options: make(map[string]interface{}),
+		Values:  url.Values{},
+	}
 }
 
 type Optioner interface {
 	GetAction() string
-	GetQueryArgs() map[string]string
 	GetNode() string
 	GetMethod() string
 }
@@ -42,7 +42,6 @@ func NewFor(options Optioner) *T {
 	r.Action = options.GetAction()
 	r.Node = options.GetNode()
 	r.Method = options.GetMethod()
-	r.QueryArgs = options.GetQueryArgs()
 	// convert options to the expected json format
 	if b, err = json.Marshal(options); err != nil {
 		return nil
