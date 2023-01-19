7b8f4302270a386f948b83d889ddb6df48ef965c
Cyril Galibern <cyril.galibern@opensvc.com>
2023-01-19 15:01:06 +0100

    [daemondata] Implements SetClusterConfig(value cluster.ClusterConfig) error
    SetClusterConfig sets .cluster.config:
        It publishes msgbus.DataUpdated for 'om mon' clients.
        It is not pushed to heatbeats

--- daemon/daemondata/stats.go
+++ daemon/daemondata/stats.go
@@ -48,6 +48,7 @@ const (
 	idGetNodesInfo
 	idGetServiceNames
 	idGetStatus
+	idSetClusterConfig
 	idSetHeartbeatPing
 	idSetSubHb
 	idSetInstanceConfig
@@ -83,6 +84,7 @@ var (
 		idGetNodesInfo:       "get-nodes-info",
 		idGetServiceNames:    "get-service-names",
 		idGetStatus:          "get-status",
+		idSetClusterConfig:   "set-cluster-config",
 		idSetSubHb:           "set-sub-hb",
 		idSetObjectStatus:    "set-object-status",
 		idSetInstanceConfig:  "set-instance-config",
