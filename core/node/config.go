9dc247d6028b00be932416f431a65204dc28df87
Cyril Galibern <cyril.galibern@opensvc.com>
2023-03-07 19:00:03 +0100

    [core/node] Add SplitAction attribute to Config

--- core/node/config.go
+++ core/node/config.go
@@ -8,6 +8,7 @@ type (
 		MaintenanceGracePeriod time.Duration `json:"maintenance_grace_period"`
 		ReadyPeriod            time.Duration `json:"ready_period"`
 		RejoinGracePeriod      time.Duration `json:"rejoin_grace_period"`
+		SplitAction            string        `json:"split_action"`
 	}
 )
 
