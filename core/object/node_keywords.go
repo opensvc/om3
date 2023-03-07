568244926a278b24c2f84ab99dc700480d36a857
Cyril Galibern <cyril.galibern@opensvc.com>
2023-03-07 18:57:41 +0100

    [core/object/node] node keyword split_action is now scopable

--- core/object/node_keywords.go
+++ core/object/node_keywords.go
@@ -686,6 +686,7 @@ The default ruser is root for all nodes. ruser accepts a list of user[@node] ...
 	{
 		Section:    "node",
 		Option:     "split_action",
+		Scopable:   true,
 		Candidates: []string{"crash", "reboot"},
 		Default:    "crash",
 		Text:       "Commit suicide method when cluster split occur. Default is crash. reboot method may be used instead of crash when it is not simple to poweron node after crash.",
