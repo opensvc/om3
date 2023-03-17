be25f32d338f92442d864140bbdd76c2acca4511
Cyril Galibern <cyril.galibern@opensvc.com>
2023-03-17 11:11:15 +0100

    [daemon/imon] Refactor non crm freeze/unfreeze to publish when no error
    Only publish msgbus.InstanceFrozenFile<Removed|Updated> when no error

--- daemon/imon/non_crm_action.go
+++ daemon/imon/non_crm_action.go
@@ -16,81 +16,67 @@ func (o *imon) getFrozen() time.Time {
 	return file.ModTime(filepath.Join(o.path.VarDir(), "frozen"))
 }
 
+// freeze creates missing instance frozen flag file, and publish InstanceFrozenFileUpdated
+// local instance status cache frozen value is updated with value read from file system
 func (o *imon) freeze() error {
-	p := filepath.Join(o.path.VarDir(), "frozen")
 	frozen := o.getFrozen()
-	if frozen.After(o.instStatus[o.localhost].Frozen) {
-		o.log.Warn().Msgf("freeze is called but frozen file already exists, refresh cache frozen from %s to %s",
-			o.instStatus[o.localhost].Frozen, frozen)
-		status := o.instStatus[o.localhost]
-		status.Frozen = frozen
-		o.instStatus[o.localhost] = status
-		o.pubsubBus.Pub(msgbus.InstanceFrozenFileUpdated{Path: o.path, Filename: p, Updated: frozen}, pubsub.Label{"path", o.path.String()})
-		return nil
-	}
 
 	o.log.Info().Msg("daemon action freeze")
-	d := filepath.Dir(p)
-	if !file.Exists(d) {
-		if err := os.MkdirAll(d, os.ModePerm); err != nil {
+	p := filepath.Join(o.path.VarDir(), "frozen")
+
+	if !file.Exists(p) {
+		d := filepath.Dir(p)
+		if !file.Exists(d) {
+			if err := os.MkdirAll(d, os.ModePerm); err != nil {
+				o.log.Error().Err(err).Msg("freeze")
+				return err
+			}
+		}
+		f, err := os.Create(p)
+		if err != nil {
 			o.log.Error().Err(err).Msg("freeze")
 			return err
 		}
+		_ = f.Close()
 	}
-	f, err := os.Create(p)
-	if err != nil {
-		o.log.Error().Err(err).Msg("freeze")
-		return err
-	}
-	_ = f.Close()
 	frozen = file.ModTime(p)
+	if instanceStatus, ok := o.instStatus[o.localhost]; ok {
+		instanceStatus.Frozen = frozen
+		o.instStatus[o.localhost] = instanceStatus
+	}
 	if frozen.IsZero() {
-		err := errors.Errorf("unable to retrieve mtime on %s", p)
+		err := errors.Errorf("unexpected frozen reset on %s", p)
 		o.log.Error().Err(err).Msg("freeze")
 		return err
 	}
-	status := o.instStatus[o.localhost]
-	status.Frozen = frozen
-	// don't wait for delayed update of local cache
-	o.instStatus[o.localhost] = status
-
-	// TODO don't publish and trust discover for this ?
-	o.pubsubBus.Pub(msgbus.InstanceFrozenFileUpdated{Path: o.path, Filename: p, Updated: frozen}, pubsub.Label{"path", o.path.String()})
+	o.pubsubBus.Pub(msgbus.InstanceFrozenFileUpdated{Path: o.path, Updated: frozen},
+		pubsub.Label{"node", o.localhost},
+		pubsub.Label{"path", o.path.String()},
+	)
 	return nil
 }
 
+// freeze removes instance frozen flag file, and publish InstanceFrozenFileUpdated
+// local instance status cache frozen value is updated with value read from file system
 func (o *imon) unfreeze() error {
+	o.log.Info().Msg("daemon action unfreeze")
 	p := filepath.Join(o.path.VarDir(), "frozen")
 	if !file.Exists(p) {
-		if !o.instStatus[o.localhost].Frozen.Equal(time.Time{}) {
-			o.log.Error().Msgf("unfreeze is called but frozen file is absent, clear previous frozen cache from %s",
-				o.instStatus[o.localhost].Frozen)
-			// don't wait for delayed update of local cache
-			// to avoid 'idle -> thawing -> idle -> thawing' until receive local instance status update
-			status := o.instStatus[o.localhost]
-			status.Frozen = time.Time{}
-			o.instStatus[o.localhost] = status
-
-			// TODO don't publish and trust discover for this ?
-			o.pubsubBus.Pub(msgbus.InstanceFrozenFileRemoved{Path: o.path, Filename: p, Updated: time.Now()}, pubsub.Label{"path", o.path.String()})
+		o.log.Info().Msg("already thawed")
+	} else {
+		err := os.Remove(p)
+		if err != nil {
+			o.log.Error().Err(err).Msg("unfreeze")
+			return err
 		}
-		return nil
 	}
-	o.log.Info().Msg("daemon action unfreeze")
-	now := time.Now()
-	err := os.Remove(p)
-	if err != nil {
-		o.log.Error().Err(err).Msg("unfreeze")
-		return err
+	if instanceStatus, ok := o.instStatus[o.localhost]; ok {
+		instanceStatus.Frozen = time.Time{}
+		o.instStatus[o.localhost] = instanceStatus
 	}
-
-	// don't wait for delayed update of local cache
-	// to avoid 'idle -> thawing -> idle -> thawing' until receive local instance status update
-	status := o.instStatus[o.localhost]
-	status.Frozen = time.Time{}
-	o.instStatus[o.localhost] = status
-
-	// TODO don't publish and trust discover for this ?
-	o.pubsubBus.Pub(msgbus.InstanceFrozenFileRemoved{Path: o.path, Filename: p, Updated: now}, pubsub.Label{"path", o.path.String()})
+	o.pubsubBus.Pub(msgbus.InstanceFrozenFileRemoved{Path: o.path, Updated: time.Now()},
+		pubsub.Label{"node", o.localhost},
+		pubsub.Label{"path", o.path.String()},
+	)
 	return nil
 }
