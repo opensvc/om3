7ad91f9c247842b4dbfe39238f98dd91b8f90dd8
Cyril Galibern <cyril.galibern@opensvc.com>
2023-05-11 01:51:51 +0200

    [daemon/imon] clear boot failed instance under calm state
    boot failed state are cleared when the following conditions are met
    + object avail is UP
    + local avail is Down, StandbyDown, NotApplicable
    + global expect is none

--- daemon/imon/orchestration_ha.go
+++ daemon/imon/orchestration_ha.go
@@ -8,6 +8,7 @@ import (
 
 func (o *imon) orchestrateNone() {
 	o.clearStartFailed()
+	o.clearBootFailed()
 	if o.objStatus.Orchestrate == "ha" {
 		o.orchestrateHAStart()
 		o.orchestrateHAStop()
@@ -48,6 +49,37 @@ func (o *imon) orchestrateHAStart() {
 		return
 	}
 	o.orchestrateStarted()
+}
+
+// clearBootFailed clears the boot failed state when the following conditions are met:
+//
+// + object avail is UP
+// + local avail is Down, StandbyDown, NotApplicable
+// + global expect is none
+func (o *imon) clearBootFailed() {
+	if o.state.State != instance.MonitorStateBootFailed {
+		return
+	}
+	if o.objStatus.Avail != status.Up {
+		return
+	}
+	switch o.instStatus[o.localhost].Avail {
+	case status.Down:
+	case status.StandbyDown:
+	case status.NotApplicable:
+	default:
+		return
+	}
+	for _, instanceMonitor := range o.instMonitor {
+		switch instanceMonitor.GlobalExpect {
+		case instance.MonitorGlobalExpectNone:
+		default:
+			return
+		}
+	}
+	o.log.Info().Msgf("clear instance %s: local instance avail is %s, object avail is %s",
+		o.state.State, o.instStatus[o.localhost].Avail, o.objStatus.Avail)
+	o.transitionTo(instance.MonitorStateIdle)
 }
 
 func (o *imon) clearStartFailed() {
