610065e628aa20c2712566fd947806283c009e9e
Christophe Varoqui <christophe.varoqui@opensvc.com>
2023-06-02 11:28:44 +0200

    Fix embeding of the VERSION file
    Embed the text/ subdir.
    Add a text/README to satisfy the embed not accepting empty vfs.
    Use this text/README to document the role of text/VERSION if it exists.

--- util/version/main.go
+++ util/version/main.go
@@ -1,28 +1,17 @@
 package version
 
 import (
-	_ "embed"
+	"embed"
 	"runtime/debug"
 	"strings"
 )
 
 var (
-	//go:embed VERSION
-	ExplicitVersion string
+	//go:embed text
+	fs embed.FS
 )
 
-// Version returns a string containing either the content of
-// util/version/VERSION (which is created or updated by the
-// packaging automation), or the build info release (ie the
-// git HEAD commit id at build time).
-//
-// Example VERSION generation:
-//
-//	git describe --tags >util/version/VERSION
-func Version() string {
-	if ExplicitVersion != "" {
-		return strings.TrimSpace(ExplicitVersion)
-	}
+func BuildVersion() string {
 	if info, ok := debug.ReadBuildInfo(); ok {
 		for _, setting := range info.Settings {
 			if setting.Key == "vcs.revision" {
@@ -31,4 +20,28 @@ func Version() string {
 		}
 	}
 	return ""
+}
+
+func ExplicitVersion() string {
+	if b, err := fs.ReadFile("text/VERSION"); err == nil {
+		s := string(b)
+		return strings.TrimSpace(s)
+	}
+	return ""
+}
+
+// Version returns a string containing either the content of
+// util/version/text/VERSION (which is created or updated by the
+// packaging automation), or the build info release (ie the git
+// HEAD commit id at build time).
+//
+// Example VERSION generation:
+//
+//	git describe --tags >util/version/VERSION
+func Version() string {
+	if v := ExplicitVersion(); v != "" {
+		return v
+	} else {
+		return BuildVersion()
+	}
 }
