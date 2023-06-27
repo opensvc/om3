1c80cb5b3c29bb38a71bba5a71330fbe7c1da806
Christophe Varoqui <christophe.varoqui@opensvc.com>
2023-06-27 09:17:20 +0200

    Remove useless condition in the event renderer
    And make it a event.Event attached func.

--- core/event/render.go
+++ core/event/render.go
@@ -1,19 +1,15 @@
 package event
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/opensvc/om3/core/output"
 )
 
 // Render formats a opensvc agent event
-func Render(e Event) string {
-	s := fmt.Sprintf("%s [%d] %s", e.Time, e.ID, e.Kind)
-	if e.Kind == "event" {
-		s += output.SprintFlat(e.Data)
-	} else if len(e.Data) > 0 {
-		s += "\n  " + string(e.Data)
-	}
+func (e Event) Render() string {
+	s := fmt.Sprintf("%s [%d] %s\n", e.Time, e.ID, e.Kind)
+	b, _ := json.Marshal(e.Data)
+	s += fmt.Sprintf("  %s\n", string(b))
 	return s
 }
 
