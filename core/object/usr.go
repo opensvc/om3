e415a6fe154a049e9e2858d98e3d1e4f4e9829ea
Cyril Galibern <cyril.galibern@opensvc.com>
2023-07-06 20:01:21 +0200

    [core/object] UsrDB implements daemonauth.UserGranter for basic auth strategy

--- core/object/usr.go
+++ core/object/usr.go
@@ -1,7 +1,11 @@
 package object
 
 import (
+	"fmt"
+
 	"github.com/opensvc/om3/core/keywords"
+	"github.com/opensvc/om3/core/kind"
+	"github.com/opensvc/om3/core/path"
 	"github.com/opensvc/om3/util/funcopt"
 	"github.com/opensvc/om3/util/key"
 )
@@ -21,6 +25,9 @@ type (
 	Usr interface {
 		Sec
 	}
+
+	// UsrDB implements UserGrants to authenticate user and get its grants
+	UsrDB struct{}
 )
 
 // NewUsr allocates a usr kind object.
@@ -37,4 +44,21 @@ func NewUsr(p any, opts ...funcopt.O) (*usr, error) {
 
 func (t usr) KeywordLookup(k key.T, sectionType string) keywords.Keyword {
 	return keywordLookup(keywordStore, k, t.path.Kind, sectionType)
+}
+
+// UserGrants returns grants for username if username and password match existing usr object
+func (_ *UsrDB) UserGrants(username, password string) ([]string, error) {
+	usrPath := path.T{Name: username, Namespace: "system", Kind: kind.Usr}
+	user, err := NewUsr(usrPath, WithVolatile(true))
+	if err != nil {
+		return nil, err
+	}
+	storedPassword, err := user.DecodeKey("password")
+	if err != nil {
+		return nil, fmt.Errorf("read password from %s: %w", usrPath, err)
+	}
+	if string(storedPassword) != password {
+		return nil, fmt.Errorf("wrong password")
+	}
+	return user.Config().GetStrings(key.T{Section: "DEFAULT", Option: "grant"}), nil
 }
