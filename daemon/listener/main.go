b669632e66b8a7c4d891c22e2092fd878e3e2579
Cyril Galibern <cyril.galibern@opensvc.com>
2023-07-06 20:11:50 +0200

    [daemon/listener] Implements methods to enable all daemonauth strategies [SOLID]
    authOption implements AllStrategieser so all daemonauth strategies are enabled.

--- daemon/listener/main.go
+++ daemon/listener/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/opensvc/om3/core/object"
 	"github.com/opensvc/om3/daemon/ccfg"
 	"github.com/opensvc/om3/daemon/daemonauth"
+	"github.com/opensvc/om3/daemon/daemonctx"
 	"github.com/opensvc/om3/daemon/daemonenv"
 	"github.com/opensvc/om3/daemon/enable"
 	"github.com/opensvc/om3/daemon/listener/lsnrhttpinet"
@@ -50,7 +51,29 @@ type (
 		new        func(t *T) subdaemon.Manager
 		subActions subdaemon.Manager
 	}
+
+	// authOption implements interfaces for daemonauth.Init
+	authOption struct {
+		*ccfg.NodeDB
+		*object.UsrDB
+	}
 )
+
+func (a *authOption) ListenAddr(ctx context.Context) string {
+	return daemonctx.ListenAddr(ctx)
+}
+
+func (a *authOption) X509CACertFile() string {
+	return daemonenv.CAsCertFile()
+}
+
+func (a *authOption) SignKeyFile() string {
+	return daemonenv.CAKeyFile()
+}
+
+func (a *authOption) VerifyKeyFile() string {
+	return daemonenv.CAsCertFile()
+}
 
 func getMandatorySub() map[string]sub {
 	clusterConfig := ccfg.Get()
@@ -127,7 +150,7 @@ func (t *T) MainStart(ctx context.Context) error {
 	if err := startCertFS(); err != nil {
 		t.log.Err(err).Msgf("start certificates volatile fs")
 	}
-	if err := daemonauth.Init(); err != nil {
+	if err := daemonauth.InitStategies(&authOption{}); err != nil {
 		return err
 	}
 	daemonenv.HttpPort = node.Config().GetInt(key.New("listener", "tls_port"))
