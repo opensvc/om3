33447b81f42f69a6554b2ea40e06ce2ba8a1582a
Christophe Varoqui <christophe.varoqui@opensvc.com>
2023-08-21 09:29:57 +0200

    Add a test for path.Relation.Split()

--- core/path/path_test.go
+++ core/path/path_test.go
@@ -138,6 +138,48 @@ func TestL_len(t *testing.T) {
 	assert.Equal(t, 0, len(l))
 }
 
+func TestParseRelation(t *testing.T) {
+	tests := map[string]struct {
+		objectPath string
+		node       string
+		ok         bool
+	}{
+		"svc1": {
+			objectPath: "svc1",
+			node:       "",
+			ok:         true,
+		},
+		"svc1@": {
+			objectPath: "svc1",
+			node:       "",
+			ok:         true,
+		},
+		"svc1@n1": {
+			objectPath: "svc1",
+			node:       "n1",
+			ok:         true,
+		},
+		"svc1@n1@n1": {
+			objectPath: "svc1",
+			node:       "n1@n1",
+			ok:         true,
+		},
+	}
+	for input, test := range tests {
+		t.Logf("input: '%s'", input)
+		objectPath, node, err := Relation(input).Split()
+		switch test.ok {
+		case true:
+			assert.Nil(t, err)
+		case false:
+			assert.NotNil(t, err)
+			continue
+		}
+		assert.Equal(t, test.objectPath, objectPath.String())
+		assert.Equal(t, test.node, node)
+	}
+}
+
 func TestParse(t *testing.T) {
 	tests := map[string]struct {
 		name      string
