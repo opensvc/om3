84a4fb099062eb4e10f117145a73f847ee8b5d0a
Cyril Galibern <cyril.galibern@opensvc.com>
2023-08-25 18:52:46 +0200

    [cli] Enable systemd actions during 'om daemon {stop,start,restart}'

--- core/commands/daemon_start.go
+++ core/commands/daemon_start.go
@@ -6,8 +6,10 @@ import (
 	"runtime/pprof"
 	"time"
 
+	"github.com/opensvc/om3/core/client"
 	"github.com/opensvc/om3/daemon/daemoncli"
 	"github.com/opensvc/om3/util/command"
+	"github.com/opensvc/om3/util/systemd"
 )
 
 type (
@@ -25,40 +27,74 @@ func (t *CmdDaemonStart) Run() error {
 		return err
 	}
 	if t.Foreground {
-		if t.CpuProfile != "" {
-			f, err := os.Create(t.CpuProfile)
-			if err != nil {
-				return fmt.Errorf("create CPU profile: %w", err)
-			}
-			defer f.Close() // error handling omitted for example
-			if err := pprof.StartCPUProfile(f); err != nil {
-				return fmt.Errorf("start CPU profile: %w", err)
-			}
-			defer pprof.StopCPUProfile()
-		}
-		if err := daemoncli.New(cli).Start(); err != nil {
-			return fmt.Errorf("start daemon cli: %w", err)
-		}
+		return t.runForeground(cli)
+	} else if os.Getenv("INVOCATION_ID") != "" {
+		// called from systemd
+		cmd := t.startNativeCmd()
+		return t.runBackground(cli, cmd)
 	} else {
-		args := []string{"daemon", "start", "--foreground"}
-		if t.Log != "" {
-			args = append(args, "--log", t.Log)
+		// not called from systemd
+		var cmd *command.T
+		if systemd.HasSystemd() {
+			// systemd is running, delegate to systemd
+			cmd = t.startUnitCmd()
+			return cmd.Run()
+		} else {
+			// systemd is not running, start background
+			cmd = t.startNativeCmd()
+			return t.runBackground(cli, cmd)
 		}
-		if t.Server != "" {
-			args = append(args, "--server", t.Server)
+	}
+}
+
+func (t *CmdDaemonStart) runBackground(cli *client.T, cmd *command.T) error {
+	checker := func() error {
+		time.Sleep(60 * time.Millisecond)
+		if err := daemoncli.New(cli).WaitRunning(); err != nil {
+			return fmt.Errorf("daemon not running: %w", err)
 		}
-		cmd := command.New(
-			command.WithName(os.Args[0]),
-			command.WithArgs(args),
-		)
-		checker := func() error {
-			time.Sleep(60 * time.Millisecond)
-			if err := daemoncli.New(cli).WaitRunning(); err != nil {
-				return fmt.Errorf("daemon not running: %w", err)
-			}
-			return nil
+		return nil
+	}
+	daemoncli.LockCmdExit(cmd, checker, "daemon start")
+	return nil
+}
+
+func (t *CmdDaemonStart) runForeground(cli *client.T) error {
+	if t.CpuProfile != "" {
+		f, err := os.Create(t.CpuProfile)
+		if err != nil {
+			return fmt.Errorf("create CPU profile: %w", err)
 		}
-		daemoncli.LockCmdExit(cmd, checker, "daemon start")
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			return fmt.Errorf("start CPU profile: %w", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+	if err := daemoncli.New(cli).Start(); err != nil {
+		return fmt.Errorf("start daemon cli: %w", err)
 	}
 	return nil
+}
+
+func (t *CmdDaemonStart) startUnitCmd() *command.T {
+	return command.New(
+		command.WithName("systemctl"),
+		command.WithVarArgs("start", "opensvc-agent"),
+	)
+}
+
+func (t *CmdDaemonStart) startNativeCmd() *command.T {
+	args := []string{"daemon", "start", "--foreground"}
+	if t.Log != "" {
+		args = append(args, "--log", t.Log)
+	}
+	if t.Server != "" {
+		args = append(args, "--server", t.Server)
+	}
+	cmd := command.New(
+		command.WithName(os.Args[0]),
+		command.WithArgs(args),
+	)
+	return cmd
 }
