0ae41d23bcdbaa9b1746cc897b2e55769b831f44
Cyril Galibern <cyril.galibern@opensvc.com>
2023-08-25 18:50:49 +0200

    [util/systemd] Refactor HasSystemd for unprivileged users

--- util/systemd/main.go
+++ util/systemd/main.go
@@ -2,20 +2,14 @@
 
 package systemd
 
-import "os"
-
-var (
-	procOneComm = "/proc/1/comm"
+import (
+	"os"
 )
 
 // HasSystemd return true if systemd is detected on current os
 func HasSystemd() bool {
-	var (
-		b   []byte
-		err error
-	)
-	if b, err = os.ReadFile(procOneComm); err != nil {
+	if _, err := os.Stat("/run/systemd/system"); err != nil {
 		return false
 	}
-	return string(b) == "systemd\n"
+	return true
 }
