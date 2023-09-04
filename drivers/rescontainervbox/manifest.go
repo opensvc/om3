77c08ec94765fbedbea41ea78d9052a04dc80925
Rémi Mendes <remi.mendes@opensvc.com>
2023-09-04 10:18:46 +0200

    delete unnecessary comments

--- drivers/rescontainervbox/manifest.go
+++ drivers/rescontainervbox/manifest.go
@@ -29,26 +29,6 @@ func (t T) Manifest() *manifest.T {
 		manifest.ContextPeers,
 		manifest.ContextDNS,
 		manifest.ContextTopology,
-		/*
-			keywords.Keyword{
-				Option:       "snap",
-				Attr:         "Snap",
-				Scopable:     true,
-				Provisioning: true,
-				Text:         "If this keyword is set, the service configures a resource-private container data store. This setup is allows stateful service relocalization.",
-				Text: keywords.NewText(fs, "text/kw/snap"),
-				Example:      "/srv/svc1/data/containers",
-			},
-			keywords.Keyword{
-				Option:       "snapof",
-				Attr:         "SnapOf",
-				Scopable:     true,
-				Provisioning: true,
-				Text:         "Sets the root fs directory of the container",
-				Text: keywords.NewText(fs, "text/kw/snapof"),
-				Example:      "/srv/svc1/data/containers",
-			},
-		*/
 		rescontainer.KWRCmd,
 		rescontainer.KWName,
 		rescontainer.KWHostname,
