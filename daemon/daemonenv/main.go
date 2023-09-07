f4a7b940880a8687c3dd4e891bdb4ea2080074d0
Cyril Galibern <cyril.galibern@opensvc.com>
2023-09-07 12:34:56 +0200

    [daemonenv] Cleanup dead code

--- daemon/daemonenv/main.go
+++ daemon/daemonenv/main.go
@@ -10,11 +10,7 @@ import (
 )
 
 var (
-	RawPort  = 1214
 	HttpPort = 1215
-
-	HeaderNode        = "o-node"
-	HeaderMultiplexed = "o-multiplexed"
 
 	DrainChanDuration = 40 * time.Millisecond
 
@@ -54,10 +50,6 @@ func KeyFile() string {
 	return filepath.Join(rawconfig.Paths.Certs, "private_key")
 }
 
-func UrlInetRaw() string {
-	return fmt.Sprintf("raw://localhost:%d", RawPort)
-}
-
 func UrlInetHttp() string {
 	return fmt.Sprintf("https://localhost:%d", HttpPort)
 }
@@ -66,20 +58,12 @@ func UrlHttpNode(node string) string {
 	return fmt.Sprintf("https://%s:%d", node, HttpPort)
 }
 
-func UrlUxRaw() string {
-	return "raw://" + PathUxRaw()
-}
-
 func UrlUxHttp() string {
 	return "http://" + PathUxHttp()
 }
 
 func PathUxProfile() string {
 	return filepath.Join(rawconfig.Paths.Lsnr, "profile.sock")
-}
-
-func PathUxRaw() string {
-	return filepath.Join(rawconfig.Paths.Lsnr, "lsnr.sock")
 }
 
 func PathUxHttp() string {
