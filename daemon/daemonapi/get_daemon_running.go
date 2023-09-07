7834130fd71c633a37dd42b6f27b8ba89f3617f2
Cyril Galibern <cyril.galibern@opensvc.com>
2023-09-07 12:32:12 +0200

    [daemonapi] Don't anymore use dispatchhandler during GET /daemon/running
    TODO: verify api.yaml

--- daemon/daemonapi/get_daemon_running.go
+++ daemon/daemonapi/get_daemon_running.go
@@ -1,23 +1,11 @@
 package daemonapi
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-
-	"github.com/opensvc/om3/daemon/handlers/dispatchhandler"
 )
 
 func (a *DaemonApi) GetDaemonRunning(ctx echo.Context) error {
-	// TODO verify if drop support of dispatchhandler
-	dispatchhandler.New(a.getDaemonRunning, http.StatusOK, 1)(ctx.Response(), ctx.Request())
-	return nil
-}
-
-func (a *DaemonApi) getDaemonRunning(w http.ResponseWriter, r *http.Request) {
-	running := a.Daemon.Running()
-	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(running)
-	w.WriteHeader(http.StatusOK)
+	return ctx.JSON(http.StatusOK, true)
 }
