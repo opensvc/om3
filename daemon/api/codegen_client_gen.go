4987a0f7474254fb2e0b48a213532ee52f0a572d
Cyril Galibern <cyril.galibern@opensvc.com>
2023-09-13 14:20:43 +0200

    [openapi] Implement POST /daemon/restart

--- daemon/api/codegen_client_gen.go
+++ daemon/api/codegen_client_gen.go
@@ -118,6 +118,9 @@ type ClientInterface interface {
 
 	PostDaemonLogsControl(ctx context.Context, body PostDaemonLogsControlJSONRequestBody, reqEditors ...RequestEditorFn) (*http.Response, error)
 
+	// PostDaemonRestart request
+	PostDaemonRestart(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
+
 	// GetDaemonRunning request
 	GetDaemonRunning(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
 
@@ -410,6 +413,18 @@ func (c *Client) PostDaemonLogsControlWithBody(ctx context.Context, contentType 
 
 func (c *Client) PostDaemonLogsControl(ctx context.Context, body PostDaemonLogsControlJSONRequestBody, reqEditors ...RequestEditorFn) (*http.Response, error) {
 	req, err := NewPostDaemonLogsControlRequest(c.Server, body)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+func (c *Client) PostDaemonRestart(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewPostDaemonRestartRequest(c.Server)
 	if err != nil {
 		return nil, err
 	}
@@ -1584,6 +1599,33 @@ func NewPostDaemonLogsControlRequestWithBody(server string, contentType string, 
 	}
 
 	req.Header.Add("Content-Type", contentType)
+
+	return req, nil
+}
+
+// NewPostDaemonRestartRequest generates requests for PostDaemonRestart
+func NewPostDaemonRestartRequest(server string) (*http.Request, error) {
+	var err error
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/daemon/restart")
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", queryURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
 
 	return req, nil
 }
@@ -4008,6 +4050,9 @@ type ClientWithResponsesInterface interface {
 
 	PostDaemonLogsControlWithResponse(ctx context.Context, body PostDaemonLogsControlJSONRequestBody, reqEditors ...RequestEditorFn) (*PostDaemonLogsControlResponse, error)
 
+	// PostDaemonRestart request
+	PostDaemonRestartWithResponse(ctx context.Context, reqEditors ...RequestEditorFn) (*PostDaemonRestartResponse, error)
+
 	// GetDaemonRunning request
 	GetDaemonRunningWithResponse(ctx context.Context, reqEditors ...RequestEditorFn) (*GetDaemonRunningResponse, error)
 
@@ -4413,6 +4458,31 @@ func (r PostDaemonLogsControlResponse) Status() string {
 
 // StatusCode returns HTTPResponse.StatusCode
 func (r PostDaemonLogsControlResponse) StatusCode() int {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.StatusCode
+	}
+	return 0
+}
+
+type PostDaemonRestartResponse struct {
+	Body         []byte
+	HTTPResponse *http.Response
+	JSON200      *Problem
+	JSON401      *Problem
+	JSON403      *Problem
+	JSON500      *Problem
+}
+
+// Status returns HTTPResponse.Status
+func (r PostDaemonRestartResponse) Status() string {
+	if r.HTTPResponse != nil {
+		return r.HTTPResponse.Status
+	}
+	return http.StatusText(0)
+}
+
+// StatusCode returns HTTPResponse.StatusCode
+func (r PostDaemonRestartResponse) StatusCode() int {
 	if r.HTTPResponse != nil {
 		return r.HTTPResponse.StatusCode
 	}
@@ -5778,6 +5848,15 @@ func (c *ClientWithResponses) PostDaemonLogsControlWithResponse(ctx context.Cont
 	return ParsePostDaemonLogsControlResponse(rsp)
 }
 
+// PostDaemonRestartWithResponse request returning *PostDaemonRestartResponse
+func (c *ClientWithResponses) PostDaemonRestartWithResponse(ctx context.Context, reqEditors ...RequestEditorFn) (*PostDaemonRestartResponse, error) {
+	rsp, err := c.PostDaemonRestart(ctx, reqEditors...)
+	if err != nil {
+		return nil, err
+	}
+	return ParsePostDaemonRestartResponse(rsp)
+}
+
 // GetDaemonRunningWithResponse request returning *GetDaemonRunningResponse
 func (c *ClientWithResponses) GetDaemonRunningWithResponse(ctx context.Context, reqEditors ...RequestEditorFn) (*GetDaemonRunningResponse, error) {
 	rsp, err := c.GetDaemonRunning(ctx, reqEditors...)
@@ -6765,6 +6844,53 @@ func ParsePostDaemonLogsControlResponse(rsp *http.Response) (*PostDaemonLogsCont
 	}
 
 	response := &PostDaemonLogsControlResponse{
+		Body:         bodyBytes,
+		HTTPResponse: rsp,
+	}
+
+	switch {
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 200:
+		var dest Problem
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON200 = &dest
+
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 401:
+		var dest Problem
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON401 = &dest
+
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 403:
+		var dest Problem
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON403 = &dest
+
+	case strings.Contains(rsp.Header.Get("Content-Type"), "json") && rsp.StatusCode == 500:
+		var dest Problem
+		if err := json.Unmarshal(bodyBytes, &dest); err != nil {
+			return nil, err
+		}
+		response.JSON500 = &dest
+
+	}
+
+	return response, nil
+}
+
+// ParsePostDaemonRestartResponse parses an HTTP response from a PostDaemonRestartWithResponse call
+func ParsePostDaemonRestartResponse(rsp *http.Response) (*PostDaemonRestartResponse, error) {
+	bodyBytes, err := io.ReadAll(rsp.Body)
+	defer func() { _ = rsp.Body.Close() }()
+	if err != nil {
+		return nil, err
+	}
+
+	response := &PostDaemonRestartResponse{
 		Body:         bodyBytes,
 		HTTPResponse: rsp,
 	}
