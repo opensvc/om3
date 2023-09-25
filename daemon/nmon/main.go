2b1059df02eaae0dbcd99524d9fefaccc8c2369a
Cyril Galibern <cyril.galibern@opensvc.com>
2023-09-25 18:44:21 +0200

    [daemon/nmon] Skip getStats on non Linux

--- daemon/nmon/main.go
+++ daemon/nmon/main.go
@@ -27,6 +27,7 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"runtime"
 	"sync"
 	"time"
 
@@ -523,6 +524,10 @@ func (o *nmon) clearPending() {
 
 func (o *nmon) getStats() (node.Stats, error) {
 	stats := node.Stats{}
+	if runtime.GOOS != "linux" {
+		return stats, nil
+	}
+
 	fs, err := procfs.NewDefaultFS()
 	if err != nil {
 		return stats, err
