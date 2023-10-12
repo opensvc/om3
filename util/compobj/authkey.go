a909b98ef5edb414cde13238a090e334dd162061
Rémi Mendes <remi.mendes@opensvc.com>
2023-10-12 16:45:30 +0200

    [compobj] adding the add function in authkey.go and its associated test

---
+++ util/compobj/authkey.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type (
+	CompAuthkeys struct {
+		*Obj
+	}
+	CompAuthKey struct {
+		Action   string `json:"action"`
+		Authfile string `json:"authfile"`
+		User     string `json:"user"`
+		Key      string `json:"key"`
+		Port     *int   `json:"port"`
+	}
+)
+
+var compAuthKeyInfo = ObjInfo{
+	DefaultPrefix: "OSVC_COMP_AUTHKEY_",
+	ExampleValue: CompAuthKey{
+		Action:   "add",
+		Authfile: "authorized_keys",
+		User:     "testuser",
+		Key:      "ssh-dss AAAAB3NzaC1kc3MAAACBAPiO1jlT+5yrdPLfQ7sYF52NkfCEzT0AUUNIl+14Sbkubqe+TcU7U3taUtiDJ5YOGOzIVFIDGGtwD0AqNHQbvsiS1ywtC5BJ9362FlrpVH4o1nVZPvMxRzz5hgh3HjxqIWqwZDx29qO8Rg1/g1Gm3QYCxqPFn2a5f2AUiYqc1wtxAAAAFQC49iboZGNqssicwUrX6TUrT9H0HQAAAIBo5dNRmTF+Vd/+PI0JUOIzPJiHNKK9rnySlaxSDml9hH2LuDSjYz7BWuNP8UnPOa2pcFA4meDp5u8d5dGOWxkuYO0bLnXwDZuHtDW/ySytjwEaBLPxoqRBAyfyQNlusGsuiqDYRA7j7bS0RxINBxvDw79KdyQhuOn8/lKVG+sjrQAAAIEAoShly/JlGLQxQzPyWADV5RFlaRSPaPvFzcYT3hS+glkVd6yrCbzc30Yc8Ndu4cflQiXSZzRoUMgsy5PzuiH1M8JjwHTGNl8r9OfJpnN/OaAhMpIyA06y1ZZD9iEME3UmthFQoZnfRuE3yxi7bqyXJU4rOq04iyCTpU1UKInPdXQ= testuser",
+	},
+	Description: `* Installs or removes ssh public keys from authorized_key files
+* Looks up the authorized_key and authorized_key2 file location in the running sshd daemon configuration.
+* Add user to sshd_config AllowUser and AllowGroup if used
+* Reload sshd if sshd_config has been changed
+`,
+	FormDefinition: `Desc: |
+  Describe a list of ssh public keys to authorize login as the specified Unix user.
+Css: comp48
+
+Outputs:
+  -
+    Dest: compliance variable
+    Type: json
+    Format: dict
+    Class: authkey
+
+Inputs:
+  -
+    Id: action
+    Label: Action
+    DisplayModeLabel: action
+    LabelCss: action16
+    Mandatory: Yes
+    Type: string
+    Candidates:
+      - add
+      - del
+    Help: Defines wether the public key must be installed or uninstalled.
+
+  -
+    Id: user
+    Label: User
+    DisplayModeLabel: user
+    LabelCss: guy16
+    Mandatory: Yes
+    Type: string
+    Help: Defines the Unix user name who will accept those ssh public keys.
+
+  -
+    Id: key
+    Label: Public key
+    DisplayModeLabel: key
+    LabelCss: guy16
+    Mandatory: Yes
+    Type: text
+    DisplayModeTrim: 60
+    Help: The ssh public key as seen in authorized_keys files.
+
+  -
+    Id: authfile
+    Label: Authorized keys file name
+    DisplayModeLabel: authfile
+    LabelCss: hd16
+    Mandatory: Yes
+    Candidates:
+      - authorized_keys
+      - authorized_keys2
+    Default: authorized_keys2
+    Type: string
+    Help: The authorized_keys file to write the keys into.
+
+  -
+    Id: port
+    Label: Port
+    DisplayModeLabel: port
+    LabelCss: hd16
+    Mandatory: no
+    Default: 22
+    Type: int
+    Help: The port used by sshd
+`,
+}
+
+func init() {
+	m["authkey"] = NewCompAutkey
+}
+
+func NewCompAutkey() interface{} {
+	return &CompAuthkeys{
+		Obj: NewObj(),
+	}
+}
+
+func (t *CompAuthkeys) Add(s string) error {
+	var data CompAuthKey
+	if err := json.Unmarshal([]byte(s), &data); err != nil {
+		return err
+	}
+	if !(data.Action == "add" || data.Action == "del") {
+		t.Errorf("action should be equal to add or del in the dict: %s\n", s)
+		return fmt.Errorf("action should be equal to add or del in the dict: %s\n", s)
+	}
+	if !(data.Authfile == "authorized_keys" || data.Authfile == "authorized_keys2") {
+		t.Errorf("authfile should equal to authorized_keys or authorized_keys2 in the dict: %s\n", s)
+		return fmt.Errorf("authfile should equal to authorized_keys or authorized_keys2 in the dict: %s\n", s)
+	}
+	if data.User == "" {
+		t.Errorf("user should be in the dict: %s\n", s)
+		return fmt.Errorf("user should be in the dict: %s\n", s)
+	}
+	if data.Key == "" {
+		t.Errorf("key should be in the dict: %s\n", s)
+		return fmt.Errorf("user should be in the dict: %s\n", s)
+	}
+	if data.Port == nil {
+		data.Port = pti(22)
+	}
+	t.Obj.Add(data)
+	return nil
+}
+
+func (t CompAuthkeys) CheckRule(rule CompAuthKey) ExitCode {
+
+	return ExitOk
+}
+
+func (t CompAuthkeys) Check() ExitCode {
+	t.SetVerbose(true)
+	e := ExitOk
+	for _, i := range t.Rules() {
+		rule := i.(CompAuthKey)
+		o := t.CheckRule(rule)
+		e = e.Merge(o)
+	}
+	return e
+}
+
+func (t CompAuthkeys) Fix() ExitCode {
+	/*t.SetVerbose(false)
+	for _, i := range t.Rules() {
+		rule := i.(CompSymlink)
+		if e := t.FixSymlink(rule); e == ExitNok {
+			return ExitNok
+		}
+	}*/
+	return ExitOk
+}
+
+func (t CompAuthkeys) Fixable() ExitCode {
+	return ExitNotApplicable
+}
+
+func (t CompAuthkeys) Info() ObjInfo {
+	return compAuthKeyInfo
+}
---
+++ util/compobj/authkey_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAuthkeyAdd(t *testing.T) {
+	testCases := map[string]struct {
+		json         string
+		expecteError bool
+		expectedRule CompAuthKey
+	}{
+		"with a full add action rule and authfile equal to authorized_keys": {
+			json:         `{"action":"add", "authfile":"authorized_keys", "authdir":"/dir/", "user":"toto", "key":"totokey","port":22}`,
+			expecteError: false,
+			expectedRule: CompAuthKey{
+				Action:   "add",
+				Authfile: "authorized_keys",
+				User:     "toto",
+				Key:      "totokey",
+				Port:     pti(22),
+			},
+		},
+
+		"with a full del action rule authfile equal to authorized_keys2": {
+			json:         `{"action":"del", "authfile":"authorized_keys2", "authdir":"/dir/", "user":"toto", "key":"totokey","port":22}`,
+			expecteError: false,
+			expectedRule: CompAuthKey{
+				Action:   "del",
+				Authfile: "authorized_keys2",
+				User:     "toto",
+				Key:      "totokey",
+				Port:     pti(22),
+			},
+		},
+
+		"with an action that is not correct (not del or add)": {
+			json:         `{"action":"lalaal", "authfile":"authorized_keys", "authdir":"/dir", "user":"toto", "key":"totokey"}`,
+			expecteError: true,
+			expectedRule: CompAuthKey{},
+		},
+
+		"json rule with no authfile": {
+			json:         `{"action":"add", "authfile":"", "authdir":"/dir", "user":"toto", "key":"totokey"}`,
+			expecteError: true,
+			expectedRule: CompAuthKey{},
+		},
+
+		"json rule with no user": {
+			json:         `{"action":"add", "authfile":"authorized_keys", "authdir":"/dir", "user":"", "key":"totokey"}`,
+			expecteError: true,
+			expectedRule: CompAuthKey{},
+		},
+
+		"json rule with no key": {
+			json:         `{"action":"add", "authfile":"authorized_keys", "authdir":"/dir", "user":"toto", "key":""}`,
+			expecteError: true,
+			expectedRule: CompAuthKey{},
+		},
+
+		"json rule with a false authfile field (not equal to authorized_keys or authorized_keys2": {
+			json:         `{"action":"add", "authfile":"lalala", "authdir":"/dir", "user":"", "key":"totokey"}`,
+			expecteError: true,
+			expectedRule: CompAuthKey{},
+		},
+
+		"json rule with no port precised": {
+			json:         `{"action":"add", "authfile":"authorized_keys", "authdir":"/dir/", "user":"toto", "key":"totokey"}`,
+			expecteError: false,
+			expectedRule: CompAuthKey{
+				Action:   "add",
+				Authfile: "authorized_keys",
+				User:     "toto",
+				Key:      "totokey",
+				Port:     pti(22),
+			},
+		},
+
+		"json rule with another than 22 precised": {
+			json:         `{"action":"add", "authfile":"authorized_keys", "authdir":"/dir/", "user":"toto", "key":"totokey","port":33}`,
+			expecteError: false,
+			expectedRule: CompAuthKey{
+				Action:   "add",
+				Authfile: "authorized_keys",
+				User:     "toto",
+				Key:      "totokey",
+				Port:     pti(33),
+			},
+		},
+	}
+
+	for name, c := range testCases {
+		t.Run(name, func(t *testing.T) {
+			obj := CompAuthkeys{Obj: &Obj{rules: make([]interface{}, 0), verbose: true}}
+			err := obj.Add(c.json)
+			if c.expecteError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, 1, len(obj.rules))
+				require.Equal(t, c.expectedRule, obj.rules[0])
+			}
+		})
+	}
+}
