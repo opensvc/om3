2611184d6299a90ed30d67d77dfbe06c8b1506d6
Rémi Mendes <remi.mendes@opensvc.com>
2023-10-27 11:17:38 +0200

    [compobj] adding the check function and its associated test in sysctl.go

--- util/compobj/sysctl.go
+++ util/compobj/sysctl.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 type (
@@ -17,19 +22,21 @@ type (
 	}
 )
 
-var compSysctlInfo = ObjInfo{
-	DefaultPrefix: "OSVC_COMP_SYSCTL_",
-	ExampleValue: CompSysctl{
-		Key:   "vm.lowmem_reserve_ratio",
-		Index: pti(1),
-		Op:    "=",
-		Value: 256,
-	},
-	Description: `* Verify a linux kernel parameter value is on target
+var (
+	execSysctl     = func(key string) *exec.Cmd { return exec.Command("sysctl", key) }
+	compSysctlInfo = ObjInfo{
+		DefaultPrefix: "OSVC_COMP_SYSCTL_",
+		ExampleValue: CompSysctl{
+			Key:   "vm.lowmem_reserve_ratio",
+			Index: pti(1),
+			Op:    "=",
+			Value: 256,
+		},
+		Description: `* Verify a linux kernel parameter value is on target
 * Live parameter value (sysctl executable)
 * Persistent parameter value (/etc/sysctl.conf)
 `,
-	FormDefinition: `Desc: |
+		FormDefinition: `Desc: |
   A rule to set a list of Linux kernel parameters to be set in /etc/sysctl.conf. Current values can be checked as strictly equal, superior or equal, inferior or equal to their target value. Each field in a vectored value can be tuned independantly using the index key.
 Css: comp48
 
@@ -83,7 +90,8 @@ Inputs:
     Type: string or integer
     Help: The /etc/sysctl.conf parameter target value.
 `,
-}
+	}
+)
 
 func init() {
 	m["sysctl"] = NewCompSysctls
@@ -131,6 +139,98 @@ func (t *CompSysctls) Add(s string) error {
 	}
 	return e
 }*/
+
+func (t CompSysctls) checkRule(rule CompSysctl) ExitCode {
+	values, err := t.getValues(rule, false)
+	if err != nil {
+		t.Errorf("error can't read in file /etc/sysctl.conf :%s\n", err)
+		return ExitNok
+	}
+	if values == nil {
+		values, err = t.getValues(rule, true)
+	}
+	if values == nil {
+		t.Errorf("error can't find the key %s in the list of kernel parameter (/etc/sysctl.conf and live)\n", rule.Key)
+		return ExitNok
+	}
+	if len(values) <= *rule.Index {
+		t.VerboseInfof("index %d is out of range for key %s--> not ok\n", *rule.Index, rule.Key)
+		return ExitNok
+	}
+	value := values[*rule.Index]
+	switch rule.Value.(type) {
+	case float64:
+		newValue, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			t.Errorf("the value given is an int but can't convert the value %s found in /etc/sysctl.conf or in live for key %s\n", value, rule.Key)
+			return ExitNok
+		}
+		if testOperator(newValue, rule.Value.(float64), rule.Op) {
+			t.VerboseInfof("%s[%d] = %f target: %f operator: %s --> ok\n", rule.Key, *rule.Index, newValue, rule.Value, rule.Op)
+			return ExitOk
+		}
+		t.VerboseInfof("%s[%d] = %f target: %f operator: %s --> not ok\n", rule.Key, *rule.Index, newValue, rule.Value, rule.Op)
+		return ExitNok
+	case string:
+		if testOperator(value, rule.Value.(string), rule.Op) {
+			t.VerboseInfof("%s[%d] = %s target: %s operator: %s --> ok\n", rule.Key, *rule.Index, value, rule.Value, rule.Op)
+			return ExitOk
+		}
+		t.VerboseInfof("%s[%d] = %s target: %s operator: %s --> not ok\n", rule.Key, *rule.Index, value, rule.Value, rule.Op)
+		return ExitNok
+	default:
+		t.Errorf("type of %s is not float64 or string\n", rule.Value)
+		return ExitNok
+	}
+}
+
+func testOperator[T float64 | string](value T, ruleValue T, operator string) bool {
+	switch operator {
+	case "=":
+		return value == ruleValue
+	case "<=":
+		return value <= ruleValue
+	case ">=":
+		return value >= ruleValue
+	default:
+		return false
+	}
+}
+
+func (t CompSysctls) getValues(rule CompSysctl, checkLive bool) ([]string, error) {
+	var content []byte
+	if checkLive {
+		cmd := execSysctl(rule.Key)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			return nil, fmt.Errorf("error can't read live key values :%w:%s", err, out)
+		}
+		content = out
+	} else {
+		var err error
+		content, err = osReadFile("/etc/sysctl.conf")
+		if err != nil {
+			return nil, err
+		}
+	}
+	scanner := bufio.NewScanner(bytes.NewReader(content))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		splitLine := strings.Fields(line)
+		if len(splitLine) > 1 {
+			if splitLine[0] == rule.Key {
+				if splitLine[1] != "=" {
+					continue
+				}
+				return splitLine[2:], nil
+			}
+		}
+	}
+	return nil, nil
+}
 
 /*func (t CompSysctls) Fix() ExitCode {
 	t.SetVerbose(false)
--- util/compobj/sysctl_test.go
+++ util/compobj/sysctl_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/stretchr/testify/require"
+	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -67,6 +69,164 @@ func TestSysctlAdd(t *testing.T) {
 				require.NoError(t, obj.Add(c.jsonRules))
 				require.Equal(t, c.expectedRules, obj.Obj.rules)
 			}
+		})
+	}
+}
+
+func TestSysctlCheck(t *testing.T) {
+	oriOsReadFile := osReadFile
+	defer func() { osReadFile = oriOsReadFile }()
+
+	oriExecSysctl := execSysctl
+	defer func() { execSysctl = oriExecSysctl }()
+	testCases := map[string]struct {
+		rule                CompSysctl
+		sysctlConfigFile    string
+		sysctlOutput        string
+		expectedCheckResult ExitCode
+	}{
+		"with a key that is not present": {
+			rule: CompSysctl{
+				Key:   "i.am.not.present",
+				Index: pti(3),
+				Op:    "=",
+				Value: 0,
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitNok,
+		},
+
+		"with a key that is present in the conf": {
+			rule: CompSysctl{
+				Key:   "kernel.printk",
+				Index: pti(0),
+				Op:    "=",
+				Value: float64(3),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitOk,
+		},
+
+		"with a key that is present in live": {
+			rule: CompSysctl{
+				Key:   "net.ipv4.tcp_l3mdev_accept",
+				Index: pti(0),
+				Op:    "=",
+				Value: float64(0),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitOk,
+		},
+
+		"with an index that is out of range": {
+			rule: CompSysctl{
+				Key:   "net.ipv4.tcp_l3mdev_accept",
+				Index: pti(89),
+				Op:    "=",
+				Value: float64(0),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitNok,
+		},
+
+		"with an index that is not 0": {
+			rule: CompSysctl{
+				Key:   "kernel.printk",
+				Index: pti(2),
+				Op:    "=",
+				Value: float64(1),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitOk,
+		},
+
+		"with a wrong value and operator =": {
+			rule: CompSysctl{
+				Key:   "kernel.printk",
+				Index: pti(2),
+				Op:    "=",
+				Value: float64(9),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitNok,
+		},
+
+		"with a wrong value and operator <=": {
+			rule: CompSysctl{
+				Key:   "kernel.printk",
+				Index: pti(2),
+				Op:    "<=",
+				Value: float64(0),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitNok,
+		},
+
+		"with a wrong value and operator >=": {
+			rule: CompSysctl{
+				Key:   "kernel.printk",
+				Index: pti(2),
+				Op:    ">=",
+				Value: float64(2),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitNok,
+		},
+
+		"with a true rule and operator <=": {
+			rule: CompSysctl{
+				Key:   "kernel.printk",
+				Index: pti(2),
+				Op:    "<=",
+				Value: float64(2),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitOk,
+		},
+
+		"with a true rule and operator >=": {
+			rule: CompSysctl{
+				Key:   "kernel.printk",
+				Index: pti(2),
+				Op:    ">=",
+				Value: float64(0),
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitOk,
+		},
+
+		"with value equal to a string and operator =": {
+			rule: CompSysctl{
+				Key:   "kernel.domainname",
+				Index: pti(0),
+				Op:    "=",
+				Value: "example.com",
+			},
+			sysctlConfigFile:    "./testdata/sysctl_config_file",
+			sysctlOutput:        "./testdata/sysctl.out",
+			expectedCheckResult: ExitOk,
+		},
+	}
+	obj := CompSysctls{Obj: &Obj{rules: make([]interface{}, 0), verbose: true}}
+	for name, c := range testCases {
+		t.Run(name, func(t *testing.T) {
+			osReadFile = func(name string) ([]byte, error) {
+				return os.ReadFile(c.sysctlConfigFile)
+			}
+			execSysctl = func(key string) *exec.Cmd {
+				return exec.Command("cat", c.sysctlOutput)
+			}
+			require.Equal(t, c.expectedCheckResult, obj.checkRule(c.rule))
 		})
 	}
 }
