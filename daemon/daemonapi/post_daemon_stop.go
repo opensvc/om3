1413835041d0326bbdc5aa2a8920040d99d14444
Cyril Galibern <cyril.galibern@opensvc.com>
2023-11-13 10:30:54 +0100

    [daemonapi] Refactor: extract announceNodeState to dedicated func, announce[un]Sub
    announceNodeState will be used during daemon shutdown

--- daemon/daemonapi/post_daemon_stop.go
+++ daemon/daemonapi/post_daemon_stop.go
@@ -2,12 +2,10 @@ package daemonapi
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 
 	"github.com/opensvc/om3/core/node"
-	"github.com/opensvc/om3/daemon/daemondata"
 	"github.com/opensvc/om3/daemon/msgbus"
 	"github.com/opensvc/om3/util/pubsub"
 )
@@ -16,19 +14,7 @@ func (a *DaemonApi) PostDaemonStop(ctx echo.Context) error {
 	log := LogHandler(ctx, "PostDaemonStop")
 	log.Debugf("starting")
 
-	maintenance := func() {
-		log.Infof("announce maintenance state")
-		state := node.MonitorStateMaintenance
-		a.EventBus.Pub(&msgbus.SetNodeMonitor{
-			Node: a.localhost,
-			Value: node.MonitorUpdate{
-				State: &state,
-			},
-		}, labelApi)
-		time.Sleep(2 * daemondata.PropagationInterval())
-	}
-
-	maintenance()
+	a.announceNodeState(log, node.MonitorStateMaintenance)
 
 	a.EventBus.Pub(&msgbus.DaemonCtl{Component: "daemon", Action: "stop"},
 		pubsub.Label{"id", "daemon"}, labelApi, a.LabelNode)
--- daemon/daemonapi/pubsub.go
+++ daemon/daemonapi/pubsub.go
@@ -3,14 +3,22 @@ package daemonapi
 import (
 	"time"
 
+	"github.com/opensvc/om3/core/node"
+	"github.com/opensvc/om3/daemon/daemondata"
 	"github.com/opensvc/om3/daemon/msgbus"
-	"github.com/opensvc/om3/util/pubsub"
+	"github.com/opensvc/om3/util/plog"
 )
 
-func announceSub(bus *pubsub.Bus, name string) {
-	bus.Pub(&msgbus.ClientSubscribed{Time: time.Now(), Name: name})
+func (a *DaemonApi) announceSub(name string) {
+	a.EventBus.Pub(&msgbus.ClientSubscribed{Time: time.Now(), Name: name})
 }
 
-func announceUnSub(bus *pubsub.Bus, name string) {
-	bus.Pub(&msgbus.ClientUnsubscribed{Time: time.Now(), Name: name})
+func (a *DaemonApi) announceUnSub(name string) {
+	a.EventBus.Pub(&msgbus.ClientUnsubscribed{Time: time.Now(), Name: name})
+}
+
+func (a *DaemonApi) announceNodeState(log *plog.Logger, state node.MonitorState) {
+	log.Infof("announce node state %s", state)
+	a.EventBus.Pub(&msgbus.SetNodeMonitor{Node: a.localhost, Value: node.MonitorUpdate{State: &state}}, labelApi)
+	time.Sleep(2 * daemondata.PropagationInterval())
 }
