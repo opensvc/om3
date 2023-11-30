7c324eb6a71e37a085bd96b8b03cdc6cf0af58fe
Rémi Mendes <remi.mendes@opensvc.com>
2023-11-30 09:52:59 +0100

    [compobj] adding a func to load data from the multipath.conf file

--- util/compobj/linuxMpath.go
+++ util/compobj/linuxMpath.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type (
+	sectionMap   map[string]sectionMap
+	MpathSection struct {
+		name   string
+		indent int
+		attr   map[string]any
+	}
+	MpathBlackList struct {
+		name     string
+		wwids    []string
+		devnodes []string
+		devices  []MpathSection
+	}
+	MpathConf struct {
+		blackList           MpathBlackList
+		blackListExceptions MpathBlackList
+		defaults            MpathSection
+		devices             []MpathSection
+		multipaths          []MpathSection
+		overrides           MpathSection
+	}
 	CompMpaths struct {
 		*Obj
 	}
@@ -16,28 +41,45 @@ type (
 	}
 )
 
-var compMpathInfo = ObjInfo{
-	DefaultPrefix: "OSVC_COMP_MPATH_",
-	ExampleValue: []CompMpath{
-		{
-			Key:   "defaults.polling_interval",
-			Op:    ">=",
-			Value: 20,
+var (
+	MpathSectionsTree = sectionMap{
+		"defaults": {},
+		"blacklist": {
+			"device": {},
 		},
-		{
-			Key:   "device.{HP}.{HSV210.*}.prio",
-			Op:    "=",
-			Value: "alua",
+		"blacklist_exception": {
+			"device": {},
 		},
-		{
-			Key:   "blacklist.wwid",
-			Op:    "=",
-			Value: 600600000001,
+		"devices": {
+			"device": {},
 		},
-	},
-	Description: `* Setup and verify the Linux native multipath configuration
+		"multipaths": {
+			"multipath": {},
+		},
+		"overrides": {},
+	}
+	compMpathInfo = ObjInfo{
+		DefaultPrefix: "OSVC_COMP_MPATH_",
+		ExampleValue: []CompMpath{
+			{
+				Key:   "defaults.polling_interval",
+				Op:    ">=",
+				Value: 20,
+			},
+			{
+				Key:   "device.{HP}.{HSV210.*}.prio",
+				Op:    "=",
+				Value: "alua",
+			},
+			{
+				Key:   "blacklist.wwid",
+				Op:    "=",
+				Value: 600600000001,
+			},
+		},
+		Description: `* Setup and verify the Linux native multipath configuration
 `,
-	FormDefinition: `Desc: |
+		FormDefinition: `Desc: |
   A rule to set a list of Linux multipath.conf parameters. Current values can be checked as equal, or superior/inferior to their target value.
 Outputs:
   -
@@ -83,7 +125,8 @@ Inputs:
     Type: string or integer
     Help: The multipath.conf parameter target value.
 `,
-}
+	}
+)
 
 func init() {
 	m["linux_mpath"] = NewCompMpaths
@@ -126,6 +169,172 @@ func (t *CompMpaths) Add(s string) error {
 		t.Obj.Add(rule)
 	}
 	return nil
+}
+
+func (t CompMpaths) loadMpathData() (MpathConf, error) {
+	mPathData := MpathConf{
+		blackList: MpathBlackList{
+			name:     "blacklist",
+			wwids:    []string{},
+			devnodes: []string{},
+			devices:  []MpathSection{},
+		},
+		blackListExceptions: MpathBlackList{
+			name:     "blacklist_exceptions",
+			wwids:    []string{},
+			devnodes: []string{},
+			devices:  []MpathSection{},
+		},
+		defaults: MpathSection{
+			name:   "default",
+			indent: 0,
+			attr:   map[string]any{},
+		},
+		devices:    []MpathSection{},
+		multipaths: []MpathSection{},
+		overrides: MpathSection{
+			name:   "overrides",
+			indent: 0,
+			attr:   map[string]any{},
+		},
+	}
+	buff, err := os.ReadFile(filepath.Join("/etc", "multipath.conf"))
+	if err != nil {
+		return MpathConf{}, err
+	}
+	buff = stripComments(buff)
+	t.recursiveLoadFile(buff, MpathSectionsTree, []string{}, &mPathData)
+	return mPathData, nil
+}
+
+func stripComments(buff []byte) []byte {
+	newBuff := []byte{}
+	scanner := bufio.NewScanner(bytes.NewReader(buff))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if !strings.HasPrefix(line, "#") && len(line) != 0 {
+			if i := strings.Index(line, "#"); i != -1 {
+				line = line[:i]
+			}
+			if len(line) > 0 {
+				newBuff = append(newBuff, []byte(line+"\n")...)
+			}
+		}
+	}
+	return newBuff
+}
+
+func (t CompMpaths) recursiveLoadFile(buff []byte, sections sectionMap, chain []string, mPathData *MpathConf) {
+	for section, subsection := range MpathSectionsTree {
+		chain = append(chain, section)
+		for {
+			data0, data1 := t.loadSection(buff, section)
+			if data0 == nil && data1 == nil {
+				break
+			}
+			buff = data1
+			t.loadKeyWords(data0, subsection, chain, mPathData)
+			t.recursiveLoadFile(data0, subsection, chain, mPathData)
+		}
+	}
+}
+
+func (t CompMpaths) loadKeyWords(buff []byte, subsection sectionMap, chain []string, mPathData *MpathConf) {
+	keywords := map[string]any{}
+	keyword := ""
+	value := ""
+	scanner := bufio.NewScanner(bytes.NewReader(buff))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			continue
+		}
+		keyval := strings.SplitN(line, " ", 2)
+		if len(keyval) != 2 {
+			continue
+		}
+		keyword = strings.TrimSpace(keyval[0])
+		value = strings.TrimSpace(keyval[1])
+		if _, ok := subsection[keyword]; ok {
+			continue
+		}
+		if (keyword == "wwid" || keyword == "devnode") && strings.HasPrefix(chain[len(chain)-1], "blacklist") {
+			if _, ok := keywords[keyword]; !ok {
+				keywords[keyword] = []string{value}
+			} else {
+				keywords[keyword] = append(keywords[keyword].([]string), value)
+			}
+		} else {
+			keywords[keyword] = value
+		}
+	}
+	switch {
+	case chain[len(chain)-1] == "device" && chain[0] == "devices":
+		mPathData.devices = append(mPathData.devices, MpathSection{
+			name:   "device",
+			indent: 1,
+			attr:   keywords,
+		})
+	case chain[len(chain)-1] == "multipath":
+		mPathData.multipaths = append(mPathData.multipaths, MpathSection{
+			name:   "multipath",
+			indent: 1,
+			attr:   keywords,
+		})
+	case chain[len(chain)-1] == "device" && chain[0] == "blacklist":
+		mPathData.blackList.devices = append(mPathData.blackList.devices, MpathSection{
+			name:   "device",
+			indent: 1,
+			attr:   keywords,
+		})
+	case chain[len(chain)-1] == "device" && chain[0] == "blacklist_exception":
+		mPathData.blackListExceptions.devices = append(mPathData.blackListExceptions.devices, MpathSection{
+			name:   "device",
+			indent: 1,
+			attr:   keywords,
+		})
+	case chain[len(chain)-1] == "blacklist":
+		if tmp, ok := keywords["wwid"]; ok {
+			mPathData.blackList.wwids = tmp.([]string)
+		}
+		if tmp, ok := keywords["devnode"]; ok {
+			mPathData.blackList.devnodes = tmp.([]string)
+		}
+	case chain[len(chain)-1] == "blacklist_exception":
+		if tmp, ok := keywords["wwid"]; ok {
+			mPathData.blackListExceptions.wwids = tmp.([]string)
+		}
+		if tmp, ok := keywords["devnode"]; ok {
+			mPathData.blackListExceptions.devnodes = tmp.([]string)
+		}
+	case chain[len(chain)-1] == "defaults":
+		mPathData.defaults.attr = keywords
+	case chain[len(chain)-1] == "override":
+		mPathData.overrides.attr = keywords
+	}
+}
+
+func (t CompMpaths) loadSection(buff []byte, section string) ([]byte, []byte) {
+	var start int
+	if start = strings.Index(string(buff), section); start == -1 {
+		return nil, nil
+	}
+	if start = strings.Index(string(buff), "{"); start == -1 {
+		return nil, nil
+	}
+	depth := 1
+	buff = buff[start+1:]
+	for i, c := range buff {
+		if c == '{' {
+			depth += 1
+		} else if c == '}' {
+			depth -= 1
+		}
+		if depth == 0 {
+			return buff[:i], buff[i+1:]
+		}
+	}
+	return nil, nil
 }
 
 /*func (t CompMpaths) Check() ExitCode {
