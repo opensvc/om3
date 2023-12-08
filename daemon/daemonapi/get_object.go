f3b21c01b7d32ecc54a0ea05ce3ed87cc2bbba4f
Christophe Varoqui <christophe.varoqui@opensvc.com>
2023-12-08 15:43:00 +0100

    Avoid making a full copy of the objectstatus cache in GET /objects

--- daemon/daemonapi/get_object.go
+++ daemon/daemonapi/get_object.go
@@ -48,42 +48,41 @@ func (a *DaemonApi) getObjects(ctx echo.Context, pathSelector *string) (api.Obje
 	if err := meta.Expand(); err != nil {
 		return nil, err
 	}
-	ostats := object.StatusData.GetAll()
 	l := make(api.ObjectItems, 0)
-	for _, ostat := range ostats {
-		if !meta.HasPath(ostat.Path.String()) {
+	for _, p := range meta.Paths() {
+		ostat := object.StatusData.Get(p)
+		if ostat == nil {
 			continue
 		}
-
 		d := api.ObjectItem{
 			Kind: "Object",
 			Meta: api.ObjectMeta{
-				Object: ostat.Path.String(),
+				Object: p.String(),
 			},
 			Data: api.ObjectData{
-				Avail:            api.Status(ostat.Value.Avail.String()),
-				FlexMax:          ostat.Value.FlexMax,
-				FlexMin:          ostat.Value.FlexMin,
-				FlexTarget:       ostat.Value.FlexTarget,
-				Frozen:           ostat.Value.Frozen,
+				Avail:            api.Status(ostat.Avail.String()),
+				FlexMax:          ostat.FlexMax,
+				FlexMin:          ostat.FlexMin,
+				FlexTarget:       ostat.FlexTarget,
+				Frozen:           ostat.Frozen,
 				Instances:        make(map[string]api.Instance),
-				Orchestrate:      api.Orchestrate(ostat.Value.Orchestrate),
-				Overall:          api.Status(ostat.Value.Overall.String()),
-				PlacementPolicy:  api.PlacementPolicy(ostat.Value.PlacementPolicy.String()),
-				PlacementState:   api.PlacementState(ostat.Value.PlacementState.String()),
-				Pool:             ostat.Value.Pool,
-				Priority:         int(ostat.Value.Priority),
-				Provisioned:      api.Provisioned(ostat.Value.Provisioned.String()),
-				Scope:            append([]string{}, ostat.Value.Scope...),
-				Size:             ostat.Value.Size,
-				Topology:         api.Topology(ostat.Value.Topology.String()),
-				UpInstancesCount: ostat.Value.UpInstancesCount,
-				UpdatedAt:        ostat.Value.UpdatedAt.String(),
+				Orchestrate:      api.Orchestrate(ostat.Orchestrate),
+				Overall:          api.Status(ostat.Overall.String()),
+				PlacementPolicy:  api.PlacementPolicy(ostat.PlacementPolicy.String()),
+				PlacementState:   api.PlacementState(ostat.PlacementState.String()),
+				Pool:             ostat.Pool,
+				Priority:         int(ostat.Priority),
+				Provisioned:      api.Provisioned(ostat.Provisioned.String()),
+				Scope:            append([]string{}, ostat.Scope...),
+				Size:             ostat.Size,
+				Topology:         api.Topology(ostat.Topology.String()),
+				UpInstancesCount: ostat.UpInstancesCount,
+				UpdatedAt:        ostat.UpdatedAt.String(),
 			},
 		}
-		for nodename, config := range instance.ConfigData.GetByPath(ostat.Path) {
-			monitor := instance.MonitorData.Get(ostat.Path, nodename)
-			status := instance.StatusData.Get(ostat.Path, nodename)
+		for nodename, config := range instance.ConfigData.GetByPath(p) {
+			monitor := instance.MonitorData.Get(p, nodename)
+			status := instance.StatusData.Get(p, nodename)
 			d.Data.Instances[nodename] = api.Instance{
 				Config:  config,
 				Monitor: monitor,
