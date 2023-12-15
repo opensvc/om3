9f493ad6e338cd9731c52b6b03d9352e4da233c8
Cyril Galibern <cyril.galibern@opensvc.com>
2023-12-15 18:10:25 +0100

    [core/object] Cleanup unused private functions

--- core/object/core_set.go
+++ core/object/core_set.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/opensvc/om3/core/actioncontext"
 	"github.com/opensvc/om3/core/keyop"
-	"github.com/opensvc/om3/core/xconfig"
 )
 
 // Set changes or adds a keyword and its value in the configuration file.
@@ -17,13 +16,4 @@ func (t *core) Set(ctx context.Context, kops ...keyop.T) error {
 	}
 	defer unlock()
 	return t.config.SetKeys(kops...)
-}
-
-func (t *core) setKeys(kops ...keyop.T) error {
-	return t.config.SetKeys(kops...)
-}
-
-func setKeywords(cf *xconfig.T, kws []string) error {
-	l := keyop.ParseOps(kws)
-	return cf.SetKeys(l...)
 }
