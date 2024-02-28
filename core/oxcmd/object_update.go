6fc461e9d33179c4b44b116c5655cc5e37d1d0cb
Alexandre Meunier <alexandre.meunier@opensvc.com>
2024-02-28 16:04:43 +0100

    [oxcmd] Parallel query for ox object commands.

--- core/oxcmd/object_update.go
+++ core/oxcmd/object_update.go
@@ -2,9 +2,12 @@ package oxcmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/opensvc/om3/core/client"
+	"github.com/opensvc/om3/core/naming"
 	"github.com/opensvc/om3/core/objectselector"
 	"github.com/opensvc/om3/daemon/api"
 )
@@ -30,29 +33,61 @@ func (t *CmdObjectUpdate) Run(selector, kind string) error {
 	if err != nil {
 		return err
 	}
-	for _, p := range paths {
-		params := api.PostObjectConfigUpdateParams{}
-		params.Set = &t.Set
-		params.Unset = &t.Unset
-		params.Delete = &t.Delete
-		response, err := c.PostObjectConfigUpdateWithResponse(context.Background(), p.Namespace, p.Kind, p.Name, &params)
-		if err != nil {
-			return err
-		}
-		switch response.StatusCode() {
-		case 204:
-			fmt.Printf("%s: commited\n", p)
-		case 400:
-			return fmt.Errorf("%s: %s", p, *response.JSON400)
-		case 401:
-			return fmt.Errorf("%s: %s", p, *response.JSON401)
-		case 403:
-			return fmt.Errorf("%s: %s", p, *response.JSON403)
-		case 500:
-			return fmt.Errorf("%s: %s", p, *response.JSON500)
-		default:
-			return fmt.Errorf("%s: unexpected response: %s", p, response.Status())
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	errC := make(chan error)
+	doneC := make(chan string)
+	todo := len(paths)
+
+	for _, path := range paths {
+		go func(p naming.Path) {
+			defer func() { doneC <- p.String() }()
+			params := api.PostObjectConfigUpdateParams{}
+			params.Set = &t.Set
+			params.Unset = &t.Unset
+			params.Delete = &t.Delete
+			response, err := c.PostObjectConfigUpdateWithResponse(ctx, p.Namespace, p.Kind, p.Name, &params)
+			if err != nil {
+				errC <- err
+				return
+			}
+			switch response.StatusCode() {
+			case 204:
+				fmt.Printf("%s: commited\n", p)
+			case 400:
+				errC <- fmt.Errorf("%s: %s", p, *response.JSON400)
+			case 401:
+				errC <- fmt.Errorf("%s: %s", p, *response.JSON401)
+			case 403:
+				errC <- fmt.Errorf("%s: %s", p, *response.JSON403)
+			case 500:
+				errC <- fmt.Errorf("%s: %s", p, *response.JSON500)
+			default:
+				errC <- fmt.Errorf("%s: unexpected response: %s", p, response.Status())
+			}
+		}(path)
+	}
+
+	var (
+		errs error
+		done int
+	)
+
+	for {
+		select {
+		case err := <-errC:
+			errs = errors.Join(errs, err)
+		case <-doneC:
+			done++
+			if done == todo {
+				return errs
+			}
+		case <-ctx.Done():
+			errs = errors.Join(errs, ctx.Err())
+			return errs
 		}
 	}
-	return nil
 }
