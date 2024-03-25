651b57c6ad39c7c061cc916aaafcd94bc7c09742
Cyril Galibern <cyril.galibern@opensvc.com>
2024-03-25 12:15:22 +0100

    [nmon] reduce msg level when new rejoin grace period is already expired

--- daemon/nmon/main_cmd.go
+++ daemon/nmon/main_cmd.go
@@ -89,7 +89,7 @@ func (t *Manager) checkRejoinTicker() {
 		return
 	}
 	if left := t.startedAt.Add(t.nodeConfig.RejoinGracePeriod).Sub(time.Now()); left <= 0 {
-		t.log.Errorf("the new rejoin grace period is already expired")
+		t.log.Infof("the new rejoin grace period is already expired")
 		t.rejoinTicker.Reset(100 * time.Millisecond)
 		return
 	} else {
