opensvc/om3

commit 128a84a6f688b3b01e104ef8e56162a05d9f6e6d

Fix the container.kvm status error when the vm is not defined
And use symbolic state names

drivers/rescontainerkvm/main.go
@@ -37,6 +37,13 @@ import (
 
 const (
 	cpusetDir = "/sys/fs/cgroup/cpuset"
+
+	DomStateBlocked = "blocked"
+	DomStateCrashed = "crashed"
+	DomStateNone    = "no state"
+	DomStatePaused  = "paused"
+	DomStateRunning = "running"
+	DomStateShutOff = "shut off"
 )
 
 type (
@@ -369,7 +376,7 @@ func (t T) containerStop(ctx context.Context) error {
 		return err
 	}
 	switch state {
-	case "running":
+	case DomStateRunning:
 		if err := t.stop(); err != nil {
 			return err
 		}
@@ -379,7 +386,7 @@ func (t T) containerStop(ctx context.Context) error {
 				return err
 			}
 		}
-	case "blocked", "paused", "crashed":
+	case DomStateBlocked, DomStatePaused, DomStateCrashed:
 		if err := t.destroy(); err != nil {
 			return err
 		}
@@ -399,7 +406,7 @@ func (t T) isUp() (bool, error) {
 }
 
 func isUpFromState(state string) bool {
-	if state == "running" {
+	if state == DomStateRunning {
 		return true
 	}
 	return false
@@ -415,7 +422,7 @@ func (t T) isDown() (bool, error) {
 
 func isDownFromState(state string) bool {
 	switch state {
-	case "shut off", "no state":
+	case DomStateShutOff, DomStateNone:
 		return true
 	}
 	return false
@@ -426,12 +433,17 @@ func (t *T) domState() (string, error) {
 		command.WithName("virsh"),
 		command.WithVarArgs("dominfo", t.Name),
 		command.WithBufferedStdout(),
+		command.WithBufferedStderr(),
+		command.WithIgnoredExitCodes(0, 1),
 	)
-	b, err := cmd.Output()
+	err := cmd.Run()
 	if err != nil {
 		return "", err
 	}
-	return domStateFromReader(bytes.NewReader(b))
+	if strings.Contains(string(cmd.Stderr()), "failed to get domain") {
+		return DomStateNone, nil
+	}
+	return domStateFromReader(bytes.NewReader(cmd.Stdout()))
 }
 
 func domStateFromReader(r io.Reader) (string, error) {

commit 34b9ac184a60f8a793ff48ff9d24de0377130aef

Strict error management in container.kvm provisioned() func
128a84a6f688b3b01e104ef8e56162a05d9f6e6d broke provisioned() assumptions
as it doesn't check the new DomInfoNone value.

As a consequence the resource.Provision() codepath exited too soon,
before the "virtinst" script was executed.

drivers/rescontainerkvm/main.go
@@ -641,16 +641,22 @@ func (t T) Label() string {
 	return t.Name
 }
 
-func (t T) provisioned() bool {
-	if _, err := t.domState(); err != nil {
-		return false
+func (t T) provisioned() (bool, error) {
+	if state, err := t.domState(); err != nil {
+		return false, err
+	} else if state == DomStateNone {
+		return false, nil
 	} else {
-		return true
+		return true, nil
 	}
 }
 
 func (t *T) UnprovisionLeaded(ctx context.Context) error {
-	if !t.provisioned() {
+	isProvisioned, err := t.provisioned()
+	if err != nil {
+		return err
+	}
+	if !isProvisioned {
 		t.Log().Infof("skip kvm unprovision: container is not provisioned")
 		return nil
 	}
@@ -670,7 +676,11 @@ func (t *T) UnprovisionLeader(ctx context.Context) error {
 }
 
 func (t T) ProvisionLeader(ctx context.Context) error {
-	if t.provisioned() {
+	isProvisioned, err := t.provisioned()
+	if err != nil {
+		return err
+	}
+	if isProvisioned {
 		t.Log().Infof("skip kvm provision: container is provisioned")
 		return nil
 	}