750740fb4291392fe4cfeaa05fdd60b1074cc608
Christophe Varoqui <christophe.varoqui@opensvc.com>
2024-04-26 18:04:01 +0200

    Add the missing pool.hoc code files git ignored

---
+++ drivers/poolhoc/caps.go
@@ -0,0 +1,15 @@
+//go:build linux || solaris
+
+package poolhoc
+
+import (
+	"github.com/opensvc/om3/util/capabilities"
+)
+
+func init() {
+	capabilities.Register(capabilitiesScanner)
+}
+
+func capabilitiesScanner() ([]string, error) {
+	return []string{drvID.Cap()}, nil
+}
