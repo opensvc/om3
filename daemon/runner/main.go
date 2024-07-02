d87e7470ff6b087c2d9f1df8e7c65bb0155aedbb
Christophe Varoqui <christophe.varoqui@opensvc.com>
2024-07-02 10:37:07 +0200

    Add the daemon/runner pkg
    This new daemon routine uses a priority queue to order job runs.
    
    The dequeueing happens every 200ms, which should be enough for imon
    routines to submit all jobs before we start dequeueing the top priority
    ones.

---
+++ daemon/runner/main.go
@@ -0,0 +1,155 @@
+package runner
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/opensvc/om3/core/priority"
+	"github.com/opensvc/om3/daemon/msgbus"
+	"github.com/opensvc/om3/util/hostname"
+	"github.com/opensvc/om3/util/plog"
+	"github.com/opensvc/om3/util/prioqueue"
+	"github.com/opensvc/om3/util/pubsub"
+)
+
+type (
+	Item struct {
+		f        func() error
+		priority priority.T
+		errC     chan error
+	}
+
+	T struct {
+		running    int
+		maxRunning int
+		interval   time.Duration
+		stage      chan Item
+		queue      *prioqueue.Queue
+		wg         sync.WaitGroup
+		ctx        context.Context
+		cancel     context.CancelFunc
+		bus        *pubsub.Bus
+		subQS      pubsub.QueueSizer
+		log        *plog.Logger
+	}
+)
+
+func (t Item) Before(other prioqueue.Interface) bool {
+	return t.priority < other.(Item).priority
+}
+
+var (
+	def *T
+)
+
+func (t *T) startSubscriptions() *pubsub.Subscription {
+	t.bus = pubsub.BusFromContext(t.ctx)
+	sub := t.bus.Sub("daemon.runner", t.subQS)
+	labelLocalhost := pubsub.Label{"node", hostname.Hostname()}
+	sub.AddFilter(&msgbus.NodeConfigUpdated{}, labelLocalhost)
+	sub.Start()
+	return sub
+}
+
+func (t *T) run() {
+	for {
+		if t.running >= t.maxRunning {
+			t.log.Debugf("priority run queue full: %d running %d waiting", t.running, t.queue.Len())
+			return
+		}
+		i := t.queue.Pop()
+		if i == nil {
+			return
+		}
+		item := i.(Item)
+		t.running += 1
+		go func() {
+			item.errC <- item.f()
+			t.running -= 1
+		}()
+	}
+}
+
+func (t *T) do(ctx context.Context) {
+	defer t.wg.Done()
+	ticker := time.NewTicker(t.interval)
+	defer ticker.Stop()
+	sub := t.startSubscriptions()
+	defer sub.Stop()
+	for {
+		select {
+		case ev := <-sub.C:
+			switch c := ev.(type) {
+			case *msgbus.NodeConfigUpdated:
+				t.maxRunning = c.Value.MaxParallel
+			}
+		case item := <-t.stage:
+			// serialize pushes
+			t.queue.Push(item)
+		case <-ticker.C:
+			t.run()
+		case <-ctx.Done():
+			// clean up ?
+			return
+		}
+	}
+}
+
+func NewDefault(subQS pubsub.QueueSizer) *T {
+	if def == nil {
+		def = New(subQS)
+	}
+	return def
+}
+
+func New(subQS pubsub.QueueSizer) *T {
+	return &T{
+		interval:   200 * time.Millisecond,
+		maxRunning: 5,
+
+		stage: make(chan Item),
+		queue: prioqueue.New(),
+		subQS: subQS,
+		log:   plog.NewDefaultLogger().Attr("pkg", "runner"),
+	}
+}
+
+func (t *T) Stop() error {
+	t.cancel()
+	t.wg.Wait()
+	return nil
+}
+
+func (t *T) Start(ctx context.Context) error {
+	t.ctx, t.cancel = context.WithCancel(ctx)
+	t.wg.Add(1)
+	go t.do(ctx)
+	return nil
+}
+
+func (t *T) Run(p priority.T, f func() error) error {
+	item := Item{
+		f:        f,
+		priority: p,
+		errC:     make(chan error),
+	}
+	t.stage <- item
+	return <-item.errC
+}
+
+func (t *T) SetMaxRunning(n int) {
+	t.maxRunning = n
+}
+
+func Start(ctx context.Context) {
+	def.Start(ctx)
+}
+
+func Run(p priority.T, f func() error) error {
+	return def.Run(p, f)
+}
+
+func SetMaxRunning(n int) {
+	def.SetMaxRunning(n)
+}
