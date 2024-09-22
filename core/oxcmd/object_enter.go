8ecfe5fc26b12eef362745b070b7f4965b9d4c50
Christophe Varoqui <christophe.varoqui@opensvc.com>
2024-09-22 08:53:59 +0200

    Mark "ox <sel> enter" todo

--- core/oxcmd/object_enter.go
+++ core/oxcmd/object_enter.go
@@ -1,35 +1,14 @@
 package oxcmd
 
-import (
-	"context"
-
-	"github.com/opensvc/om3/core/naming"
-	"github.com/opensvc/om3/core/object"
-	"github.com/opensvc/om3/core/objectaction"
-)
+import "fmt"
 
 type (
 	CmdObjectEnter struct {
 		ObjectSelector string
 		RID            string
 	}
-
-	enterer interface {
-		Enter(ctx context.Context, rid string) error
-	}
 )
 
 func (t *CmdObjectEnter) Run(selector, kind string) error {
-	mergedSelector := mergeSelector(selector, t.ObjectSelector, kind, "")
-	return objectaction.New(
-		objectaction.LocalFirst(),
-		objectaction.WithObjectSelector(mergedSelector),
-		objectaction.WithLocalFunc(func(ctx context.Context, p naming.Path) (interface{}, error) {
-			o, err := object.NewActor(p)
-			if err != nil {
-				return nil, err
-			}
-			return nil, o.Enter(ctx, t.RID)
-		}),
-	).Do()
+	return fmt.Errorf("TODO")
 }
