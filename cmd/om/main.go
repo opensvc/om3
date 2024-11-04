ab983436051309b55b5a03acb7bf639ee6f7b075
Christophe Varoqui <christophe.varoqui@opensvc.com>
2024-11-04 08:34:28 +0100

    Add a last-stack-file for om and ox
    <var>/om.stack
    <var>/ox.stack

--- cmd/om/main.go
+++ cmd/om/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"runtime/debug"
 
 	"github.com/opensvc/om3/core/env"
 	"github.com/opensvc/om3/core/om"
+	"github.com/opensvc/om3/core/rawconfig"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			filename := filepath.Join(rawconfig.Paths.Var, "om.stack")
+			if f, err := os.Create(filename); err == nil {
+				defer f.Close()
+				fmt.Fprintf(f, "panic: %s\n\n", r)
+				fmt.Fprint(f, string(debug.Stack()))
+			}
+			panic(r)
+		}
+	}()
 	if err := os.Unsetenv(env.ContextVar); err != nil {
 		panic(err)
 	}
