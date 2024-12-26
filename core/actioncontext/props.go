9e198423e5aebbad53f4fb5b7c26b5feece581a0
Christophe Varoqui <christophe.varoqui@opensvc.com>
2024-12-26 17:08:38 +0100

    Remove unused actioncontext.Properties fields
    * Kinds
    * Local
    * LocalExpect
    * DisableNodeValidation

--- core/actioncontext/props.go
+++ core/actioncontext/props.go
@@ -1,27 +1,22 @@
 package actioncontext
 
 import (
-	"github.com/opensvc/om3/core/naming"
 	"github.com/opensvc/om3/core/ordering"
 )
 
 type (
 	Properties struct {
-		Name                  string
-		Target                string
-		Progress              string
-		Failure               string
-		Order                 ordering.T
-		LocalExpect           string
-		Local                 bool
-		MustLock              bool
-		LockGroup             string
-		Freeze                bool
-		Kinds                 naming.Kinds
-		DisableNodeValidation bool
-		Rollback              bool
-		PG                    bool
-		TimeoutKeywords       []string
+		Name            string
+		Target          string
+		Progress        string
+		Failure         string
+		Order           ordering.T
+		MustLock        bool
+		LockGroup       string
+		Freeze          bool
+		Rollback        bool
+		PG              bool
+		TimeoutKeywords []string
 	}
 )
 
@@ -31,11 +26,8 @@ var (
 		Target:          "booted",
 		Progress:        "booting",
 		Failure:         "boot failed",
-		Local:           true,
 		MustLock:        true,
 		Order:           ordering.Desc,
-		LocalExpect:     "",
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		Freeze:          true,
 		TimeoutKeywords: []string{"timeout"},
 		PG:              true,
@@ -46,27 +38,20 @@ var (
 		Progress: "deleting",
 		Failure:  "delete failed",
 		Order:    ordering.Desc,
-		Local:    true,
 		MustLock: true,
-		Kinds:    naming.NewKinds(naming.KindSvc, naming.KindVol, naming.KindCfg, naming.KindSec, naming.KindUsr),
 	}
 	Freeze = Properties{
-		Name:        "freeze",
-		Target:      "frozen",
-		Progress:    "freezing",
-		Failure:     "freeze failed",
-		Local:       true,
-		LocalExpect: "unset",
-		Kinds:       naming.NewKinds(naming.KindSvc, naming.KindVol),
-		PG:          true,
+		Name:     "freeze",
+		Target:   "frozen",
+		Progress: "freezing",
+		Failure:  "freeze failed",
+		PG:       true,
 	}
 	SyncFull = Properties{
 		Name:     "sync_full",
-		Local:    true,
 		Progress: "syncing",
 		Failure:  "idle",
 		MustLock: true,
-		Kinds:    naming.NewKinds(naming.KindSvc, naming.KindVol),
 		PG:       true,
 	}
 	Set = Properties{
@@ -75,12 +60,10 @@ var (
 	}
 	SetProvisioned = Properties{
 		Name:     "set provisioned",
-		Local:    true,
 		MustLock: true,
 	}
 	SetUnprovisioned = Properties{
 		Name:     "set unprovisioned",
-		Local:    true,
 		MustLock: true,
 	}
 	Status = Properties{
@@ -110,26 +93,19 @@ var (
 		Target:          "provisioned",
 		Progress:        "provisioning",
 		Failure:         "provision failed",
-		Local:           true,
 		MustLock:        true,
-		LocalExpect:     "unset",
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		Rollback:        true,
 		TimeoutKeywords: []string{"unprovision_timeout", "timeout"},
 		PG:              true,
 	}
 	PushResInfo = Properties{
-		Name:  "push resinfo",
-		Local: true,
-		Kinds: naming.NewKinds(naming.KindSvc, naming.KindVol),
-		PG:    true,
+		Name: "push resinfo",
+		PG:   true,
 	}
 	Run = Properties{
 		Name:            "run",
 		Progress:        "running",
 		Failure:         "idle",
-		Local:           true,
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		TimeoutKeywords: []string{"run_timeout", "timeout"},
 		PG:              true,
 	}
@@ -138,9 +114,7 @@ var (
 		Target:          "shutdown",
 		Progress:        "shutting",
 		Failure:         "shutdown failed",
-		Local:           true,
 		Order:           ordering.Desc,
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		TimeoutKeywords: []string{"stop_timeout", "timeout"},
 		PG:              true,
 	}
@@ -149,9 +123,6 @@ var (
 		Target:          "started",
 		Progress:        "starting",
 		Failure:         "start failed",
-		Local:           true,
-		LocalExpect:     "unset",
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		Rollback:        true,
 		TimeoutKeywords: []string{"start_timeout", "timeout"},
 		PG:              true,
@@ -160,9 +131,6 @@ var (
 		Name:            "startstandby",
 		Progress:        "starting",
 		Failure:         "start failed",
-		Local:           true,
-		LocalExpect:     "unset",
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		Rollback:        true,
 		TimeoutKeywords: []string{"start_timeout", "timeout"},
 		PG:              true,
@@ -172,62 +140,48 @@ var (
 		Target:          "stopped",
 		Progress:        "stopping",
 		Failure:         "stop failed",
-		Local:           true,
 		MustLock:        true,
 		Order:           ordering.Desc,
-		LocalExpect:     "",
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		Freeze:          true,
 		TimeoutKeywords: []string{"stop_timeout", "timeout"},
 		PG:              true,
 	}
 	SyncResync = Properties{
 		Name:     "sync_resync",
-		Local:    true,
 		Progress: "syncing",
 		Failure:  "idle",
 		MustLock: true,
-		Kinds:    naming.NewKinds(naming.KindSvc, naming.KindVol),
 		PG:       true,
 	}
 	Unfreeze = Properties{
-		Name:        "unfreeze",
-		Target:      "thawed",
-		Progress:    "thawing",
-		Failure:     "unfreeze failed",
-		Local:       true,
-		LocalExpect: "unset",
-		Kinds:       naming.NewKinds(naming.KindSvc, naming.KindVol),
-		PG:          true,
+		Name:     "unfreeze",
+		Target:   "thawed",
+		Progress: "thawing",
+		Failure:  "unfreeze failed",
+		PG:       true,
 	}
 	Unprovision = Properties{
 		Name:            "unprovision",
 		Target:          "unprovisioned",
 		Progress:        "unprovisioning",
 		Failure:         "unprovision failed",
-		Local:           true,
 		MustLock:        true,
 		Order:           ordering.Desc,
-		Kinds:           naming.NewKinds(naming.KindSvc, naming.KindVol),
 		TimeoutKeywords: []string{"unprovision_timeout", "timeout"},
 		PG:              true,
 	}
 	SyncIngest = Properties{
 		Name:     "sync_ingest",
-		Local:    true,
 		Progress: "syncing",
 		Failure:  "idle",
 		MustLock: true,
-		Kinds:    naming.NewKinds(naming.KindSvc, naming.KindVol),
 		PG:       true,
 	}
 	SyncUpdate = Properties{
 		Name:     "sync_update",
 		Progress: "syncing",
 		Failure:  "idle",
-		Local:    true,
 		MustLock: true,
-		Kinds:    naming.NewKinds(naming.KindSvc, naming.KindVol),
 		PG:       true,
 	}
 )
