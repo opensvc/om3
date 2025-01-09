993b68052a77d87286cb2384aa0c975f59190de5
Cyril Galibern <cyril.galibern@opensvc.com>
2025-01-09 08:45:54 +0100

    [daemonapi] Add label node and label api to Client[un]Subscribed

--- daemon/daemonapi/lib_pubsub.go
+++ daemon/daemonapi/lib_pubsub.go
@@ -10,11 +10,11 @@ import (
 )
 
 func (a *DaemonAPI) announceSub(name string) {
-	a.EventBus.Pub(&msgbus.ClientSubscribed{Time: time.Now(), Name: name})
+	a.EventBus.Pub(&msgbus.ClientSubscribed{Time: time.Now(), Name: name}, a.LabelNode, labelAPI)
 }
 
 func (a *DaemonAPI) announceUnsub(name string) {
-	a.EventBus.Pub(&msgbus.ClientUnsubscribed{Time: time.Now(), Name: name})
+	a.EventBus.Pub(&msgbus.ClientUnsubscribed{Time: time.Now(), Name: name}, a.LabelNode, labelAPI)
 }
 
 func (a *DaemonAPI) announceNodeState(log *plog.Logger, state node.MonitorState) {
