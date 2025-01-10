ebee0e1e9e788b3166648b10997341f5621e7ebd
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-01-10 19:05:57 +0100

    Remove the tree renderer for "om <sel> validate"
    Use the tab renderer.
    
    	$ bin/om cluster val
    	LEVEL  PATH     DRIVER  KEY         KIND             COMMENT
    	🟠     cluster          node.uuid   unknown keyword
    	🟠     cluster          node.prkey  unknown keyword

--- core/omcmd/object_validate_config.go
+++ core/omcmd/object_validate_config.go
@@ -3,11 +3,15 @@ package omcmd
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/opensvc/om3/core/actioncontext"
 	"github.com/opensvc/om3/core/naming"
 	"github.com/opensvc/om3/core/object"
 	"github.com/opensvc/om3/core/objectaction"
+	"github.com/opensvc/om3/core/output"
+	"github.com/opensvc/om3/core/rawconfig"
+	"github.com/opensvc/om3/core/xconfig"
 )
 
 type (
@@ -19,13 +23,30 @@ type (
 
 func (t *CmdObjectValidateConfig) Run(selector, kind string) error {
 	mergedSelector := mergeSelector(selector, t.ObjectSelector, kind, "")
-	return objectaction.New(
+	alerts := make(xconfig.Alerts, 0)
+	alertsQ := make(chan xconfig.Alerts)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case moreAlerts := <-alertsQ:
+				alerts = append(alerts, moreAlerts...)
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	var wg sync.WaitGroup
+
+	err := objectaction.New(
 		objectaction.LocalFirst(),
 		objectaction.WithLocal(t.Local),
 		objectaction.WithColor(t.Color),
-		objectaction.WithOutput(t.Output),
 		objectaction.WithObjectSelector(mergedSelector),
 		objectaction.WithLocalFunc(func(ctx context.Context, p naming.Path) (interface{}, error) {
+			wg.Add(1)
+			defer wg.Done()
 			o, err := object.New(p)
 			if err != nil {
 				return nil, err
@@ -36,7 +57,28 @@ func (t *CmdObjectValidateConfig) Run(selector, kind string) error {
 			}
 			ctx = actioncontext.WithLockDisabled(ctx, t.Disable)
 			ctx = actioncontext.WithLockTimeout(ctx, t.Timeout)
-			return c.ValidateConfig(ctx)
+			if moreAlerts, err := c.ValidateConfig(ctx); err != nil {
+				return nil, err
+			} else {
+				alertsQ <- moreAlerts
+			}
+			return nil, nil
 		}),
 	).Do()
+
+	if err != nil {
+		return err
+	}
+
+	wg.Wait()
+	done <- true
+
+	output.Renderer{
+		DefaultOutput: "tab=LEVEL:icon,PATH:path,DRIVER:driver,KEY:key,KIND:kind,COMMENT:comment",
+		Output:        t.Output,
+		Color:         t.Color,
+		Data:          alerts,
+		Colorize:      rawconfig.Colorize,
+	}.Print()
+	return nil
 }
