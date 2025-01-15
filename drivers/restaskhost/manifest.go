3b5b37dfb74a77368aa1198c8cd1939271210dda
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-01-15 11:29:49 +0100

    Align all task.* T functions to the pointer-receiver mode

--- drivers/restaskhost/manifest.go
+++ drivers/restaskhost/manifest.go
@@ -17,7 +17,7 @@ func init() {
 }
 
 // Manifest ...
-func (t T) Manifest() *manifest.T {
+func (t *T) Manifest() *manifest.T {
 	m := manifest.New(drvID, t)
 	m.Kinds.Or(naming.KindSvc, naming.KindVol)
 	m.Add(
