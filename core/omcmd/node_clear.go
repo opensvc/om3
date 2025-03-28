db21c93ec3283065df3b7d7c8d751c3b4305c056
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-03-28 12:13:09 +0100

    Move POST /node/clear to POST /node/name/{nodename}/action/clear
    And support '--node *' in the "o[mx] node clear" commands.

--- core/omcmd/node_clear.go
+++ core/omcmd/node_clear.go
@@ -2,14 +2,18 @@ package omcmd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/opensvc/om3/core/client"
+	"github.com/opensvc/om3/core/nodeselector"
+	"github.com/opensvc/om3/util/hostname"
 )
 
 type CmdNodeClear struct {
 	OptsGlobal
+	NodeSelector string
 }
 
 func (t *CmdNodeClear) Run() error {
@@ -17,10 +21,50 @@ func (t *CmdNodeClear) Run() error {
 	if err != nil {
 		return err
 	}
-	if resp, err := c.PostNodeClear(context.Background()); err != nil {
-		return err
-	} else if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected post node clear status code %s", resp.Status)
+	var nodenames []string
+	if t.NodeSelector == "" {
+		nodenames = []string{hostname.Hostname()}
+	} else {
+		nodenames, err = nodeselector.New(t.NodeSelector, nodeselector.WithClient(c)).Expand()
+		if err != nil {
+			return err
+		}
 	}
-	return nil
+	errC := make(chan error)
+	ctx := context.Background()
+	for _, nodename := range nodenames {
+		go func(nodename string) {
+			if resp, err := c.PostNodeActionClearWithResponse(ctx, nodename); err != nil {
+				errC <- err
+			} else {
+				switch resp.StatusCode() {
+				case http.StatusOK:
+					fmt.Printf("node %s: action cleared\n", nodename)
+				case 401:
+					errC <- fmt.Errorf("%s: %s", nodename, *resp.JSON401)
+				case 403:
+					errC <- fmt.Errorf("%s: %s", nodename, *resp.JSON403)
+				case 500:
+					errC <- fmt.Errorf("%s: %s", nodename, *resp.JSON500)
+				default:
+					errC <- fmt.Errorf("%s: unexpected status [%d]", nodename, resp.StatusCode())
+				}
+			}
+			errC <- err
+		}(nodename)
+	}
+	var (
+		count int
+		errs  error
+	)
+	for {
+		err := <-errC
+		errs = errors.Join(errs, err)
+		count++
+		if count == len(nodenames) {
+			break
+		}
+	}
+	return errs
+
 }
