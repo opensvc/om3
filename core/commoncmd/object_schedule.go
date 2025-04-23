946ef3c36a326a312c654affdd0699a3f18ce54a
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-04-23 11:52:18 +0200

    Classify "om <actor> schedule" in the "subsystems" cmd group

--- core/commoncmd/object_schedule.go
+++ core/commoncmd/object_schedule.go
@@ -4,6 +4,7 @@ import "github.com/spf13/cobra"
 
 func NewCmdObjectSchedule(kind string) *cobra.Command {
 	cmd := &cobra.Command{
+		GroupID: GroupIDSubsystems,
 		Use:     "schedule",
 		Short:   "object scheduler commands",
 		Aliases: []string{"sched"},
