2cb09d29754dd75839dc257196dd7d9310f698d1
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-07-07 18:13:20 +0200

    Implement node hooks
    Execute a detached command when an event occurs.
    Pass the event as a json-formatted EVENT environment variable.
    
    Example:
    
    	[hook#1]
    	event = InstanceStatusUpdated
    	command = env | tee /tmp/hook1.txt
    
    	[hook#2]
    	event = InstanceMonitorUpdated
    	command = echo $EVENT | cat - | tee /tmp/hook2.txt
    
    =>
    
    	tail: /tmp/hook1.txt:
    	EVENT={"labels":{"namespace":"root","node":"dev2n1","path":"tflex"},"path":"tflex","node":"dev2n1","instance_status":{"avail":"up","frozen_at":"0001-01-01T00:00:00Z","last_started_at":"2025-06-26T15:12:03.754553654+02:00","overall":"warn","provisioned":"true","resources":{"fs#1":{"label":"flag /dev/shm/opensvc/svc/tflex/fs#1.flag","status":"up","type":"fs.flag","provisioned":{"mtime":"2025-01-10T12:47:00.868117049+01:00","state":"true"}},"task#1":{"label":"host ","log":[{"level":"warn","message":"last run failed (7)"}],"status":"n/a","type":"task.host","provisioned":{"mtime":"2025-01-10T12:47:00.869117049+01:00","state":"n/a"},"info":{"last_run_at":"2025-07-07T18:09:55.039243823+02:00","last_run_exitcode":7,"last_run_session_id":"aca14b85-bb26-43d0-ad8f-c596dcbb23f9"}},"task#2":{"label":"docker ","log":[{"level":"info","message":"last run failed (7)"}],"status":"warn","type":"task.docker","provisioned":{"mtime":"0001-01-01T00:00:00Z","state":"n/a"},"info":{"last_run_at":"2025-06-27T10:58:03.08349896+02:00","last_run_exitcode":7,"last_run_session_id":"4410a8a8-3f8f-4c56-94c7-bf5f6e72c2b2"}},"task#3":{"label":"host ","status":"n/a","type":"task.host","provisioned":{"mtime":"0001-01-01T00:00:00Z","state":"n/a"},"info":{"last_run_at":"2025-07-07T18:14:19.142390332+02:00","last_run_exitcode":0,"last_run_session_id":"467bef29-c6a8-43de-ac0a-c8f4141860df"}},"task#4":{"label":"host ","status":"n/a","type":"task.host","provisioned":{"mtime":"0001-01-01T00:00:00Z","state":"n/a"},"info":{"last_run_at":"2025-03-26T09:02:33.859632563+01:00","last_run_exitcode":0,"last_run_session_id":"00000000-0000-0000-0000-000000000000"}}},"updated_at":"2025-07-07T18:14:19.144955635+02:00"}}

---
+++ daemon/hook/main.go
@@ -0,0 +1,237 @@
+package hook
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"slices"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/opensvc/om3/core/object"
+	"github.com/opensvc/om3/core/xconfig"
+	"github.com/opensvc/om3/daemon/msgbus"
+	"github.com/opensvc/om3/util/command"
+	"github.com/opensvc/om3/util/hostname"
+	"github.com/opensvc/om3/util/key"
+	"github.com/opensvc/om3/util/plog"
+	"github.com/opensvc/om3/util/pubsub"
+	"github.com/opensvc/om3/util/xmap"
+)
+
+type (
+	Manager struct {
+		// config is the node merged config
+		config *xconfig.T
+
+		ctx       context.Context
+		cancel    context.CancelFunc
+		log       *plog.Logger
+		startedAt time.Time
+
+		labelLocalhost pubsub.Label
+		localhost      string
+
+		sub   *pubsub.Subscription
+		subQS pubsub.QueueSizer
+
+		hooks hooks
+
+		wg sync.WaitGroup
+	}
+
+	hook struct {
+		sig    string
+		cancel func()
+	}
+
+	hooks map[string]hook
+)
+
+func NewManager(drainDuration time.Duration, subQS pubsub.QueueSizer) *Manager {
+	localhost := hostname.Hostname()
+	return &Manager{
+		log:       plog.NewDefaultLogger().Attr("pkg", "daemon/hook").WithPrefix("daemon: hook: "),
+		localhost: localhost,
+
+		labelLocalhost: pubsub.Label{"node", localhost},
+
+		subQS: subQS,
+		hooks: make(hooks),
+	}
+}
+
+func (t *Manager) Start(parent context.Context) error {
+	t.log.Infof("starting")
+	t.ctx, t.cancel = context.WithCancel(parent)
+	t.update()
+	t.startSubscriptions()
+	t.startUpdateLoop()
+	t.log.Infof("started")
+	return nil
+}
+
+func (t *Manager) loadConfig() error {
+	n, err := object.NewNode(object.WithVolatile(false))
+	if err != nil {
+		return err
+	}
+	t.config = n.MergedConfig()
+	return nil
+}
+
+func (t *Manager) startUpdateLoop() {
+	go func() {
+		for {
+			select {
+			case <-t.ctx.Done():
+				return
+			case <-t.sub.C:
+				t.update()
+			}
+		}
+	}()
+}
+
+func (t *Manager) startSubscriptions() {
+	sub := pubsub.SubFromContext(t.ctx, "daemon.hook", t.subQS)
+	sub.AddFilter(&msgbus.NodeConfigUpdated{}, t.labelLocalhost)
+	sub.Start()
+	t.sub = sub
+}
+
+func (t *Manager) Stop() error {
+	t.log.Infof("stopping")
+	defer t.log.Infof("stopped")
+	t.cancel()
+	t.wg.Wait()
+	return nil
+}
+
+func (t *Manager) update() {
+	if err := t.loadConfig(); err != nil {
+		t.log.Warnf("%s", err)
+		return
+	}
+	currentHookNames := xmap.Keys(t.hooks)
+	var hooksToStart, hooksToStop, scannedHooks []string
+	for _, name := range t.config.SectionStrings() {
+		if !strings.HasPrefix(name, "hook#") {
+			continue
+		}
+		scannedHooks = append(scannedHooks, name)
+		currentHook, ok := t.hooks[name]
+		if !ok {
+			hooksToStart = append(hooksToStart, name)
+			continue
+		}
+		sig := t.config.SectionSig(name)
+		if sig != currentHook.sig {
+			hooksToStop = append(hooksToStop, name)
+			hooksToStart = append(hooksToStart, name)
+		}
+
+	}
+	for _, name := range currentHookNames {
+		if !slices.Contains(scannedHooks, name) {
+			hooksToStop = append(hooksToStop, name)
+		}
+	}
+	for _, name := range hooksToStop {
+		t.hooks[name].cancel()
+		delete(t.hooks, name)
+	}
+	for _, name := range hooksToStart {
+		kind := t.config.Get(key.New(name, "event"))
+		s := t.config.Get(key.New(name, "command"))
+		args, err := command.CmdArgsFromString(s)
+		if err != nil {
+			t.log.Warnf("%s: failed to split command: %s", name, err)
+		}
+		if len(args) < 1 {
+			t.log.Warnf("%s: empty command", name)
+		}
+		msg, err := msgbus.KindToT(kind)
+		if err != nil {
+			t.log.Warnf("%s: invalid event %s: %s", name, kind, err)
+			continue
+		}
+		t.hooks[name] = hook{
+			sig:    t.config.SectionSig(name),
+			cancel: t.startHook(name, kind, msg, args),
+		}
+	}
+}
+
+func (t *Manager) hookExec(event any, args []string) error {
+	b, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("failed to json-encode event: %w", err)
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Env = []string{
+		"EVENT=" + string(b),
+	}
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true, // Create a new process group
+	}
+	stdinFile, err := os.OpenFile(os.DevNull, os.O_RDONLY, 0)
+	if err != nil {
+		return fmt.Errorf("failed to open os.DevNull for stdin: %w", err)
+	}
+	cmd.Stdin = stdinFile
+	defer stdinFile.Close()
+
+	stdoutFile, err := os.OpenFile(os.DevNull, os.O_RDONLY, 0)
+	if err != nil {
+		return fmt.Errorf("failed to open os.DevNull for stdout: %w", err)
+	}
+	cmd.Stdout = stdoutFile
+	defer stdoutFile.Close()
+
+	stderrFile, err := os.OpenFile(os.DevNull, os.O_RDONLY, 0)
+	if err != nil {
+		return fmt.Errorf("failed to open os.DevNull for stderr: %w", err)
+	}
+	cmd.Stderr = stderrFile
+	defer stderrFile.Close()
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to start cmd: %w", err)
+	}
+	return nil
+}
+
+func (t *Manager) hookLoop(ctx context.Context, sub *pubsub.Subscription, name, kind string, args []string) {
+	t.log.Infof("%s: listening for event %s", name, kind)
+	defer t.log.Infof("%s: stop listening for event %s", name, kind)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case event := <-sub.C:
+			t.log.Infof("%s: %s => exec %s", name, kind, args)
+			if err := t.hookExec(event, args); err != nil {
+				t.log.Warnf("%s: %s", name, err)
+			}
+		}
+	}
+}
+
+func (t *Manager) startHook(name, kind string, event any, args []string) func() {
+	ctx, cancel := context.WithCancel(t.ctx)
+	sub := pubsub.SubFromContext(ctx, "daemon.hook", t.subQS, pubsub.Timeout(time.Second))
+	sub.AddFilter(event, t.labelLocalhost)
+	sub.Start()
+
+	go func() {
+		t.hookLoop(ctx, sub, name, kind, args)
+		sub.Stop()
+	}()
+	return cancel
+}
