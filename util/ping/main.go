394e92efa8c07686962ed39e7d2d096cc86762db
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-07-16 13:57:44 +0200

    Add more configuration options to the ping util package
    Add a ping.T type to store ping options.
    
    Move the ping codepath to ping.T.Ping() and make the ping.Ping()
    and ping.PingContext use the ping.T.Ping() as backend, setting
    appropriate options before.
    
    Use the ping.T.Ping() directly in the ip.netns driver, ip6 start
    codepath, to force neighbour announcing.

--- util/ping/main.go
+++ util/ping/main.go
@@ -2,8 +2,20 @@ package ping
 
 import (
 	"context"
+	"fmt"
 	"os/exec"
 	"time"
+)
+
+type (
+	T struct {
+		Dev      string
+		Ctx      context.Context
+		Dst      string
+		V        int
+		Interval int
+		Count    int
+	}
 )
 
 func Ping(s string, timeout time.Duration) (bool, error) {
@@ -13,10 +25,45 @@ func Ping(s string, timeout time.Duration) (bool, error) {
 }
 
 func PingContext(ctx context.Context, s string) (bool, error) {
-	cmd := exec.CommandContext(ctx, "ping", "-i", "1", "-c", "1", s)
+	t := T{
+		Ctx:      ctx,
+		Dst:      s,
+		Count:    1,
+		Interval: 1,
+	}
+	return t.Ping()
+}
+
+func (t *T) Ping() (bool, error) {
+	var (
+		name string
+		args []string
+	)
+	if t.V == 6 {
+		name = "ping6"
+	} else {
+		name = "ping"
+	}
+	if t.Count > 0 {
+		args = append(args, "-c", fmt.Sprint(t.Count))
+	}
+	if t.Interval > 0 {
+		args = append(args, "-i", fmt.Sprint(t.Interval))
+	}
+	if t.Dev != "" {
+		args = append(args, "-I", t.Dev)
+	}
+	args = append(args, t.Dst)
+
+	var cmd *exec.Cmd
+	if t.Ctx != nil {
+		cmd = exec.CommandContext(t.Ctx, name, args...)
+	} else {
+		cmd = exec.Command(name, args...)
+	}
 	err := cmd.Run()
 	if err != nil {
-		if ctx.Err() == context.DeadlineExceeded {
+		if t.Ctx.Err() == context.DeadlineExceeded {
 			return false, nil
 		}
 		if cmd.ProcessState.ExitCode() == 1 {
