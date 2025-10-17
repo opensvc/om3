84641f854f0f9baea450a94442d2cc880f613f24
Cyril Galibern <cyril.galibern@opensvc.com>
2025-10-17 13:57:36 +0200

    [daemon/daemonapi] Add rate limiting for cluster heartbeat rotation
    - Introduced a mutex to enforce minimum intervals between successive rotation calls.
    - Improved logging and error responses for early rotation attempts.

--- daemon/daemonapi/post_cluster_heartbeat_rotate.go
+++ daemon/daemonapi/post_cluster_heartbeat_rotate.go
@@ -1,7 +1,10 @@
 package daemonapi
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
@@ -11,6 +14,16 @@ import (
 	"github.com/opensvc/om3/daemon/msgbus"
 )
 
+var (
+	// heartbeatRotateLock is a mutex used to enforce rate limiting for cluster
+	// heartbeat rotation operations.
+	heartbeatRotateLock = sync.Mutex{}
+
+	// heartbeatRotateMinCallInterval defines the minimum time interval required
+	// between successive heartbeat rotate calls.
+	heartbeatRotateMinCallInterval = 60 * time.Second
+)
+
 func (a *DaemonAPI) PostClusterHeartbeatRotate(ctx echo.Context) error {
 	if v, err := assertRoot(ctx); !v {
 		return err
@@ -18,7 +31,7 @@ func (a *DaemonAPI) PostClusterHeartbeatRotate(ctx echo.Context) error {
 
 	leader, err := getLeaderNode()
 	if err != nil {
-		log := LogHandler(ctx, "postLocalClusterHeartbeatRotate")
+		log := LogHandler(ctx, "PostClusterHeartbeatRotate")
 		log.Infof("unable to get leader node: %s", err)
 		return JSONProblemf(ctx, http.StatusConflict, "cluster heartbeat rotate refused", "can't get leader node: %s", err)
 	}
@@ -33,9 +46,20 @@ func (a *DaemonAPI) PostClusterHeartbeatRotate(ctx echo.Context) error {
 }
 
 func (a *DaemonAPI) postLocalClusterHeartbeatRotate(ctx echo.Context) error {
-	log := LogHandler(ctx, "postLocalClusterHeartbeatRotate")
+	log := LogHandler(ctx, "PostClusterHeartbeatRotate")
+	if ok := heartbeatRotateLock.TryLock(); !ok {
+		log := LogHandler(ctx, "postLocalClusterHeartbeatRotate")
+		err := fmt.Errorf("initiated too early, rate limiting is enforced")
+		log.Infof("cluster heartbeat rotate refused: %s", err)
+		return JSONProblemf(ctx, http.StatusConflict, "cluster heartbeat rotate refused", "%s", err)
+	}
 	log.Infof("publish heartbeat rotate request")
 	id := ctx.Get("uuid").(uuid.UUID)
 	a.Publisher.Pub(&msgbus.HeartbeatRotateRequest{ID: id}, a.LabelLocalhost, labelOriginAPI)
+	go func() {
+		time.AfterFunc(heartbeatRotateMinCallInterval, func() {
+			heartbeatRotateLock.Unlock()
+		})
+	}()
 	return ctx.JSON(http.StatusOK, api.HeartbeatRotateResponse{ID: id})
 }
