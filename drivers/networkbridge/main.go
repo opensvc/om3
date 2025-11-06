3e4227cf2b747b089d8eb55b4805671c3cdcbef5
Christophe Varoqui <christophe.varoqui@opensvc.com>
2025-11-06 17:08:54 +0100

    Fix the "default" network not configuring the obr_default
    Add a Setup() func to the network.bridge driver.

--- drivers/networkbridge/main.go
+++ drivers/networkbridge/main.go
@@ -1,8 +1,12 @@
 package networkbridge
 
 import (
+	"fmt"
+	"net"
+
 	"github.com/opensvc/om3/core/driver"
 	"github.com/opensvc/om3/core/network"
+	"github.com/vishvananda/netlink"
 )
 
 type (
@@ -72,4 +76,126 @@ func (t *T) CNIConfigData() (interface{}, error) {
 		},
 	}
 	return m, nil
+}
+
+func (t *T) bridgeIP() (net.IP, error) {
+	subnetStr := t.Network()
+	if subnetStr == "" {
+		return nil, fmt.Errorf("network#%s.subnet is required", t.Name())
+	}
+	ip, _, err := net.ParseCIDR(subnetStr)
+	if err != nil {
+		return nil, err
+	}
+	ip[len(ip)-1]++
+	return ip, nil
+}
+
+func (t *T) setupBridge() (netlink.Link, error) {
+	la := netlink.NewLinkAttrs()
+	la.Name = t.brName()
+	link, err := netlink.LinkByName(la.Name)
+	_, linkNotFound := err.(netlink.LinkNotFoundError)
+	switch {
+	case linkNotFound:
+	case err != nil:
+		return nil, err
+	case link != nil:
+		t.Log().Infof("bridge link %s already exists", la.Name)
+		return link, nil
+	}
+	br := &netlink.Bridge{LinkAttrs: la}
+	err = netlink.LinkAdd(br)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add bridge link %s: %v", la.Name, err)
+	}
+	t.Log().Infof("added bridge link %s", la.Name)
+	return br, nil
+}
+
+func (t *T) setupBridgeIP(br netlink.Link, brIP net.IP) error {
+	subnetStr := t.Network()
+	if br == nil {
+		return nil
+	}
+	brName := t.brName()
+	_, ipnet, err := net.ParseCIDR(subnetStr)
+	if err != nil {
+		return err
+	}
+	ipnet.IP = brIP
+	ipnetStr := ipnet.String()
+
+	if intf, err := net.InterfaceByName(brName); err != nil {
+		return err
+	} else if addrs, err := intf.Addrs(); err != nil {
+		return err
+	} else {
+		for _, addr := range addrs {
+			if addr.String() == ipnetStr {
+				t.Log().Infof("bridge ip %s already added to %s", ipnet, brName)
+				return nil
+			}
+		}
+	}
+	addr := &netlink.Addr{IPNet: ipnet}
+	if err := netlink.AddrAdd(br, addr); err != nil {
+		return err
+	}
+	t.Log().Infof("added ip %s to bridge %s", ipnet, brName)
+	return nil
+}
+
+func (t *T) setupBridgeMAC(br netlink.Link, brIP net.IP) error {
+	var (
+		mac net.HardwareAddr
+		err error
+	)
+	if br == nil {
+		return nil
+	}
+	if t.IsIP6() {
+		return nil
+	}
+	if mac, err = network.MACFromIP4(brIP); err != nil {
+		return err
+	}
+	if br.Attrs().HardwareAddr.String() == mac.String() {
+		t.Log().Infof("bridge %s mac is already %s", br.Attrs().Name, mac)
+		return nil
+	}
+	t.Log().Infof("bridge %s set mac to %s", br.Attrs().Name, mac)
+	return netlink.LinkSetHardwareAddr(br, mac)
+}
+
+func (t *T) IsIP6() bool {
+	ip, _, err := net.ParseCIDR(t.Network())
+	if err != nil {
+		return false
+	}
+	return ip.To4() == nil
+}
+
+func (t *T) Setup() error {
+	var (
+		brIP net.IP
+		link netlink.Link
+		err  error
+	)
+	if brIP, err = t.bridgeIP(); err != nil {
+		return err
+	}
+	if link, err = t.setupBridge(); err != nil {
+		return fmt.Errorf("setup br: %w", err)
+	}
+	if err := t.setupBridgeIP(link, brIP); err != nil {
+		return fmt.Errorf("setup br ip: %w", err)
+	}
+	if err := t.setupBridgeMAC(link, brIP); err != nil {
+		return fmt.Errorf("setup mac: %w", err)
+	}
+	if err := netlink.LinkSetUp(link); err != nil {
+		return fmt.Errorf("link up: %w", err)
+	}
+	return nil
 }
