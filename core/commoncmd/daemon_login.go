dd85c727c8531c475cccd096b6a64a5cbdb413bd
Tristan Clowez <tristan.clowez@opensvc.com>
2025-11-14 15:31:52 +0100

    Add login and logout commands for authentication token management

---
+++ core/commoncmd/daemon_login.go
@@ -0,0 +1,139 @@
+package commoncmd
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
+
+	"github.com/opensvc/om3/core/client"
+	reqtoken "github.com/opensvc/om3/core/client/token"
+	"github.com/opensvc/om3/core/clientcontext"
+	"github.com/opensvc/om3/core/env"
+	"github.com/opensvc/om3/daemon/api"
+)
+
+type (
+	CmdDaemonLogin struct {
+		Context string
+	}
+)
+
+func NewCmdDaemonLogin() *cobra.Command {
+	var options CmdDaemonLogin
+	cmd := &cobra.Command{
+		Use:   "login",
+		Short: "Login and cache authentication tokens",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return options.Run()
+		},
+	}
+	flags := cmd.Flags()
+	flags.StringVar(&options.Context, "context", "", "The context to use to login")
+	return cmd
+}
+
+func (t *CmdDaemonLogin) Run() error {
+
+	if t.Context == "" {
+		if ctx := env.Context(); ctx != "" {
+			t.Context = ctx
+		} else {
+			config, err := clientcontext.Load()
+			if err != nil {
+				return err
+			}
+			fmt.Println("Known Contexts:")
+			for name := range config.Contexts {
+				fmt.Println(" - " + name)
+			}
+			fmt.Println()
+			name, _ := reqtoken.GetMostRecent()
+			fmt.Print("Select context")
+			if name != "" {
+				fmt.Printf(" [<%s>]", name)
+			}
+			fmt.Print(": ")
+			reader := bufio.NewReader(os.Stdin)
+			input, err := reader.ReadString('\n')
+			if err != nil && err != io.EOF {
+				return err
+			}
+			if input == "\n" && name != "" {
+				t.Context = name
+				return nil
+			} else if input == "\n" {
+				return fmt.Errorf("no context selected")
+			}
+			t.Context = strings.TrimSpace(input)
+		}
+	}
+
+	fmt.Print("Password: ")
+	pwdBytes, err := term.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		return err
+	}
+	fmt.Println()
+	password := string(pwdBytes)
+
+	os.Setenv("OSVC_CONTEXT", t.Context)
+
+	clientc, err := clientcontext.New()
+	if err != nil {
+		return err
+	}
+
+	c, err := client.New(client.WithUsername(clientc.User.Name), client.WithPassword(password))
+	if err != nil {
+		return err
+	}
+	refresh := true
+	params := api.PostAuthTokenParams{}
+	params.Refresh = &refresh
+
+	resp, err := c.PostAuthTokenWithResponse(context.Background(), &params)
+
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != http.StatusOK {
+		switch resp.StatusCode() {
+		case 400:
+			return fmt.Errorf("%s", resp.JSON400)
+		case 401:
+			return fmt.Errorf("%s", resp.JSON401)
+		case 403:
+			return fmt.Errorf("%s", resp.JSON403)
+		case 404:
+			return fmt.Errorf("%s", resp.JSON404)
+		case 500:
+			return fmt.Errorf("%s", resp.JSON500)
+		case 503:
+			return fmt.Errorf("%s", resp.JSON503)
+		default:
+			return fmt.Errorf("unexpected status code: %d", resp.StatusCode())
+		}
+	}
+
+	resp200 := resp.JSON200
+	token := reqtoken.Token{
+		AccessTokenExpire:  resp200.AccessExpiredAt,
+		AccessToken:        resp200.AccessToken,
+		RefreshTokenExpire: *resp200.RefreshExpiredAt,
+		RefreshToken:       *resp200.RefreshToken,
+	}
+	err = reqtoken.SaveToken(t.Context, token)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ core/commoncmd/daemon_logout.go
@@ -0,0 +1,82 @@
+package commoncmd
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	reqtoken "github.com/opensvc/om3/core/client/token"
+	"github.com/opensvc/om3/core/env"
+)
+
+type (
+	CmdDaemonLogout struct {
+		Context string
+	}
+)
+
+func NewCmdDaemonLogout() *cobra.Command {
+	var options CmdDaemonLogout
+	cmd := &cobra.Command{
+		Use:   "logout",
+		Short: "Logout and clear cached authentication tokens",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return options.Run()
+		},
+	}
+	flags := cmd.Flags()
+	flags.StringVar(&options.Context, "context", "", "The context to use to logout")
+	return cmd
+}
+
+func (t *CmdDaemonLogout) Run() error {
+	if t.Context == "" {
+		if ctx := env.Context(); ctx != "" {
+			t.Context = ctx
+		} else {
+			var valid []string
+			tokens := reqtoken.GetAllToken()
+			for name := range tokens {
+				tok := tokens[name]
+				if time.Now().Before(tok.RefreshTokenExpire) {
+					valid = append(valid, name)
+				}
+			}
+			if len(valid) == 0 {
+				return fmt.Errorf("no valid context login found")
+			}
+			fmt.Println("Current valid context logins:")
+			for _, name := range valid {
+				fmt.Println(" - " + name)
+			}
+			fmt.Println()
+			name, _ := reqtoken.GetMostRecent()
+			fmt.Print("Select context")
+			if name != "" {
+				fmt.Printf(" [<%s>]", name)
+			}
+			fmt.Print(": ")
+			reader := bufio.NewReader(os.Stdin)
+			input, err := reader.ReadString('\n')
+			if err != nil && err != io.EOF {
+				return err
+			}
+			t.Context = strings.TrimSpace(input)
+		}
+	}
+
+	if !reqtoken.TokenExists(t.Context) {
+		return fmt.Errorf("no token found for context %s", t.Context)
+	}
+
+	if err := reqtoken.DeleteToken(t.Context); err != nil {
+		return err
+	}
+
+	return nil
+}
