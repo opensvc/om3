522f0b216bde84d22249c7f35740406ceef9b170
Christophe Varoqui <christophe.varoqui@opensvc.com>
2026-01-24 09:14:18 +0100

    Remove daemon/daemonapi/lib_config_keywords.go
    Convert its last caller (daemon/daemonapi/get_cluster_config_keywords)
    to use the doc pkg instead.
    
    The "om cluster config doc" had already switched to the "doc" pkg.

--- daemon/daemonapi/get_cluster_config_keywords.go
+++ daemon/daemonapi/get_cluster_config_keywords.go
@@ -1,22 +1,21 @@
 package daemonapi
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 
+	"github.com/opensvc/om3/v3/core/doc"
 	"github.com/opensvc/om3/v3/core/naming"
 	"github.com/opensvc/om3/v3/core/object"
 	"github.com/opensvc/om3/v3/daemon/api"
 )
 
 func (a *DaemonAPI) GetClusterConfigKeywords(ctx echo.Context, params api.GetClusterConfigKeywordsParams) error {
-	var (
-		err    error
-		status int
-	)
+	var err error
 	store := object.KeywordStoreWithDrivers(naming.KindCcfg)
-	store, status, err = filterKeywordStore(ctx, store, params.Driver, params.Section, params.Option, naming.Cluster, func() (configProvider, error) {
+	store, err = doc.FilterKeywordStore(store, params.Driver, params.Section, params.Option, naming.Cluster, func() (doc.ConfigProvider, error) {
 		var (
 			i   any
 			err error
@@ -25,14 +24,18 @@ func (a *DaemonAPI) GetClusterConfigKeywords(ctx echo.Context, params api.GetClu
 		if err != nil {
 			return nil, err
 		}
-		return i.(configProvider), nil
+		return i.(doc.ConfigProvider), nil
 	})
-	if err != nil {
+	if errors.Is(err, doc.ErrBadRequest) {
+		status := http.StatusBadRequest
+		return JSONProblemf(ctx, status, http.StatusText(status), "%s", err)
+	} else if err != nil {
+		status := http.StatusInternalServerError
 		return JSONProblemf(ctx, status, http.StatusText(status), "%s", err)
 	}
 	r := api.KeywordDefinitionList{
 		Kind:  "KeywordDefinitionList",
-		Items: convertKeywordStore(store),
+		Items: doc.ConvertKeywordStore(store),
 	}
 	return ctx.JSON(http.StatusOK, r)
 }
