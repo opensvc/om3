6c92c54d9a668a856539ca71472f82a03c1c5db8
Cyril Galibern <cyril.galibern@opensvc.com>
2026-03-05 08:45:18 +0100

    [docker/podman/oci] Fix stop failure: docker stop has no '--timeout'
    Previously we used the version to enable or not the "--timeout" instead of "--time"
    but we can't trust the version
    Example: Docker version 26.1.5+dfsg1, build a72d7cd doesn't provides the docker stop --timemout
    
    Signed-off-by: Cyril Galibern <cyril.galibern@opensvc.com>

--- drivers/rescontainerdocker/caps.go
+++ drivers/rescontainerdocker/caps.go
@@ -3,10 +3,7 @@ package rescontainerdocker
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"os/exec"
-
-	"github.com/hashicorp/go-version"
 
 	"github.com/opensvc/om3/v3/util/capabilities"
 )
@@ -20,24 +17,13 @@ type (
 )
 
 var (
-	drvCap            = DrvID.Cap()
-	capRegistryCreds  = drvCap + ".registry_creds"
-	capSignal         = drvCap + ".signal"
-	capHasTimeoutFlag = drvCap + ".has_timeout_flag"
+	drvCap           = DrvID.Cap()
+	capRegistryCreds = drvCap + ".registry_creds"
+	capSignal        = drvCap + ".signal"
 )
 
 func init() {
 	capabilities.Register(capabilitiesScanner)
-}
-
-func getDockerInfo(ctx context.Context) (*dockerInfo, error) {
-	var di dockerInfo
-	b, err := exec.CommandContext(ctx, "docker", "info", "-f", "json").Output()
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(b, &di)
-	return &di, err
 }
 
 func IsGenuine(ctx context.Context) bool {
@@ -45,24 +31,6 @@ func IsGenuine(ctx context.Context) bool {
 	if err != nil {
 		return false
 	} else if bytes.Contains(b, []byte("Docker")) {
-		return true
-	}
-	return false
-}
-
-func isTimeoutCapable(di *dockerInfo) bool {
-	if di == nil {
-		return false
-	}
-	v, err := version.NewVersion(di.ClientInfo.Version)
-	if err != nil {
-		return false
-	}
-	constraints, err := version.NewConstraint(">= 22")
-	if err != nil {
-		return false
-	}
-	if constraints.Check(v) {
 		return true
 	}
 	return false
@@ -76,13 +44,6 @@ func capabilitiesScanner(ctx context.Context) ([]string, error) {
 	l = append(l, drvCap)
 	l = append(l, capRegistryCreds)
 	l = append(l, capSignal)
-
-	di, err := getDockerInfo(ctx)
-	if err == nil {
-		if isTimeoutCapable(di) {
-			l = append(l, capHasTimeoutFlag)
-		}
-	}
 
 	return l, nil
 }
