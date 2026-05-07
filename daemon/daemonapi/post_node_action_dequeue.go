31ddf8e1004fbbb90fcce39f031fe157c23be49d
Cyril Galibern <cyril.galibern@opensvc.com>
2026-05-07 22:26:06 +0200

    [daemon-api] Add `PostPeerActionDequeue` endpoint to handle node action dequeuing
    - Introduced `/api/node/name/{nodename}/action/dequeue` POST endpoint in API specification.
    - Added client and server support for the new endpoint.
    - Implemented `PostPeerActionDequeue` handling in `daemonapi` to process and acknowledge dequeued node actions.

---
+++ daemon/daemonapi/post_node_action_dequeue.go
@@ -0,0 +1,39 @@
+package daemonapi
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+
+	"github.com/opensvc/om3/v3/core/client"
+	"github.com/opensvc/om3/v3/core/naming"
+	"github.com/opensvc/om3/v3/daemon/api"
+)
+
+func (a *DaemonAPI) PostPeerActionDequeue(ctx echo.Context, nodename string, params api.PostPeerActionDequeueParams) error {
+	if v, err := assertRoot(ctx); !v {
+		return err
+	}
+	nodename = a.parseNodename(nodename)
+	if nodename == a.localhost {
+		return a.localNodeActionDequeue(ctx, params)
+	}
+	return a.proxy(ctx, nodename, func(c *client.T) (*http.Response, error) {
+		return c.PostPeerActionDequeue(ctx.Request().Context(), nodename, &params)
+	})
+}
+
+func (a *DaemonAPI) localNodeActionDequeue(ctx echo.Context, params api.PostPeerActionDequeueParams) error {
+	log := LogHandler(ctx, "PostPeerActionDequeue")
+	var requesterSid uuid.UUID
+	args := []string{"node", "dequeue"}
+	if params.RequesterSid != nil {
+		requesterSid = *params.RequesterSid
+	}
+	if sid, err := a.apiExec(ctx, naming.Path{}, requesterSid, args, log); err != nil {
+		return JSONProblemf(ctx, http.StatusInternalServerError, "", "%s", err)
+	} else {
+		return ctx.JSON(http.StatusOK, api.NodeActionAccepted{SessionID: sid})
+	}
+}
