9054790466f6c042613c4af924c00a949198c69e
Cyril Galibern <cyril.galibern@opensvc.com>
2026-06-29 15:12:05 +0200

    [resfssgcp_nfs] Add driver implementation and keyword definitions
    - Implemented the `resfssgcp_nfs` driver with manifest and capabilities registration.
    - Added keyword definitions for UUID, host, permission, exclusive, protocol, secret, and endpoint.
    - Embedded text descriptions for keywords to facilitate configuration and usage.

---
+++ drivers/resfssgcp_nfs/manifest.go
@@ -0,0 +1,90 @@
+package resfssgcp_nfs
+
+import (
+	"embed"
+
+	"github.com/opensvc/om3/v3/core/datarecv"
+	"github.com/opensvc/om3/v3/core/driver"
+	"github.com/opensvc/om3/v3/core/keywords"
+	"github.com/opensvc/om3/v3/core/manifest"
+	"github.com/opensvc/om3/v3/core/naming"
+)
+
+var (
+	//go:embed text
+	fs embed.FS
+
+	drvID = driver.NewID(driver.GroupFS, "sgcp_nfs")
+
+	kws = []*keywords.Keyword{
+		{
+			Attr:     "UUID",
+			Option:   "uuid",
+			Required: true,
+			Scopable: true,
+			Text:     keywords.NewText(fs, "text/kw/uuid"),
+		},
+		{
+			Attr:     "Host",
+			Option:   "host",
+			Required: true,
+			Scopable: true,
+			Text:     keywords.NewText(fs, "text/kw/host"),
+		},
+		{
+			Attr:       "Permission",
+			Option:     "permission",
+			Candidates: []string{"read-only", "read-write"},
+			Default:    DefaultPermission,
+			Scopable:   true,
+			Text:       keywords.NewText(fs, "text/kw/permission"),
+		},
+		{
+			Attr:      "Exclusive",
+			Option:    "exclusive",
+			Converter: "boolean",
+			Default:   "false", // TODO: move to config
+			Scopable:  true,
+			Text:      keywords.NewText(fs, "text/kw/exclusive"),
+		},
+		{
+			Attr:       "Protocol",
+			Option:     "protocol",
+			Candidates: []string{"nfs4.1"},
+			Default:    DefaultProtocol, // TODO: move to config
+			Scopable:   true,
+			Text:       keywords.NewText(fs, "text/kw/protocol"),
+		},
+		{
+			Attr:     "Secret",
+			Option:   "secret",
+			Default:  SecretDefaultName,
+			Scopable: true,
+			Text:     keywords.NewText(fs, "text/kw/secret"),
+		},
+		{
+			Attr:     "Endpoint",
+			Option:   "endpoint",
+			Default:  "https://localhost/v1", // TODO: move to config
+			Scopable: true,
+			Text:     keywords.NewText(fs, "text/kw/endpoint"),
+		},
+	}
+)
+
+func init() {
+	driver.Register(drvID, New)
+}
+
+func (t *T) DriverID() driver.ID {
+	return drvID
+}
+
+// Manifest exposes to the core the input expected by the driver.
+func (t *T) Manifest() *manifest.T {
+	m := manifest.New(drvID, t)
+	m.Kinds.Or(naming.KindSvc, naming.KindVol)
+	m.AddKeywords(kws...)
+	m.AddKeywords(datarecv.Keywords("DataRecv.")...)
+	return m
+}
