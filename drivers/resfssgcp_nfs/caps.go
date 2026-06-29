c30e69b37bc77c5e8840300c296faa14d9afbd2f
Cyril Galibern <cyril.galibern@opensvc.com>
2026-06-29 15:12:05 +0200

    [resfssgcp_nfs] Add driver implementation and keyword definitions
    - Implemented the `resfssgcp_nfs` driver with manifest and capabilities registration.
    - Added keyword definitions for UUID, host, permission, exclusive, protocol, secret, and endpoint.
    - Embedded text descriptions for keywords to facilitate configuration and usage.

---
+++ drivers/resfssgcp_nfs/caps.go
@@ -0,0 +1,15 @@
+package resfssgcp_nfs
+
+import (
+	"context"
+
+	"github.com/opensvc/om3/v3/util/capabilities"
+)
+
+func init() {
+	capabilities.Register(capabilitiesScanner)
+}
+
+func capabilitiesScanner(ctx context.Context) ([]string, error) {
+	return []string{drvID.Cap()}, nil
+}
