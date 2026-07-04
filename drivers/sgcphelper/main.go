ffc797283ea1a896a1f39da8f7d0804a85cd6168
Cyril Galibern <cyril.galibern@opensvc.com>
2026-07-04 12:14:50 +0200

    [sgcp] Add AuthInfo struct and GetAuthInfo utility function
    - Defined `AuthInfo` struct to encapsulate authentication details (Account ID, Client ID, Client Secret, Signature).
    - Implemented `GetAuthInfo` function to retrieve and decode auth details from a datastore path.

---
+++ drivers/sgcphelper/main.go
@@ -0,0 +1,39 @@
+package sgcphelper
+
+import (
+	"fmt"
+
+	"github.com/opensvc/om3/v3/core/naming"
+	"github.com/opensvc/om3/v3/core/object"
+	"github.com/opensvc/om3/v3/util/sgcp"
+)
+
+// GetAuthInfo retrieves authentication information from the specified datastore path and returns an AuthInfo struct.
+func GetAuthInfo(datastorePath string) (*sgcp.AuthInfo, error) {
+	var (
+		ds       object.DataStore
+		dsValues [][]byte
+	)
+	secPath, err := naming.ParsePath(datastorePath)
+	if err != nil {
+		return nil, fmt.Errorf("parse secret path: %w", err)
+	}
+
+	ds, err = object.NewSec(secPath, object.WithVolatile(true))
+	if err != nil {
+		return nil, fmt.Errorf("get secret %s: %w", secPath, err)
+	}
+
+	dsValues, err = ds.DecodeKeys("account_id", "client_id", "client_secret")
+	if err != nil {
+		return nil, fmt.Errorf("decode auth keys from %s: %w", secPath, err)
+	}
+	authInfo := &sgcp.AuthInfo{
+		AccountID:    string(dsValues[0]),
+		ClientID:     string(dsValues[1]),
+		ClientSecret: string(dsValues[2]),
+
+		Signature: fmt.Sprintf("sgcp-authinfo-%s-%s", secPath.Namespace, secPath.Name),
+	}
+	return authInfo, nil
+}
