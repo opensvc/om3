8e57b519ce1156b64683eaec14f302706f1f44e7
Cyril Galibern <cyril.galibern@opensvc.com>
2026-08-17 09:15:18 +0200

    [core/rawconfig] Expand default SSRF blocked CIDR ranges
    Add reserved IPv4 and IPv6 ranges to the default SSRFBlockedCIDR,
    including RFC 5737 TEST-NET and link-local ranges, to enhance security
    and compliance with best practices.

--- core/rawconfig/ssrf_policy.go
+++ core/rawconfig/ssrf_policy.go
@@ -14,17 +14,20 @@ var (
 	defaultSSRFAllowedCIDR = []string{}
 
 	defaultSSRFBlockedCIDR = []string{
-		// RFC 1918 private address ranges
-		"10.0.0.0/8",
-		"172.16.0.0/12",
-		"192.168.0.0/16",
+		"127.0.0.0/8",     // RFC 1122 loopback
+		"10.0.0.0/8",      // RFC 1918 private
+		"172.16.0.0/12",   // RFC 1918 private
+		"192.168.0.0/16",  // RFC 1918 private
+		"169.254.0.0/16",  // link local
+		"192.0.2.0/24",    // RFC 5737 private TEST-NET-1
+		"198.51.100.0/24", // RFC 5737 private TEST-NET-2
+		"203.0.113.0/24",  // RFC 5737 private TEST-NET-3
 
-		// RFC 4193 Unique Local IPv6 Unicast Addresses
-		"fc00::/7",
-
-		// Loopback ranges
-		"127.0.0.0/8", // RFC 1122
-		"::1/128",     // RFC 4291
+		// IPV6
+		"::1/128",   // RFC 4291 loopback
+		"fe80::/10", // link-local
+		"fc00::/7",  // RFC 4193 Unique Local IPv6 Unicast Addresses
+		"ff00::/8",  // reserved
 	}
 )
 
@@ -32,7 +35,7 @@ var (
 // Default is:
 // OSVC_SSRF_ALLOWED_URL = https://raw.githubusercontent.com/opensvc/opensvc_templates/*
 // OSVC_SSRF_BLOCKED_URL = *
-// OSVC_SSRF_BLOCKED_CIDR = 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16 fc00::/7 127.0.0.0/8 ::1/128
+// OSVC_SSRF_BLOCKED_CIDR = 127.0.0.0/8 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16 169.254.0.0/16 192.0.2.0/24 198.51.100.0/24 203.0.113.0/24 ::1/128 fe80::/10 fc00::/7 ff00::/8
 // OSVC_SSRF_ALLOWED_CIDR =
 // OSVC_SSRF_ENABLE_REDIRECTS = false
 func setSSRF(env map[string]string) {
